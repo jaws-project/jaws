29774706711e45c0b5953be7688b561db044508a
Ali Fazelzadeh <afz@dev-code.com>
2012-10-07 20:30:16 +0330

    Added Skeleton gadget

---
+++ Skeleton/Actions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Skeleton Actions file
+ *
+ * @category   GadgetActions
+ * @package    Skeleton
+ * @author     Pablo Fischer <pablo@pablo.com.mx>
+ * @copyright  2004-2012 Jaws Development Group
+ * @license    http://www.gnu.org/copyleft/gpl.html
+ */
+/* Normal actions*/
+$actions = array();
+/* Layout actions */
+$actions['Display'] = array('LayoutAction', 
+                            _t('SKELETON_ACTION_DISPLAY'),
+                            _t('SKELETON_ACTION_DISPLAY_DESC'));
+?>
---
+++ Skeleton/languages/nb.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Meta Data
+ *
+ * "Project-Id-Version: Skeleton"
+ * "Last-Translator: Karl-Gustav Freding <gusse500@gusse.no>"
+ * "Language-Team: NB"
+ * "MIME-Version: 1.0"
+ * "Content-Type: text/plain; charset=UTF-8"
+ * "Content-Transfer-Encoding: 8bit"
+ */
+
+define('_NB_SKELETON_NAME', "Skjelett");
+define('_NB_SKELETON_DESCRIPTION', "En grunnmodul for å bruke i nye moduler.");
+define('_NB_SKELETON_ACTION_DEFAULT', "Standard handling");
+define('_NB_SKELETON_ACTION_DISPLAY', "Vis versjon");
+define('_NB_SKELETON_ACTION_DISPLAY_DESC', "Viser versjonsnummeret til Jawsinstalleringen.");
+define('_NB_SKELETON_DISPLAY_MESSAGE', "Du bruker Jaws {0}");
+define('_NB_SKELETON_ADMIN_MESSAGE', "Denne modulen er et eksempel. Det er ingen kontrollpanel til modulen.");
