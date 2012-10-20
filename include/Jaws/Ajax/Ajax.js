bea631081c7d86e53ee8d50ffd80acb036ac0fe3
Ali Fazelzadeh <afz@dev-code.com>
2012-10-20 22:13:18 +0330

    Added JawsDefilter method to jaws javascript based file

--- include/Jaws/Ajax/Ajax.js
+++ include/Jaws/Ajax/Ajax.js
@@ -81,6 +81,63 @@ function getEditorValue(name)
 }
 
 /**
+ * Javascript htmlspecialchars_decode
+ *
+ * @see https://raw.github.com/kvz/phpjs/master/functions/strings/htmlspecialchars_decode.js
+ */
+function JawsDefilter(string, quote_style)
+{
+    var optTemp = 0,
+        i = 0,
+        noquotes = false;
+
+    if (typeof quote_style === 'undefined') {
+        quote_style = 3;
+    }
+
+    string = string.toString().replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+    var OPTS = {
+        'ENT_NOQUOTES': 0,
+        'ENT_HTML_QUOTE_SINGLE': 1,
+        'ENT_HTML_QUOTE_DOUBLE': 2,
+        'ENT_COMPAT': 2,
+        'ENT_QUOTES': 3,
+        'ENT_IGNORE': 4
+    };
+
+    if (quote_style === 0) {
+        noquotes = true;
+    }
+
+    // Allow for a single string or an array of string flags
+    if (typeof quote_style !== 'number') {
+        quote_style = [].concat(quote_style);
+        for (i = 0; i < quote_style.length; i++) {
+            // Resolve string input to bitwise e.g. 'PATHINFO_EXTENSION' becomes 4
+            if (OPTS[quote_style[i]] === 0) {
+                noquotes = true;
+            } else if (OPTS[quote_style[i]]) {
+                optTemp = optTemp | OPTS[quote_style[i]];
+            }
+        }
+        quote_style = optTemp;
+    }
+
+    if (quote_style & OPTS.ENT_HTML_QUOTE_SINGLE) {
+        string = string.replace(/&#0*39;/g, "'"); // PHP doesn't currently escape if more than one 0, but it should
+        // string = string.replace(/&apos;|&#x0*27;/g, "'"); // This would also be useful here, but not a part of PHP
+    }
+
+    if (!noquotes) {
+        string = string.replace(/&quot;/g, '"');
+    }
+
+    // Put this in last place to avoid escape being double-decoded
+    string = string.replace(/&amp;/g, '&');
+    return string;
+}
+
+/**
  * Reset a (piwi)datagrid:
  *  - Clean all data
  *  - Set the new data
