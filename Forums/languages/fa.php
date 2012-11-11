c46f63c572f0a72792ed67ccad07b2f856a61b35
Ali Fazelzadeh <afz@dev-code.com>
2012-11-11 21:49:26 +0330

    [Forums]: Added Persian translation

---
+++ Forums/languages/fa.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Meta Data
+ *
+ * "Project-Id-Version: Forums"
+ * "Last-Translator: Ali Fazelzadeh <afz@php.net>"
+ * "Language-Team: FA"
+ * "MIME-Version: 1.0"
+ * "Content-Type: text/plain; charset=UTF-8"
+ * "Content-Transfer-Encoding: 8bit"
+ */
+
+define('_FA_FORUMS_NAME', "انجمن");
+define('_FA_FORUMS_DESCRIPTION', "ابزاری برای گفتمان کاربران در بخشهای گوناگون");
+define('_FA_FORUMS_GROUP', "دسته");
+define('_FA_FORUMS_GROUPS', "دسته ها");
+define('_FA_FORUMS_FORUM', "انجمن");
+define('_FA_FORUMS_FORUMS', "تالار گفتمان");
+define('_FA_FORUMS_TOPIC', "جستار");
+define('_FA_FORUMS_TOPICS', "جستارها");
+define('_FA_FORUMS_POST', "نوشته");
+define('_FA_FORUMS_POSTS', "نوشته ها");
+define('_FA_FORUMS_ORDER', "راسته");
+define('_FA_FORUMS_FASTURL', "نگاشت");
+define('_FA_FORUMS_VIEWS', "بازدیدها");
+define('_FA_FORUMS_REPLIES', "پاسخ ها");
+define('_FA_FORUMS_LOCKED', "بسته شده");
+define('_FA_FORUMS_POSTEDBY', "بدست");
+define('_FA_FORUMS_LASTPOST', "واپسین نوشته");
+define('_FA_FORUMS_TOPICS_SUBJECT', "جستار");
+define('_FA_FORUMS_TOPICS_NEW', "جستار تازه");
+define('_FA_FORUMS_TOPICS_NEW_TITLE', "جستار تازه");
+define('_FA_FORUMS_TOPICS_NEW_BUTTON', "افزودن جستار تازه");
+define('_FA_FORUMS_TOPICS_NEW_ERROR', "هنگام افزودن جستار تازه، خطایی رخ داده است");
+define('_FA_FORUMS_TOPICS_EDIT', "ویرایش جستار");
+define('_FA_FORUMS_TOPICS_EDIT_TITLE', "ویرایش جستار");
+define('_FA_FORUMS_TOPICS_EDIT_BUTTON', "بروزرسانی جستار");
+define('_FA_FORUMS_TOPICS_EDIT_ERROR', "هنگام بروزرسانی جستار، خطایی رخ داده است");
+define('_FA_FORUMS_TOPICS_DELETE', "زدودن جستار");
+define('_FA_FORUMS_TOPICS_DELETE_TITLE', "زدودن جستار");
+define('_FA_FORUMS_TOPICS_DELETE_BUTTON', "زدودن جستار");
+define('_FA_FORUMS_TOPICS_DELETE_ERROR', "هنگام زدودن جستار، خطایی رخ داده است");
+define('_FA_FORUMS_TOPICS_LOCK', "بستن جستار");
+define('_FA_FORUMS_TOPICS_UNLOCK', "بازکردن جستار");
+define('_FA_FORUMS_POSTS_NEW', "نوشته تازه");
+define('_FA_FORUMS_POSTS_NEW_TITLE', "نوشته تازه");
+define('_FA_FORUMS_POSTS_NEW_BUTTON', "افزودن نوشته تازه");
+define('_FA_FORUMS_POSTS_NEW_ERROR', "هنگام افزودن نوشته تازه، خطایی رخ داده است");
+define('_FA_FORUMS_POSTS_EDIT', "ویرایش نوشته");
+define('_FA_FORUMS_POSTS_EDIT_TITLE', "ویرایش نوشته");
+define('_FA_FORUMS_POSTS_EDIT_BUTTON', "بروزرسانی");
+define('_FA_FORUMS_POSTS_EDIT_ERROR', "هنگام بروزرسانی نوشته، خطایی رخ داده است");
+define('_FA_FORUMS_POSTS_DELETE', "زدودن نوشته");
+define('_FA_FORUMS_POSTS_DELETE_TITLE', "زدودن نوشته");
+define('_FA_FORUMS_POSTS_DELETE_BUTTON', "زدودن نوشته");
+define('_FA_FORUMS_POSTS_DELETE_ERROR', "هنگام زدودن نوشته، خطایی رخ داده است");
+define('_FA_FORUMS_POSTS_MESSAGE', "پیام");
+define('_FA_FORUMS_POSTS_EDIT_REASON', "انگیزه ویرایش");
+define('_FA_FORUMS_POSTS_UPDATEDBY', "بروزرسانی بدست");
+define('_FA_FORUMS_USERS_POSTS_COUNT', "نوشته ها");
+define('_FA_FORUMS_USERS_REGISTERED_DATE', "زمان نام نویسی");
