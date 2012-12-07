5bfc5cf33bef4ecfd533e3d2bb924dc44300a018
Ali Fazelzadeh <afz@dev-code.com>
2012-12-07 14:18:41 +0330

    Added "da" full translations

---
+++ da/gadgets/Settings.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ *  Danish Translation of the ControlPanel gadget
+ *
+ *  Author: Mark Rosenstand <mark@borkware.net>
+ *  Copyright 2005, Jaws Project.
+ *
+ */
+
+/**
+ * Meta Data
+ *
+ * "Project-Id-Version: ControlPanel"
+ * "Last-Translator: Mark Rosenstand <mark@borkware.net>"
+ * "Language-Team: DA"
+ * "MIME-Version: 1.0"
+ * "Content-Type: text/plain; charset=UTF-8"
+ * "Content-Transfer-Encoding: 8bit"
+ */
+
+define('_DA_SETTINGS_NAME', "Indstillinger");
+define('_DA_SETTINGS_DESCRIPTION', "Generelle Jaws-indstillinger.");
+
+/* ACLs */
+define('_DA_SETTINGS_ACL_MANAGESETTINGS', "Generelle indstillinger");
+
+/* Site Settings */
+define('_DA_SETTINGS', "Indstillinger");
+define('_DA_SETTINGS_SITE_NAME', "Navn på siden");
+define('_DA_SETTINGS_SITE_DESCRIPTION', "Beskrivelse af siden");
+define('_DA_SETTINGS_DEFAULT_THEME', "Standardtema");
+define('_DA_SETTINGS_DEFAULT_SITE_LANGUAGE', "Standardsprog");
+define('_DA_SETTINGS_MAIN_GADGET', "Standardmodul");
+define('_DA_SETTINGS_SITE_AUTHOR', "Ejers navn");
+define('_DA_SETTINGS_COPYRIGHT', "Ophavsret");
+define('_DA_SETTINGS_DATE_FORMAT', "Dateringsformat");
+define('_DA_SETTINGS_AUTH_METHOD', "Autencitetsmetode");
+define('_DA_SETTINGS_GRAVATAR', "Gravatar-rangering");
+define('_DA_SETTINGS_GRAVATAR_G', "G: Børnevenligt indhold.");
+define('_DA_SETTINGS_GRAVATAR_PG', "PG: Disputabelt stødende indhold.");
+define('_DA_SETTINGS_GRAVATAR_R', "R: Nøgenhed, intensiv vold, utvetydigt seksuelt indhold eller information om narkotika.");
+define('_DA_SETTINGS_GRAVATAR_X', "X: Alvorligt seksuelt billedsprog eller ekstremt foruroligende vold.");
+define('_DA_SETTINGS_SAVED', "Dine indstillinger er blevet gemt.");
