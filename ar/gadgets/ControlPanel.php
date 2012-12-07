8583d77c13a669152c0896bfa9941041335bc619
Ali Fazelzadeh <afz@dev-code.com>
2012-12-07 14:15:28 +0330

    Added Arabic full translations

---
+++ ar/gadgets/ControlPanel.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Meta Data
+ *
+ * "Project-Id-Version: ControlPanel"
+ * "Last-Translator: Ahmed Shaheen  <ahmedshaheen@freewarearabia.com>"
+ * "Language-Team: AR"
+ * "MIME-Version: 1.0"
+ * "Content-Type: text/plain; charset=UTF-8"
+ * "Content-Transfer-Encoding: 8bit"
+ */
+
+define('_AR_CONTROLPANEL_NAME', "لوحة التحكم");
+define('_AR_CONTROLPANEL_DESCRIPTION', "لوحة تحكم جاوس.");
+define('_AR_CONTROLPANEL_ACL_DEFAULT', "استخدم لوحة التحكم");
+define('_AR_CONTROLPANEL_ACL_BACKUP', "نسخ احتياطى لقاعدة البيانات");
+define('_AR_CONTROLPANEL_GENERAL', "عام");
+define('_AR_CONTROLPANEL_GENERAL_BACKUP', "نسخة احتياطية لقاعدة البيانات");
+define('_AR_CONTROLPANEL_GENERAL_VIEWSITE', "اعرض الموقع");
+define('_AR_CONTROLPANEL_GADGETS', "البريمجات");
+define('_AR_CONTROLPANEL_LOGIN_BAD_AUTH', "لقد قمت بإدخال اسم مستخدم أو كلمة سر غير صحيحة .");
+define('_AR_CONTROLPANEL_LOGIN_LOST_PASSWORD', "هل فقدت كلمة السر ؟");
+define('_AR_CONTROLPANEL_LOGIN_BACK_TO_SITE', "العودة للموقع");
+define('_AR_CONTROLPANEL_LOGIN_TITLE', "الدخول إلى لوحة التحكم");
+define('_AR_CONTROLPANEL_LOGGED_IN_AS', "تم الدخول بحساب ");
+define('_AR_CONTROLPANEL_UNSAVED_CHANGES', "لديك متغيرات لم تحفظ فى هذه الصفحة");
---
+++ ar/gadgets/StaticPage.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Meta Data
+ *
+ * "Project-Id-Version: StaticPage"
+ * "Last-Translator:    Ahmed Shaheen "
+ * "Language-Team: AR"
+ * "MIME-Version: 1.0"
+ * "Content-Type: text/plain; charset=UTF-8"
+ * "Content-Transfer-Encoding: 8bit"
+ */
+  
+define('_AR_STATICPAGE_NAME', "الصفحات الثابتة");
+define('_AR_STATICPAGE_DESCRIPTION', "بريمج لعمل وادارة المحتويات الثابتة .");
+define('_AR_STATICPAGE_ACTION_DEFAULT', "عرض الصفحة الأفتراضية");
+define('_AR_STATICPAGE_ACTION_INDEX', "عرض دليل للصفحات الثابتة");
+define('_AR_STATICPAGE_ACTION_PAGE', "عرض صفحة واحدة");
+define('_AR_STATICPAGE_LAYOUT_PAGES_LIST', "قائمة الصفحة");
+define('_AR_STATICPAGE_LAYOUT_PAGES_LIST_DESCRIPTION', "عرض الصفحات الثابتة الموجودة فى قائمة");
+define('_AR_STATICPAGE_ACL_DEFAULT', "التحكم فى الصفحات الثابتة");
+define('_AR_STATICPAGE_ACL_ADDPAGE', "اضافة صفحات");
+define('_AR_STATICPAGE_ACL_EDITPAGE', "تعديل الصفحات");
+define('_AR_STATICPAGE_ACL_DELETEPAGE', "حذف الصفحات");
+define('_AR_STATICPAGE_ERROR_PAGE_NOT_ADDED', ".هناك مشكلة فى اضافة الصفحة");
+define('_AR_STATICPAGE_ERROR_PAGE_NOT_UPDATED', ".هناك مشكلة فى تحديث الصفحة");
+define('_AR_STATICPAGE_ERROR_PAGE_NOT_DELETED', "هناك مشكلة فى حذف الصفحة .");
+define('_AR_STATICPAGE_ERROR_PAGE_NOT_FOUND', "الصفحة التى طلبتها غير موجودة ");
+define('_AR_STATICPAGE_ERROR_UNKNOWN_COLUMN', "An unknown sort column was provided.");
+define('_AR_STATICPAGE_ERROR_PAGES_NOT_RETRIEVED', "The page index could not be loaded.");
+define('_AR_STATICPAGE_ERROR_INDEX_NOT_LOADED', "The page index could not be loaded.");
+define('_AR_STATICPAGE_ERROR_PAGE_NOT_MASSIVE_DELETED', "هناك مشكلة عند حذف مجموعة الصفحات");
+define('_AR_STATICPAGE_PAGES_LIST', "Page Index");
+define('_AR_STATICPAGE_TITLE_NOT_FOUND', "الصفحة غير موجودة");
+define('_AR_STATICPAGE_CONTENT_NOT_FOUND', "الصفحة التى طلبتها غير موجودة , اذا كنت متأكدا من وجودها من فضلك اتصل بالمدير العام للموقع .");
+define('_AR_STATICPAGE_TITLE_INDEX', "Page Index");
+define('_AR_STATICPAGE_MENU_PAGES', "ادارة الصفحات");
+define('_AR_STATICPAGE_MENU_ADDPAGE', "اضافة صفحة");
+define('_AR_STATICPAGE_LAST_UPDATE', "آخر تحديث");
+define('_AR_STATICPAGE_NO_PAGES', ".لم يعثر على أى صفحات");
+define('_AR_STATICPAGE_PAGE_CREATED', "تم عمل الصفحة.");
+define('_AR_STATICPAGE_PAGE_UPDATED', "تم تحديث الصفحة.");
+define('_AR_STATICPAGE_PAGE_DELETED', "تم حذف الصفحة.");
+define('_AR_STATICPAGE_PAGE_MASSIVE_DELETED', "تم حذف مجموعة الصفحات");
+define('_AR_STATICPAGE_PAGE_AUTOUPDATED', "تم حفظ الصفحة اوتوماتيكيا.");
+define('_AR_STATICPAGE_ADD_PAGE', "اضافة صفحة");
+define('_AR_STATICPAGE_UPDATE_PAGE', "Update Page");
+define('_AR_STATICPAGE_PAGE', "Page");
+define('_AR_STATICPAGE_CONFIRM_DELETE_PAGE', "هل أنت متأكد من حذف هذه الصفحة ؟");
+define('_AR_STATICPAGE_CONFIRM_MASIVE_DELETE_PAGE', "هل أنت متأكد من حذف الصفحات المحددة ؟");
+define('_AR_STATICPAGE_FASTURL', "رابط سريع");
+define('_AR_STATICPAGE_DRAFT', "مسودة");
+define('_AR_STATICPAGE_PUBLISHED', "منشور");
+define('_AR_STATICPAGE_STATUS', "الحالة");
+define('_AR_STATICPAGE_SHOW_TITLE', "اظهر العنوان ");
