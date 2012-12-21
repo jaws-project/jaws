651097444f0935f02056494a103c6a19b031c5b5
Ali Fazelzadeh <afz@dev-code.com>
2012-12-21 17:33:03 +0330

    [Launcher]: separating install/uninstall/upgrade methods

---
+++ gadgets/Launcher/Installer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Launcher Installer
+ *
+ * @category    GadgetModel
+ * @package     Launcher
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2012 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/lesser.html
+ */
+class Launcher_Installer extends Jaws_Gadget_Installer
+{
+    /**
+     */
+}
