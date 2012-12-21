7fb9be1ce909e784cdef97283818ac3f8c444f70
Ali Fazelzadeh <afz@dev-code.com>
2012-12-21 10:12:37 +0330

    [SysInfo]: Applying new changes of Jaws_Gadget_Model

--- SysInfo/AdminModel.php
+++ SysInfo/AdminModel.php
@@ -34,7 +34,7 @@ class SysInfoAdminModel extends SysInfoModel
     function UpdateGadget($old, $new)
     {
         // Registry keys
-        $this->DelRegistry('frontend_avail');
+        $this->gadget->DelRegistry('frontend_avail');
 
         // ACL keys
         $GLOBALS['app']->ACL->NewKey('/ACL/gadgets/SysInfo/SysInfo',  'false');
