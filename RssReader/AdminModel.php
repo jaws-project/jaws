e5efb3109d34f414bc6cace5649f3f8af3c5b381
Ali Fazelzadeh <afz@dev-code.com>
2012-12-22 17:25:25 +0330

    [RssReader]: separating install/uninstall/upgrade methods

--- RssReader/AdminModel.php
+++ RssReader/AdminModel.php
@@ -1,4 +1,5 @@
 <?php
+require_once JAWS_PATH . 'gadgets/RssReader/Model.php';
 /**
  * RssReader Gadget
  *
@@ -10,88 +11,8 @@
  * @copyright  2005-2012 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
-require_once JAWS_PATH . 'gadgets/RssReader/Model.php';
-
 class RssReaderAdminModel extends RssReaderModel
 {
-    /**
-     * Installs the gadget
-     *
-     * @access  public
-     * @return  mixed   true on successful installation, Jaws_Error otherwise
-     */
-    function InstallGadget()
-    {
-        if (!Jaws_Utils::is_writable(JAWS_DATA)) {
-            return new Jaws_Error(_t('GLOBAL_ERROR_FAILED_DIRECTORY_UNWRITABLE', JAWS_DATA));
-        }
-
-        $new_dir = JAWS_DATA . 'rsscache' . DIRECTORY_SEPARATOR;
-        if (!Jaws_Utils::mkdir($new_dir)) {
-            return new Jaws_Error(_t('GLOBAL_ERROR_FAILED_CREATING_DIR', $new_dir), _t('RSSREADER_NAME'));
-        }
-
-        $result = $this->installSchema('schema.xml');
-        if (Jaws_Error::IsError($result)) {
-            return $result;
-        }
-
-        // Registry keys
-        $this->gadget->AddRegistry('default_feed', '0');
-
-        return true;
-    }
-
-    /**
-     * Uninstalls the gadget
-     *
-     * @access  public
-     * @return  mixed   true on success, Jaws_Error otherwise
-     */
-    function UninstallGadget()
-    {
-        $result = $GLOBALS['db']->dropTable('rss_sites');
-        if (Jaws_Error::IsError($result)) {
-            $gName  = _t('RSSREADER_NAME');
-            $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $gName);
-            $GLOBALS['app']->Session->PushLastResponse($errMsg, RESPONSE_ERROR);
-            return new Jaws_Error($errMsg, $gName);
-        }
-
-        // registry keys
-        $this->gadget->DelRegistry('default_feed');
-
-        return true;
-    }
-
-    /**
-     * Updates the gadget
-     *
-     * @access  public
-     * @param   string  $old    Current version (in registry)
-     * @param   string  $new    New version (in the $gadgetInfo file)
-     * @return  mixed   true on success, Jaws_Error otherwise
-     */
-    function UpdateGadget($old, $new)
-    {
-        $result = $this->installSchema('schema.xml', '', "$old.xml");
-        if (Jaws_Error::IsError($result)) {
-            return $result;
-        }
-
-        // ACL keys
-        $GLOBALS['app']->ACL->DeleteKey('/ACL/gadgets/RssReader/DeleteSite');
-        $GLOBALS['app']->ACL->DeleteKey('/ACL/gadgets/RssReader/UpdateProperties');
-
-        //registry keys
-        $this->gadget->AddRegistry('default_feed', '0');
-        $this->gadget->DelRegistry('limit_entries');
-        $this->gadget->DelRegistry('order_type');
-        $this->gadget->DelRegistry('sort_type');
-
-        return true;
-    }
-
     /**
      * Inserts a new RSS site
      *
