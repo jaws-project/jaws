971dd30a61b73e44f47757616a48d931a0c554bd
Ali Fazelzadeh <afz@dev-code.com>
2012-12-22 16:30:08 +0330

    [Friends]: separating install/uninstall/upgrade methods

--- Friends/AdminModel.php
+++ Friends/AdminModel.php
@@ -14,67 +14,6 @@ require_once JAWS_PATH . 'gadgets/Friends/Model.php';
 class FriendsAdminModel extends FriendsModel
 {
     /**
-     * Install the gadget
-     *
-     * @access  public
-     * @return  mixed   Returns true if installation success and Jaws_Error on any error found
-     */
-    function InstallGadget()
-    {
-        $result = $this->installSchema('schema.xml');
-        if (Jaws_Error::IsError($result)) {
-            return $result;
-        }
-
-        //registry keys.
-        $this->gadget->AddRegistry('limit', '5');
-
-        return true;
-    }
-
-    /**
-     * Uninstalls the gadget
-     *
-     * @access  public
-     * @return  mixed  True on Success or Jaws_Error on Failure
-     */
-    function UninstallGadget()
-    {
-        $result = $GLOBALS['db']->dropTable('friend');
-        if (Jaws_Error::IsError($result)) {
-            $gName  = _t('FRIENDS_NAME');
-            $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $gName);
-            $GLOBALS['app']->Session->PushLastResponse($errMsg, RESPONSE_ERROR);
-            return new Jaws_Error($errMsg, $gName);
-        }
-
-        //registry keys.
-        $this->gadget->DelRegistry('limit');
-
-        return true;
-    }
-
-    /**
-     * Update the gadget
-     *
-     * @access  public
-     * @param   string  $old    Current version (in registry)
-     * @param   string  $new    New version (in the $gadgetInfo file)
-     * @return  mixed   True on Success or Jaws_Error on Failure
-     */
-    function UpdateGadget($old, $new)
-    {
-        $result = $this->installSchema('schema.xml', '', "$old.xml");
-        if (Jaws_Error::IsError($result)) {
-            return $result;
-        }
-
-        // Registry keys.
-
-        return true;
-    }
-
-    /**
      * Set properties of the gadget
      *
      * @access  public
---
+++ Friends/Installer.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Friends Installer
+ *
+ * @category    GadgetModel
+ * @package     Friends
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2012 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ */
+class Friends_Installer extends Jaws_Gadget_Installer
+{
+    /**
+     * Install the gadget
+     *
+     * @access  public
+     * @return  mixed   Returns true if installation success and Jaws_Error on any error found
+     */
+    function Install()
+    {
+        $result = $this->installSchema('schema.xml');
+        if (Jaws_Error::IsError($result)) {
+            return $result;
+        }
+
+        //registry keys.
+        $this->gadget->AddRegistry('limit', '5');
+
+        return true;
+    }
+
+    /**
+     * Uninstalls the gadget
+     *
+     * @access  public
+     * @return  mixed  True on Success or Jaws_Error on Failure
+     */
+    function Uninstall()
+    {
+        $result = $GLOBALS['db']->dropTable('friend');
+        if (Jaws_Error::IsError($result)) {
+            $gName  = _t('FRIENDS_NAME');
+            $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $gName);
+            $GLOBALS['app']->Session->PushLastResponse($errMsg, RESPONSE_ERROR);
+            return new Jaws_Error($errMsg, $gName);
+        }
+
+        //registry keys.
+        $this->gadget->DelRegistry('limit');
+
+        return true;
+    }
+
+    /**
+     * Upgrades the gadget
+     *
+     * @access  public
+     * @param   string  $old    Current version (in registry)
+     * @param   string  $new    New version (in the $gadgetInfo file)
+     * @return  mixed   True on Success or Jaws_Error on Failure
+     */
+    function Upgrade($old, $new)
+    {
+        $result = $this->installSchema('schema.xml', '', "$old.xml");
+        if (Jaws_Error::IsError($result)) {
+            return $result;
+        }
+
+        // Registry keys.
+
+        return true;
+    }
+
+}
