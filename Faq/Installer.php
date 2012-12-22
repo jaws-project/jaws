4ef14dadeff9bfb9e2a2fe54df1b962bd1ba0cc3
Ali Fazelzadeh <afz@dev-code.com>
2012-12-22 16:30:52 +0330

    [Faq]: separating install/uninstall/upgrade methods

---
+++ Faq/Installer.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Faq Installer
+ *
+ * @category    GadgetModel
+ * @package     Faq
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2012 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ */
+class Faq_Installer extends Jaws_Gadget_Installer
+{
+    /**
+     * Installs the gadget
+     *
+     * @access  public
+     * @return  mixed   True on successful installation, Jaws_Error otherwise
+     */
+    function Install()
+    {
+        $result = $this->installSchema('schema.xml');
+        if (Jaws_Error::IsError($result)) {
+            return $result;
+        }
+
+        $variables = array();
+        $variables['timestamp'] = $GLOBALS['db']->Date();
+
+        $result = $this->installSchema('insert.xml', $variables, 'schema.xml', true);
+        if (Jaws_Error::IsError($result)) {
+            return $result;
+        }
+
+        return true;
+    }
+
+    /**
+     * Uninstalls the gadget
+     *
+     * @access  public
+     * @return  mixed   True on Success or Jaws_Error on Failure
+     */
+    function Uninstall()
+    {
+        $tables = array('faq',
+                        'faq_category');
+        foreach ($tables as $table) {
+            $result = $GLOBALS['db']->dropTable($table);
+            if (Jaws_Error::IsError($result)) {
+                $gName  = _t('FAQ_NAME');
+                $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $gName);
+                $GLOBALS['app']->Session->PushLastResponse($errMsg, RESPONSE_ERROR);
+                return new Jaws_Error($errMsg, $gName);
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Upgrades the gadget
+     *
+     * @access  public
+     * @param   string  $old    Current version (in registry)
+     * @param   string  $new    New version (in the $gadgetInfo file)
+     * @return  mixed   True on Success or Jaws_Error on Failure
+     */
+    function Upgrade($old, $new)
+    {
+        if (version_compare($old, '0.8.0', '<')) {
+            $result = $this->installSchema('0.8.0.xml', '', "0.7.0.xml");
+            if (Jaws_Error::IsError($result)) {
+                return $result;
+            }
+        }
+
+        if (version_compare($old, '0.8.1', '<')) {
+            $result = $this->installSchema('0.8.1.xml', '', "0.8.0.xml");
+            if (Jaws_Error::IsError($result)) {
+                return $result;
+            }
+        }
+
+        $result = $this->installSchema('schema.xml', '', "0.8.1.xml");
+        if (Jaws_Error::IsError($result)) {
+            return $result;
+        }
+
+        // Registry keys.
+
+        return true;
+    }
+
+}
