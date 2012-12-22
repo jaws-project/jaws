f7719ce412351f7d28e94a592e06f53f114fd53d
Ali Fazelzadeh <afz@dev-code.com>
2012-12-22 17:25:00 +0330

    [Preferences]: separating install/uninstall/upgrade methods

--- Preferences/AdminModel.php
+++ Preferences/AdminModel.php
@@ -1,4 +1,5 @@
 <?php
+require_once JAWS_PATH . 'gadgets/Preferences/Model.php';
 /**
  * Preferences Gadget Model
  *
@@ -9,82 +10,8 @@
  * @copyright  2004-2012 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
-require_once JAWS_PATH . 'gadgets/Preferences/Model.php';
-
 class PreferencesAdminModel extends PreferencesModel
 {
-    /**
-     * Install the gadget
-     *
-     * @access  public
-     * @return  bool    true on successful installation, Jaws_Error otherwise
-     */
-    function InstallGadget()
-    {
-        $this->gadget->AddRegistry('display_theme',             'true');
-        $this->gadget->AddRegistry('display_editor',            'true');
-        $this->gadget->AddRegistry('display_language',          'true');
-        $this->gadget->AddRegistry('display_calendar_type',     'true');
-        $this->gadget->AddRegistry('display_calendar_language', 'true');
-        $this->gadget->AddRegistry('display_date_format',       'true');
-        $this->gadget->AddRegistry('display_timezone',          'true');
-
-        //enable cookie precedence
-        $this->gadget->SetRegistry('cookie_precedence', 'true', 'Settings');
-
-        return true;
-    }
-
-    /**
-     * Uninstalls the gadget
-     *
-     * @access  public
-     * @return  bool     Success/Failure (Jaws_Error)
-     */
-    function UninstallGadget()
-    {
-        // registry keys
-        $this->gadget->DelRegistry('display_theme');
-        $this->gadget->DelRegistry('display_editor');
-        $this->gadget->DelRegistry('display_language');
-        $this->gadget->DelRegistry('display_calendar_type');
-        $this->gadget->DelRegistry('display_calendar_language');
-        $this->gadget->DelRegistry('display_date_format');
-        $this->gadget->DelRegistry('display_timezone');
-
-        //disable cookie precedence
-        $this->gadget->SetRegistry('cookie_precedence', 'false', 'Settings');
-
-        return true;
-    }
-
-    /**
-     * Update the gadget
-     *
-     * @access  public
-     * @param   string  $old    Current version (in registry)
-     * @param   string  $new    New version (in the $gadgetInfo file)
-     * @return  bool     Success/Failure (Jaws_Error)
-     */
-    function UpdateGadget($old, $new)
-    {
-        // ACL keys
-        $GLOBALS['app']->ACL->NewKey('/ACL/gadgets/Preferences/UpdateProperties',   'true');
-        $GLOBALS['app']->ACL->DeleteKey('/ACL/gadgets/Preferences/ChangeSettings');
-
-        // Registry keys.
-        $this->gadget->AddRegistry('display_editor',            'true');
-        $this->gadget->AddRegistry('display_calendar_type',     'true');
-        $this->gadget->AddRegistry('display_calendar_language', 'true');
-        $this->gadget->AddRegistry('display_date_format',       'true');
-        $this->gadget->AddRegistry('display_timezone',          'true');
-
-        //enable cookie precedence
-        $this->gadget->SetRegistry('cookie_precedence', 'true', 'Settings');
-
-        return true;
-    }
-
     /**
      * Update preferences
      *
