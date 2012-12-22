40eedde036af6563e704967d8a91751f8d344959
Ali Fazelzadeh <afz@dev-code.com>
2012-12-22 17:27:28 +0330

    [Webcam]: separating install/uninstall/upgrade methods

--- Webcam/AdminModel.php
+++ Webcam/AdminModel.php
@@ -1,4 +1,5 @@
 <?php
+require_once JAWS_PATH . 'gadgets/Webcam/Model.php';
 /**
  * Webcam Gadget Admin
  *
@@ -9,71 +10,8 @@
  * @copyright  2004-2012 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
-require_once JAWS_PATH . 'gadgets/Webcam/Model.php';
-
 class WebcamAdminModel extends WebcamModel
 {
-    /**
-     * Installs the gadget
-     *
-     * @access  public
-     * @return  mixed   True on successful installation, Jaws_Error otherwise
-     */
-    function InstallGadget()
-    {
-        $result = $this->installSchema('schema.xml');
-        if (Jaws_Error::IsError($result)) {
-            return $result;
-        }
-
-        //Registry keys.
-        $this->gadget->AddRegistry('limit_random', '3');
-
-        return true;
-    }
-
-    /**
-     * Uninstalls the gadget
-     *
-     * @access  public
-     * @return  mixed   True on success, Jaws_Error otherwise
-     */
-    function UninstallGadget()
-    {
-        $result = $GLOBALS['db']->dropTable('webcam');
-        if (Jaws_Error::IsError($result)) {
-            $gName  = _t('WEBCAM_NAME');
-            $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $gName);
-            $GLOBALS['app']->Session->PushLastResponse($errMsg, RESPONSE_ERROR);
-            return new Jaws_Error($errMsg, $gName);
-        }
-
-        // registry keys
-        $this->gadget->DelRegistry('limit_random');
-
-        return true;
-    }
-
-    /**
-     * Updates the gadget
-     *
-     * @access  public
-     * @param   string  $old    Current version (in registry)
-     * @param   string  $new    New version (in the $gadgetInfo file)
-     * @return  mixed   True on success, Jaws_Error otherwise
-     */
-    function UpdateGadget($old, $new)
-    {
-        $result = $this->installSchema('schema.xml', '', "$old.xml");
-        if (Jaws_Error::IsError($result)) {
-            return $result;
-        }
-
-        // Registry keys
-
-        return true;
-    }
-
     /**
      * Inserts a new webcam
      *
---
+++ Webcam/Installer.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Webcam Installer
+ *
+ * @category    GadgetModel
+ * @package     Webcam
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2012 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ */
+class Webcam_Installer extends Jaws_Gadget_Installer
+{
+    /**
+     * Installs the gadget
+     *
+     * @access  public
+     * @return  mixed   True on successful installation, Jaws_Error otherwise
+     */
+    function Install()
+    {
+        $result = $this->installSchema('schema.xml');
+        if (Jaws_Error::IsError($result)) {
+            return $result;
+        }
+
+        //Registry keys.
+        $this->gadget->AddRegistry('limit_random', '3');
+
+        return true;
+    }
+
+    /**
+     * Uninstalls the gadget
+     *
+     * @access  public
+     * @return  mixed   True on success, Jaws_Error otherwise
+     */
+    function Uninstall()
+    {
+        $result = $GLOBALS['db']->dropTable('webcam');
+        if (Jaws_Error::IsError($result)) {
+            $gName  = _t('WEBCAM_NAME');
+            $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $gName);
+            $GLOBALS['app']->Session->PushLastResponse($errMsg, RESPONSE_ERROR);
+            return new Jaws_Error($errMsg, $gName);
+        }
+
+        // registry keys
+        $this->gadget->DelRegistry('limit_random');
+
+        return true;
+    }
+
+    /**
+     * Upgrades the gadget
+     *
+     * @access  public
+     * @param   string  $old    Current version (in registry)
+     * @param   string  $new    New version (in the $gadgetInfo file)
+     * @return  mixed   True on success, Jaws_Error otherwise
+     */
+    function Upgrade($old, $new)
+    {
+        $result = $this->installSchema('schema.xml', '', "$old.xml");
+        if (Jaws_Error::IsError($result)) {
+            return $result;
+        }
+
+        // Registry keys
+
+        return true;
+    }
+
+}
