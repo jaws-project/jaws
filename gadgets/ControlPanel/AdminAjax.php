3327d4fa3c3d10ad5df18462ff9d3ca8ae9fe24d
Ali Fazelzadeh <afz@dev-code.com>
2012-12-24 15:45:07 +0330

    [ControlPanel]: Applying new changes of Jaws Ajax classes

--- gadgets/ControlPanel/AdminAjax.php
+++ gadgets/ControlPanel/AdminAjax.php
@@ -8,9 +8,19 @@
  * @copyright  2012 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/lesser.html
  */
-class ControlPanel_AdminAjax extends Jaws_Gadget_Ajax
+class ControlPanel_AdminAjax extends Jaws_Gadget_HTML
 {
     /**
+     * Constructor
      *
+     * @access  public
+     * @param   object $gadget Jaws_Gadget object
+     * @return  void
      */
+    function ControlPanel_AdminAjax($gadget)
+    {
+        parent::Jaws_Gadget_HTML($gadget);
+        $this->_Model = $this->gadget->load('Model')->loadModel('AdminModel');
+    }
+
 }
