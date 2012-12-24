89d57db7069d0d8984a90431ae9c7b24776432d3
Ali Fazelzadeh <afz@dev-code.com>
2012-12-24 14:25:47 +0330

    [Settings]: Applying class name changes

--- gadgets/Settings/AdminModel.php
+++ gadgets/Settings/AdminModel.php
@@ -10,7 +10,7 @@
  * @copyright  2004-2012 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/lesser.html
  */
-class SettingsAdminModel extends Jaws_Gadget_Model
+class Settings_AdminModel extends Jaws_Gadget_Model
 {
     /**
      * Gets the available calendars
--- gadgets/Settings/Info.php
+++ gadgets/Settings/Info.php
@@ -9,7 +9,7 @@
  * @copyright  2004-2012 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/lesser.html
  */
-class SettingsInfo extends Jaws_Gadget
+class Settings_Info extends Jaws_Gadget
 {
     /**
      * Gadget version
