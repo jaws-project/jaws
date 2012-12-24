7625f3219b588096951bdcd125cb2f3661c6bd46
Ali Fazelzadeh <afz@dev-code.com>
2012-12-24 15:46:07 +0330

    [Search]: Applying new changes of Jaws Ajax classes

--- gadgets/Search/AdminAjax.php
+++ gadgets/Search/AdminAjax.php
@@ -8,8 +8,21 @@
  * @copyright   2005-2012 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/lesser.html
  */
-class Search_AdminAjax extends Jaws_Gadget_Ajax
+class Search_AdminAjax extends Jaws_Gadget_HTML
 {
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   object $gadget Jaws_Gadget object
+     * @return  void
+     */
+    function Search_AdminAjax($gadget)
+    {
+        parent::Jaws_Gadget_HTML($gadget);
+        $this->_Model = $this->gadget->load('Model')->loadModel('AdminModel');
+    }
+
     /**
      * Updates searchable gadgets
      *
