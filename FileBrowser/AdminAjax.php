66f1758a075c581d37724d102733a23e7199359d
Ali Fazelzadeh <afz@dev-code.com>
2013-01-01 02:18:17 +0330

    @2013

--- FileBrowser/AdminAjax.php
+++ FileBrowser/AdminAjax.php
@@ -5,7 +5,7 @@
  * @category   Ajax
  * @package    FileBrowser
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2010-2012 Jaws Development Group
+ * @copyright  2010-2013 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class FileBrowser_AdminAjax extends Jaws_Gadget_HTML
