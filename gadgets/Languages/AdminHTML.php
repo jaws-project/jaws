ba7cb89663c1e1cf77ee78d14f74a5e3f438d538
Ali Fazelzadeh <afz@dev-code.com>
2013-01-01 02:03:46 +0330

    @2013

--- gadgets/Languages/AdminHTML.php
+++ gadgets/Languages/AdminHTML.php
@@ -5,7 +5,7 @@
  * @category   GadgetAdmin
  * @package    Languages
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2007-2012 Jaws Development Group
+ * @copyright  2007-2013 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/lesser.html
  */
 class Languages_AdminHTML extends Jaws_Gadget_HTML
--- gadgets/Languages/AdminModel.php
+++ gadgets/Languages/AdminModel.php
@@ -5,7 +5,7 @@
  * @category   GadgetModel
  * @package    Languages
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2007-2012 Jaws Development Group
+ * @copyright  2007-2013 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/lesser.html
  */
 class Languages_AdminModel extends Jaws_Gadget_Model
