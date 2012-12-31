09af228527f2a3ac2bc32bc4db50036127d75a18
Ali Fazelzadeh <afz@dev-code.com>
2013-01-01 02:17:15 +0330

    @2013

--- Forums/Actions/Attachment.php
+++ Forums/Actions/Attachment.php
@@ -5,7 +5,7 @@
  * @category    Gadget
  * @package     Forums
  * @author      Ali Fazelzadeh <afz@php.net>
- * @copyright   2012 Jaws Development Group
+ * @copyright   2012-2013 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/gpl.html
  */
 class Forums_Actions_Attachment extends Forums_HTML
