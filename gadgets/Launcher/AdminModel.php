0864fa3dd3feaee055504d32a778f845e264224c
Ali Fazelzadeh <afz@dev-code.com>
2013-01-01 02:03:51 +0330

    @2013

--- gadgets/Launcher/AdminModel.php
+++ gadgets/Launcher/AdminModel.php
@@ -5,7 +5,7 @@
  * @category   GadgetModel
  * @package    Launcher
  * @author     Jonathan Hernandez <ion@suavizado.com>
- * @copyright  2006-2012 Jaws Development Group
+ * @copyright  2006-2013 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 require_once JAWS_PATH . 'gadgets/Launcher/Model.php';
