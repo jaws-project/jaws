bdcf3f5d104c1d2b87dba5654d3f33d73104d370
Ali Fazelzadeh <afz@dev-code.com>
2013-01-01 02:17:58 +0330

    @2013

--- RssReader/AdminModel.php
+++ RssReader/AdminModel.php
@@ -8,7 +8,7 @@ require_once JAWS_PATH . 'gadgets/RssReader/Model.php';
  * @author     Pablo Fischer <pablo@pablo.com.mx>
  * @author     Jonathan Hernandez <ion@suavizado.com>
  * @author     Ali Fazelzadeh  <afz@php.net>
- * @copyright  2005-2012 Jaws Development Group
+ * @copyright  2005-2013 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class RssReader_AdminModel extends RssReader_Model
