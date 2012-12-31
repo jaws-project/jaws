9961b38b95fcf0d87db93b71ec49c41e747ea589
Ali Fazelzadeh <afz@dev-code.com>
2013-01-01 02:18:52 +0330

    @2013

--- SysInfo/AdminModel.php
+++ SysInfo/AdminModel.php
@@ -6,7 +6,7 @@ require_once JAWS_PATH . 'gadgets/SysInfo/Model.php';
  * @category   GadgetModel
  * @package    SysInfo
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2008-2012 Jaws Development Group
+ * @copyright  2008-2013 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/lesser.html
  */
 class SysInfo_AdminModel extends SysInfo_Model
