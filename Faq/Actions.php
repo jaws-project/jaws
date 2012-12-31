63038ebf9842bb83bd8cd5495b2581ac023e9c60
Ali Fazelzadeh <afz@dev-code.com>
2013-01-01 02:19:07 +0330

    @2013

--- Faq/Actions.php
+++ Faq/Actions.php
@@ -5,7 +5,7 @@
  * @category    GadgetActions
  * @package     Faq
  * @author      Pablo Fischer <pablo@pablo.com.mx>
- * @copyright   2004-2012 Jaws Development Group
+ * @copyright   2004-2013 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/gpl.html
  */
 $actions = array();
