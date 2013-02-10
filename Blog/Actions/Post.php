11c8683ab8af5514b539e79a422fa31df18c5182
Ali Fazelzadeh <afz@dev-code.com>
2013-02-10 15:42:53 +0330

    [Blog]: Fixed php warning message

--- Blog/Actions/Post.php
+++ Blog/Actions/Post.php
@@ -69,7 +69,7 @@ class Blog_Actions_Post extends Blog_HTML
             if ($this->gadget->GetRegistry('pingback') == 'true') {
                 require_once JAWS_PATH . 'include/Jaws/Pingback.php';
                 $pback =& Jaws_PingBack::getInstance();
-                $pback->showHeaders($this->gadget->GetURLFor('Pingback', null, false, 'site_url'));
+                $pback->showHeaders($this->gadget->GetURLFor('Pingback', array(), false, 'site_url'));
             }
 
             $this->SetTitle($entry['title']);
