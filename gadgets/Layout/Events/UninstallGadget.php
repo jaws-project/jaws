112b0b50cb114f9bd8282b51e6bbf8a37f3a54e2
Ali Fazelzadeh <afz@dev-code.com>
2013-04-01 19:49:47 +0430

    [Layout]: Applying new changes in Jaws_Gadget_Event

--- gadgets/Layout/Events/UninstallGadget.php
+++ gadgets/Layout/Events/UninstallGadget.php
@@ -8,7 +8,7 @@
  * @copyright  2013 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/lesser.html
  */
-class Layout_Events_UninstallGadget extends Jaws_Gadget
+class Layout_Events_UninstallGadget extends Jaws_Gadget_Event
 {
     /**
      * Event execute method
