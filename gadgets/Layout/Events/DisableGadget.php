5fd80e1de8c670a18132af0041dd0e1e58f52973
Ali Fazelzadeh <afz@dev-code.com>
2013-04-08 09:01:33 +0430

    tail of last commit

---
+++ gadgets/Layout/Events/DisableGadget.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Layout DisableGadget event
+ *
+ * @category   Gadget
+ * @package    Layout
+ * @author     Ali Fazelzadeh <afz@php.net>
+ * @copyright  2013 Jaws Development Group
+ * @license    http://www.gnu.org/copyleft/lesser.html
+ */
+class Layout_Events_DisableGadget extends Jaws_Gadget_Event
+{
+    /**
+     * Event execute method
+     *
+     */
+    function Execute($gadget)
+    {
+        $lModel = $GLOBALS['app']->loadGadget('Layout', 'AdminModel');
+        $res = $lModel->PublishGadgetElements($gadget, false);
+        return $res;
+    }
+
+}
---
+++ gadgets/Layout/Events/EnableGadget.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Layout EnableGadget event
+ *
+ * @category   Gadget
+ * @package    Layout
+ * @author     Ali Fazelzadeh <afz@php.net>
+ * @copyright  2013 Jaws Development Group
+ * @license    http://www.gnu.org/copyleft/lesser.html
+ */
+class Layout_Events_EnableGadget extends Jaws_Gadget_Event
+{
+    /**
+     * Event execute method
+     *
+     */
+    function Execute($gadget)
+    {
+        $lModel = $GLOBALS['app']->loadGadget('Layout', 'AdminModel');
+        $res = $lModel->PublishGadgetElements($gadget, true);
+        return $res;
+    }
+
+}
