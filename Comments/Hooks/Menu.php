56c24ac281149e296c6ed26c8b8a74f007d0d5e1
Ali Fazelzadeh <afz@dev-code.com>
2013-04-12 23:23:19 +0430

    [Comments]: Added menu hook

---
+++ Comments/Hooks/Menu.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Comments - URL List gadget hook
+ *
+ * @category    GadgetHook
+ * @package     Comments
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2013 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/lesser.html
+ */
+class Comments_Hooks_Menu extends Jaws_Gadget_Hook
+{
+    /**
+     * Returns an array with all available items the Menu gadget can use
+     *
+     * @access  public
+     * @return  array   URLs array
+     */
+    function Execute()
+    {
+        $urls = array();
+        $urls[] = array(
+            'url'   => $this->gadget->GetURLFor('Comments'),
+            'title' => _t('COMMENTS_ACTIONS_COMMENTS')
+        );
+
+        return $urls;
+    }
+
+}
