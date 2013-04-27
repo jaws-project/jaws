a7c4e9f477bdaf7e8f1afd5c8878c89b407cea12
Ali Fazelzadeh <afz@dev-code.com>
2013-04-27 09:52:47 +0430

    [Contact]: Apply new changes of Jaws_Captcha

--- Contact/LayoutHTML.php
+++ Contact/LayoutHTML.php
@@ -93,7 +93,7 @@ class Contact_LayoutHTML extends Jaws_Gadget_HTML
         $mPolicy = $GLOBALS['app']->LoadGadget('Policy', 'Model');
         if (false !== $captcha = $mPolicy->LoadCaptcha()) {
             $tpl->SetBlock('contact/captcha');
-            $tpl->SetVariable('lbl_captcha', $captcha['label']);
+            $tpl->SetVariable('captcha_lbl', $captcha['label']);
             $tpl->SetVariable('captcha_key', $captcha['key']);
             $tpl->SetVariable('captcha', $captcha['captcha']);
             if (!empty($captcha['entry'])) {
