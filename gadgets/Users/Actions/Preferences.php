5cc48336981543ab99ab4ad6c24f11f6922bb4d3
Ali Fazelzadeh <afz@dev-code.com>
2013-05-13 11:25:13 +0430

    [Users]: Fixed typo

--- gadgets/Users/Actions/Preferences.php
+++ gadgets/Users/Actions/Preferences.php
@@ -2,12 +2,12 @@
 /**
  * Users Core Gadget
  *
- * @category   Gadget
- * @package    Users
- * @author     Jonathan Hernandez <ion@suavizado.com>
- * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2004-2013 Jaws Development Group
- * @license    http://www.gnu.org/copyleft/lesser.html
+ * @category    Gadget
+ * @package     Users
+ * @author      Jonathan Hernandez <ion@suavizado.com>
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2004-2013 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/lesser.html
  */
 class Users_Actions_Preferences extends Users_HTML
 {
@@ -145,8 +145,8 @@ class Users_Actions_Preferences extends Users_HTML
         } else {
             $GLOBALS['app']->Session->PushResponse(
                 $result->GetMessage(),
-                'Users.Preferences'
-                RESPONSE_ERROR,
+                'Users.Preferences',
+                RESPONSE_ERROR
             );
         }
 
