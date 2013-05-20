91e6040841dda0a7c333ff47fc3315d424c0ae2d
Ali Fazelzadeh <afz@dev-code.com>
2013-05-20 15:00:48 +0430

    [Users]: flexible template for Contacts action

--- gadgets/Users/Actions/Contacts.php
+++ gadgets/Users/Actions/Contacts.php
@@ -46,55 +46,14 @@ class Users_Actions_Contacts extends Users_HTML
         $tpl->SetVariable('base_script', BASE_SCRIPT);
         $tpl->SetVariable('update', _t('USERS_USERS_ACCOUNT_UPDATE'));
 
-        // country
-        $country =& Piwi::CreateWidget('Entry', 'country', $contacts['country']);
-        $country->SetStyle('width: 142px;');
-        $country->SetID('country');
         $tpl->SetVariable('lbl_country', _t('USERS_CONTACTS_COUNTRY'));
-        $tpl->SetVariable('country', $country->Get());
-
-        // city
-        $city =& Piwi::CreateWidget('Entry', 'city', $contacts['city']);
-        $city->SetStyle('width: 142px;');
-        $city->SetID('city');
         $tpl->SetVariable('lbl_city', _t('USERS_CONTACTS_CITY'));
-        $tpl->SetVariable('city', $city->Get());
-
-        // address
-        $address =& Piwi::CreateWidget('TextArea', 'address', $contacts['address']);
-        $address->SetID('address');
-        $address->SetRows(4);
-        $address->SetColumns(34);
         $tpl->SetVariable('lbl_address', _t('USERS_CONTACTS_ADDRESS'));
-        $tpl->SetVariable('address', $address->Get());
-
-        // postal_code
-        $postalCode =& Piwi::CreateWidget('Entry', 'postal_code', $contacts['postal_code']);
-        $postalCode->SetStyle('width: 142px;');
-        $postalCode->SetID('postal_code');
         $tpl->SetVariable('lbl_postal_code', _t('USERS_CONTACTS_POSTAL_CODE'));
-        $tpl->SetVariable('postal_code', $postalCode->Get());
-
-        // phone_number
-        $phoneNumber =& Piwi::CreateWidget('Entry', 'phone_number', $contacts['phone_number']);
-        $phoneNumber->SetStyle('width: 142px;');
-        $phoneNumber->SetID('phone_number');
         $tpl->SetVariable('lbl_phone_number', _t('USERS_CONTACTS_PHONE_NUMBER'));
-        $tpl->SetVariable('phone_number', $phoneNumber->Get());
-
-        // mobile_number
-        $mobileNumber =& Piwi::CreateWidget('Entry', 'mobile_number', $contacts['mobile_number']);
-        $mobileNumber->SetStyle('width: 142px;');
-        $mobileNumber->SetID('mobile_number');
         $tpl->SetVariable('lbl_mobile_number', _t('USERS_CONTACTS_MOBILE_NUMBER'));
-        $tpl->SetVariable('mobile_number', $mobileNumber->Get());
-
-        // fax_number
-        $faxNumber =& Piwi::CreateWidget('Entry', 'fax_number', $contacts['fax_number']);
-        $faxNumber->SetStyle('width: 142px;');
-        $faxNumber->SetID('fax_number');
         $tpl->SetVariable('lbl_fax_number', _t('USERS_CONTACTS_FAX_NUMBER'));
-        $tpl->SetVariable('fax_number', $faxNumber->Get());
+        $tpl->SetVariablesArray($contacts);
 
         if (!empty($response)) {
             $tpl->SetBlock('contacts/response');
