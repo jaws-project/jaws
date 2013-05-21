667ae039b8a454c22d7034f83a7c66cf5e750766
Ali Fazelzadeh <afz@dev-code.com>
2013-05-21 22:28:59 +0430

    tail of last commit

---
+++ gadgets/ControlPanel/Actions/Admin/Backup.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * ControlPanel Core Gadget Admin
+ *
+ * @category    GadgetAdmin
+ * @package     ControlPanel
+ * @author      Jonathan Hernandez <ion@suavizado.com>
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2004-2013 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/lesser.html
+ */
+class ControlPanel_Actions_Admin_Backup extends Jaws_Gadget_HTML
+{
+    /**
+     * Returns downloadable backup file
+     *
+     * @access  public
+     * @return void
+     */
+    function Backup()
+    {
+        $this->gadget->CheckPermission('Backup');
+        $tmpDir = sys_get_temp_dir();
+        $domain = preg_replace("/^(www.)|(:{$_SERVER['SERVER_PORT']})$|[^a-z0-9-.]/", '', strtolower($_SERVER['HTTP_HOST']));
+        $nameArchive = $domain . '-' . date('Y-m-d') . '.tar.gz';
+        $pathArchive = $tmpDir . DIRECTORY_SEPARATOR . $nameArchive;
+
+        //Dump database data
+        $dbFileName = 'dbdump.xml';
+        $dbFilePath = $tmpDir . DIRECTORY_SEPARATOR . $dbFileName;
+        $GLOBALS['db']->Dump($dbFilePath);
+
+        $files = array();
+        require_once PEAR_PATH. "File/Archive.php"; 
+        $files[] = File_Archive::read(JAWS_DATA);
+        $files[] = File_Archive::read($dbFilePath , $dbFileName);
+        File_Archive::extract($files, File_Archive::toArchive($pathArchive, File_Archive::toFiles()));
+        Jaws_Utils::Delete($dbFilePath);
+
+        // browser must download file from server instead of cache
+        header("Expires: 0");
+        header("Pragma: public");
+        header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+        // force download dialog
+        header("Content-Type: application/force-download");
+        // set data type, size and filename
+        header("Content-Disposition: attachment; filename=\"$nameArchive\"");
+        header("Content-Transfer-Encoding: binary");
+        header('Content-Length: '.@filesize($pathArchive));
+        @readfile($pathArchive);
+        Jaws_Utils::Delete($pathArchive);
+    }
+
+}
---
+++ gadgets/ControlPanel/Actions/Admin/Login.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * ControlPanel Core Gadget Admin
+ *
+ * @category    GadgetAdmin
+ * @package     ControlPanel
+ * @author      Jonathan Hernandez <ion@suavizado.com>
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2004-2013 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/lesser.html
+ */
+class ControlPanel_Actions_Admin_Login extends Jaws_Gadget_HTML
+{
+    /**
+     * Get HTML login form
+     *
+     * @access  public
+     * @param   string  $message If a message is needed
+     * @return  string  XHTML template of the login form
+     */
+    function LoginBox($message = '')
+    {
+        $use_crypt = $this->gadget->registry->fetch('crypt_enabled', 'Policy') == 'true';
+        if ($use_crypt) {
+            require_once JAWS_PATH . 'include/Jaws/Crypt.php';
+            $JCrypt = new Jaws_Crypt();
+            $use_crypt = $JCrypt->Init();
+        }
+
+        // Init layout
+        $GLOBALS['app']->Layout->Load('gadgets/ControlPanel/templates', 'LoginBox.html');
+        $ltpl =& $GLOBALS['app']->Layout->_Template;
+        $GLOBALS['app']->Layout->AddHeadLink('gadgets/ControlPanel/resources/style.css');
+        $ltpl->SetVariable('admin_script', BASE_SCRIPT);
+        $ltpl->SetVariable('control-panel', _t('CONTROLPANEL_NAME'));
+
+        $request =& Jaws_Request::getInstance();
+        $reqpost = $request->get(array('username', 'authtype', 'remember', 'usecrypt', 'redirect_to'), 'post');
+        if (is_null($reqpost['authtype'])) {
+            $reqpost['authtype'] = $request->get('authtype', 'get');
+        }
+
+        // referrer page link
+        $reqURL = Jaws_Utils::getRequestURL();
+        $reqURL.= ($reqURL == BASE_SCRIPT)? '?checksess' : '&checksess';
+        $redirect_to = is_null($reqpost['redirect_to'])? bin2hex($reqURL) : $reqpost['redirect_to'];
+        $ltpl->SetVariable('redirect_to', $redirect_to);
+
+        if ($use_crypt) {
+            $GLOBALS['app']->Layout->AddScriptLink('libraries/js/rsa.lib.js');
+            $ltpl->SetBlock('layout/onsubmit');
+            $ltpl->ParseBlock('layout/onsubmit');
+            $ltpl->SetBlock('layout/encryption');
+            $ltpl->SetVariable('modulus',  $JCrypt->math->bin2int($JCrypt->pub_key->getModulus()));
+            $ltpl->SetVariable('exponent', $JCrypt->math->bin2int($JCrypt->pub_key->getExponent()));
+            $ltpl->ParseBlock('layout/encryption');
+
+            // usecrypt
+            $ltpl->SetBlock('layout/usecrypt');
+            $ltpl->SetVariable('lbl_usecrypt', _t('GLOBAL_LOGIN_SECURE'));
+            if (empty($reqpost['username']) || !empty($reqpost['usecrypt'])) {
+                $ltpl->SetBlock('layout/usecrypt/selected');
+                $ltpl->ParseBlock('layout/usecrypt/selected');
+            }
+            $ltpl->ParseBlock('layout/usecrypt');
+        }
+
+        $ltpl->SetVariable('legend_title', _t('CONTROLPANEL_LOGIN_TITLE'));
+        $ltpl->SetVariable('lbl_username', _t('GLOBAL_USERNAME'));
+        $ltpl->SetVariable('username', $reqpost['username']);
+        $ltpl->SetVariable('lbl_password', _t('GLOBAL_PASSWORD'));
+
+        $authtype = $this->gadget->registry->fetch('authtype', 'Users');
+        if (!is_null($reqpost['authtype']) || $authtype !== 'Default') {
+            $authtype = is_null($reqpost['authtype'])? $authtype : $reqpost['authtype'];
+            $ltpl->SetBlock('layout/authtype');
+            $ltpl->SetVariable('lbl_authtype', _t('GLOBAL_AUTHTYPE'));
+            foreach ($GLOBALS['app']->GetAuthTypes() as $method) {
+                $ltpl->SetBlock('layout/authtype/item');
+                $ltpl->SetVariable('method', $method);
+                if ($method == $authtype) {
+                    $ltpl->SetVariable('selected', 'selected="selected"');
+                } else {
+                    $ltpl->SetVariable('selected', '');
+                }
+                $ltpl->ParseBlock('layout/authtype/item');
+            }
+            $ltpl->ParseBlock('layout/authtype');
+        }
+
+        // remember
+        $ltpl->SetBlock('layout/remember');
+        $ltpl->SetVariable('lbl_remember', _t('GLOBAL_REMEMBER_ME'));
+        if (!empty($reqpost['remember'])) {
+            $ltpl->SetBlock('layout/remember/selected');
+            $ltpl->ParseBlock('layout/remember/selected');
+        }
+        $ltpl->ParseBlock('layout/remember');
+
+        //captcha
+        $mPolicy = $GLOBALS['app']->LoadGadget('Policy', 'HTML');
+        $mPolicy->loadCaptcha($ltpl, 'layout', 'login');
+
+        $ltpl->SetVariable('login', _t('GLOBAL_LOGIN'));
+        $ltpl->SetVariable('back', _t('CONTROLPANEL_LOGIN_BACK_TO_SITE'));
+
+        $message = is_null($request->get('checksess'))? $message : _t('GLOBAL_ERROR_SESSION_NOTFOUND');
+        if (!empty($message)) {
+            $ltpl->SetBlock('layout/message');
+            $ltpl->SetVariable('message', $message);
+            $ltpl->ParseBlock('layout/message');
+        }
+
+        return $GLOBALS['app']->Layout->Get();
+    }
+
+    /**
+     * Terminates Control Panel session and redirects to website
+     *
+     * @access  public
+     */
+    function Logout()
+    {
+        $GLOBALS['app']->Session->Logout();
+        Jaws_Header::Location(BASE_SCRIPT);
+    }
+
+}
