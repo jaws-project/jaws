2da6e4148e33dfd06bde1e3ce351b334c997c753
Ali Fazelzadeh <afz@dev-code.com>
2013-05-23 00:52:32 +0430

    [ControlPanel]: try to fetch Jaws version from remote server

--- gadgets/ControlPanel/Actions/Admin/JawsVersion.php
+++ gadgets/ControlPanel/Actions/Admin/JawsVersion.php
@@ -5,7 +5,6 @@
  * @category    GadgetAdmin
  * @package     ControlPanel
  * @author      Ali Fazelzadeh <afz@php.net>
- * @author      Mohsen Khahani <mkhahani@gmail.com>
  * @copyright   2013 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/lesser.html
  */
@@ -19,12 +18,28 @@ class ControlPanel_Actions_Admin_JawsVersion extends Jaws_Gadget_HTML
      */
     function JawsVersion()
     {
-        // Set Headers
-        header('Content-Type: application/json; charset=utf-8');
-        header('Cache-Control: no-cache, must-revalidate');
-        header('Pragma: no-cache');
+        $options = array();
+        $timeout = (int)$this->gadget->registry->fetch('connection_timeout', 'Settings');
+        $options['timeout'] = $timeout;
+        if ($this->gadget->registry->fetch('proxy_enabled', 'Settings') == 'true') {
+            if ($this->gadget->registry->fetch('proxy_auth', 'Settings') == 'true') {
+                $options['proxy_user'] = $this->gadget->registry->fetch('proxy_user', 'Settings');
+                $options['proxy_pass'] = $this->gadget->registry->fetch('proxy_pass', 'Settings');
+            }
+            $options['proxy_host'] = $this->gadget->registry->fetch('proxy_host', 'Settings');
+            $options['proxy_port'] = $this->gadget->registry->fetch('proxy_port', 'Settings');
+        }
 
-        return Jaws_UTF8::json_encode(JAWS_VERSION);
+        $jaws_version = '-';
+        require_once PEAR_PATH. 'HTTP/Request.php';
+        $httpRequest = new HTTP_Request('http://localhost/jaws/?gadget=Components&action=Version&type=0', $options);
+        $httpRequest->setMethod(HTTP_REQUEST_METHOD_GET);
+        $resRequest  = $httpRequest->sendRequest();
+        if (!PEAR::isError($resRequest) && $httpRequest->getResponseCode() == 200) {
+            $jaws_version = trim($httpRequest->getResponseBody());
+        }
+
+        return $jaws_version;
     }
 
 }
