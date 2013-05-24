3c5fb912b142b29102a89504df85868e1197e723
Mojtaba Ebrahimi <irangame@gmail.com>
2013-05-24 18:36:58 +0430

    [shoutbox]: Added settings action in admin side

--- Shoutbox/AdminActions.php
+++ Shoutbox/AdminActions.php
@@ -5,7 +5,12 @@
  * @category    GadgetActions
  * @package     Shoutbox
  * @author      Ali Fazelzadeh <afz@php.net>
+ * @author      Mojtaba Ebrahimi <ebrahimi@zehneziba.ir>
  * @copyright   2012-2013 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/gpl.html
  */
 $actions = array();
+
+$actions['Settings'] = array(
+    'normal' => true,
+);
--- Shoutbox/AdminHTML.php
+++ Shoutbox/AdminHTML.php
@@ -39,6 +39,78 @@ class Shoutbox_AdminHTML extends Jaws_Gadget_HTML
 
 
     /**
+     * Displays shoutbox Settings
+     *
+     * @access  public
+     * @return  string  XHTML template content
+     */
+    function Settings()
+    {
+        $this->AjaxMe('script.js');
+        $tpl = $this->gadget->loadTemplate('Settings.html');
+        $tpl->SetBlock('settings');
+
+        $tpl->SetVariable('menubar',  $this->MenuBar('Settings'));
+
+        ///Config properties
+        if ($this->gadget->GetPermission('UpdateProperties')) {
+            $form =& Piwi::CreateWidget('Form', BASE_SCRIPT, 'POST');
+            $form->Add(Piwi::CreateWidget('HiddenEntry', 'gadget', 'Shoutbox'));
+            $form->Add(Piwi::CreateWidget('HiddenEntry', 'action', 'UpdateProperties'));
+
+            include_once JAWS_PATH . 'include/Jaws/Widgets/FieldSet.php';
+            $fieldset = new Jaws_Widgets_FieldSet(_t('SHOUTBOX_SETTINGS'));
+            $fieldset->SetDirection('vertical');
+
+            //
+            $limitcombo =& Piwi::CreateWidget('Combo', 'limit_entries');
+            $limitcombo->SetTitle(_t('SHOUTBOX_ENTRY_LIMIT'));
+            for ($i = 1; $i <= 20; ++$i) {
+                $limitcombo->AddOption($i, $i);
+            }
+            $limit = $this->gadget->registry->fetch('limit');
+            if (Jaws_Error::IsError($limit)) {
+                $limit = 10;
+            }
+            $limitcombo->SetDefault($limit);
+            $fieldset->Add($limitcombo);
+
+            // max length
+            $max_lencombo =& Piwi::CreateWidget('Combo', 'max_strlen');
+            $max_lencombo->SetTitle(_t('SHOUTBOX_ENTRY_MAX_LEN'));
+            for ($i = 1; $i <= 10; ++$i) {
+                $max_lencombo->AddOption($i*25, $i*25);
+            }
+            $max_strlen = $this->gadget->registry->fetch('max_strlen');
+            if (Jaws_Error::IsError($max_strlen)) {
+                $max_strlen = 125;
+            }
+            $max_lencombo->SetDefault($max_strlen);
+            $fieldset->Add($max_lencombo);
+
+            //Anonymous post authority
+            $authority =& Piwi::CreateWidget('Combo', 'authority');
+            $authority->SetTitle(_t('SHOUTBOX_ANON_POST_AUTHORITY'));
+            $authority->AddOption(_t('GLOBAL_DISABLED'), 'false');
+            $authority->AddOption(_t('GLOBAL_ENABLED'),  'true');
+            $anon_authority = $this->gadget->registry->fetch('anon_post_authority');
+            $authority->SetDefault($anon_authority == 'true'? 'true' : 'false');
+            $fieldset->Add($authority);
+
+            $form->Add($fieldset);
+            $submit =& Piwi::CreateWidget('Button', 'saveproperties', _t('GLOBAL_SAVE'), STOCK_SAVE);
+            $submit->AddEvent(ON_CLICK, 'javascript: updateProperties(this.form);');
+
+            $form->Add($submit);
+            $tpl->SetVariable('config_form', $form->Get());
+        }
+
+        $tpl->ParseBlock('settings');
+        return $tpl->Get();
+
+    }
+
+    /**
      * Prepares the shoutbox menubar
      *
      * @access  public
@@ -56,7 +128,7 @@ class Shoutbox_AdminHTML extends Jaws_Gadget_HTML
         $menubar = new Jaws_Widgets_Menubar();
         $menubar->AddOption(
             'Comments',
-            _t('COMMENTS_NAME'),
+            _t('SHOUTBOX_NAME'),
             BASE_SCRIPT . '?gadget=Shoutbox&amp;action=Admin');
         if ($this->gadget->GetPermission('Settings')) {
             $menubar->AddOption(
