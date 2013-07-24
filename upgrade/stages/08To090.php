81ef59856386871cb61823a8809cdcd760bb41ab
Ali Fazelzadeh <afz@php.net>
2013-07-24 16:29:43 +0430

    preparing registry upgrade

--- upgrade/stages/08To090.php
+++ upgrade/stages/08To090.php
@@ -47,82 +47,126 @@ class Upgrader_08To090 extends JawsUpgraderStage
             return new Jaws_Error(_t('UPGRADE_DB_RESPONSE_CONNECT_FAILED'), 0, JAWS_ERROR_WARNING);
         }
 
-        _log(JAWS_LOG_DEBUG,"delete all record of session table");
-        $sql = 'DELETE FROM [[session]]';
-        $res = $GLOBALS['db']->query($sql);
-        if (Jaws_Error::IsError($res)) {
-            return $res;
+        // upgrade core database schema
+        $old_schema = JAWS_PATH . 'upgrade/schema/0.8.18.xml';
+        $new_schema = JAWS_PATH . 'upgrade/schema/schema-middle.xml';
+        if (!file_exists($old_schema)) {
+            return new Jaws_Error(_t('GLOBAL_ERROR_SQLFILE_NOT_EXISTS', '0.8.18.xml'),0 , JAWS_ERROR_ERROR);
         }
 
-        // Create application
-        include_once JAWS_PATH . 'include/Jaws.php';
-        $GLOBALS['app'] = new Jaws();
-        $GLOBALS['app']->loadObject('Jaws_ACL', 'ACL');
-        $GLOBALS['app']->loadObject('Jaws_Registry', 'Registry');
-        $GLOBALS['app']->loadObject('Jaws_Translate', 'Translate');
-        $GLOBALS['app']->Registry->Init();
-
-        // This is needed for most gadgets
-        require_once JAWS_PATH . 'include/Jaws/Gadget.php';
-        require_once JAWS_PATH . 'include/Jaws/Template.php';
-        require_once JAWS_PATH . 'include/Jaws/Header.php';
-        $GLOBALS['app']->loadObject('Jaws_URLMapping', 'Map');
-
-        // Input datas
-        $timestamp = $GLOBALS['db']->Date();
-
-        // Registry keys
-        $plugins = $GLOBALS['app']->Registry->fetch('/plugins/parse_text/enabled_items');
-        $GLOBALS['app']->Registry->update('/plugins/parse_text/enabled_items', '');
-        $GLOBALS['app']->Registry->NewKey('/plugins/parse_text/admin_enabled_items', $plugins);
-        $GLOBALS['app']->Registry->NewKey('/config/global_website', 'true');
-        $GLOBALS['app']->Registry->DeleteKey('/config/frontend_ajaxed');
-        $GLOBALS['app']->Registry->DeleteKey('/gadgets/allowurl_items');
-        $GLOBALS['app']->Registry->update('/version', JAWS_VERSION);
-        $GLOBALS['app']->Registry->update('/last_update', $timestamp);
-
-        // Commit the changes so they get saved
-        $GLOBALS['app']->Registry->commit('core');
-
-        // Trying to add missed acl keys
-        _log(JAWS_LOG_DEBUG,"trying to add missed acl keys");
-        $igadgets = $GLOBALS['app']->Registry->get('/gadgets/enabled_items');
-        $igadgets.= ','. $GLOBALS['app']->Registry->get('/gadgets/core_items');
-        $igadgets = array_filter(array_map('trim', explode(',', $igadgets)));
-        foreach ($igadgets as $ig) {
-            $GLOBALS['app']->ACL->NewKey("/ACL/gadgets/$ig/default", 'true');
-            $GLOBALS['app']->ACL->NewKey("/ACL/gadgets/$ig/default_admin", 'false');
+        if (!file_exists($new_schema)) {
+            return new Jaws_Error(_t('GLOBAL_ERROR_SQLFILE_NOT_EXISTS', 'schema-middle.xml'),0 , JAWS_ERROR_ERROR);
         }
 
-        // Installing core gadgets
-        $gadgets = array('UrlMapper', 'Layout', 'Users');
-        foreach ($gadgets as $gadget) {
-            $objGadget = $GLOBALS['app']->LoadGadget($gadget, 'Info');
-            if (Jaws_Error::IsError($objGadget)) {
-                _log(JAWS_LOG_DEBUG,"There was a problem installing core gadget: ".$gadget);
-                return $objGadget;
-            }
-
-            $result = $objGadget->UpdateGadget();
-            if (Jaws_Error::IsError($result)) {
-                _log(JAWS_LOG_DEBUG,"There was a problem upgrading core gadget: $gadget");
-                return $result;
+        _log(JAWS_LOG_DEBUG,"Upgrading core schema");
+        $result = $GLOBALS['db']->installSchema($new_schema, '', $old_schema);
+        if (Jaws_Error::isError($result)) {
+            _log(JAWS_LOG_WARNING, $result->getMessage());
+            if ($result->getCode() !== MDB2_ERROR_ALREADY_EXISTS) {
+                return new Jaws_Error($result->getMessage(), 0, JAWS_ERROR_ERROR);
             }
         }
 
-        _log(JAWS_LOG_DEBUG,"Cleaning previous registry cache data files - step 0.8->0.9.0");
-        //Make sure user don't have any data/cache/registry stuff
-        $path = JAWS_DATA . 'cache/registry';
-        if (!Jaws_Utils::Delete($path, false)) {
-            _log(JAWS_LOG_DEBUG,"Can't delete $path");
+        // convert registry key name to new format
+        $sql = 'SELECT [old_key_name], [key_value] FROM [[registry]]';
+        $keys = $GLOBALS['db']->queryAll($sql, array(), null, MDB2_FETCHMODE_DEFAULT, true);
+        if (Jaws_Error::isError($keys)) {
+            _log(JAWS_LOG_WARNING, $keys->getMessage());
+            return new Jaws_Error($keys->getMessage(), 0, JAWS_ERROR_ERROR);
         }
 
-        _log(JAWS_LOG_DEBUG,"Cleaning previous acl cache data files - step 0.8->0.9.0");
-        //Make sure user don't have any data/cache/acl stuff
-        $path = JAWS_DATA . 'cache/acl';
-        if (!Jaws_Utils::Delete($path, false)) {
-            _log(JAWS_LOG_DEBUG,"Can't delete $path");
+        $keys['/gadgets/enabled_items'] = $keys['/gadgets/core_items']. $keys['/gadgets/enabled_items']. ',';
+        unset($keys['/gadgets/allowurl_items']);
+        
+        $sql = '
+            UPDATE [[registry]] SET
+                [component] = {component},
+                [key_name2] = {new_key_name},
+                [key_value] = {new_key_value}
+            WHERE [old_key_name] = {old_key_name}';
+        $params = array();
+        foreach ($keys as $key => $value) {
+            $params['old_key_name'] = $key;
+            $key = trim($key, '/');
+            $key = explode('/', $key);
+            switch ($key[0]) {
+                case 'config':
+                case 'network':
+                    $component = 'Settings';
+                    $new_key_name = $key[1];
+                    break;
+
+                case 'policy':
+                    $component = 'Policy';
+                    $new_key_name = $key[1];
+                    break;
+
+                case 'map':
+                    $component = 'UrlMapper';
+                    $new_key_name = $key[1];
+                    break;
+
+                case 'crypt':
+                    $component = 'Policy';
+                    $new_key_name = 'crypt_'. $key[1];
+                    break;
+
+                case 'gadgets':
+                    switch ($key[1]) {
+                        case 'enabled_items':
+                            $component = '';
+                            $new_key_name = 'gadgets_installed_items';
+                            break;
+
+                        case 'core_items':
+                            $component = '';
+                            $new_key_name = 'gadgets_disabled_items';
+                            $value = ',';
+                            break;
+
+                        case 'autoload_items':
+                            $component = '';
+                            $new_key_name = 'gadgets_autoload_items';
+                            $value.= ',';
+                            break;
+
+                        default:
+                            $component = $key[1];
+                            $new_key_name = $key[2];
+                    }
+                    break;
+
+                case 'plugins':
+                    switch ($key[1]) {
+                        case 'parse_text':
+                            $component = '';
+                            $new_key_name = 'plugins_installed_items';
+                            $value.= ',';
+                            break;
+
+                        default:
+                            $component = $key[1];
+                            $new_key_name = $key[2];
+                    }
+                    break;
+
+                default:
+                    $component = '';
+                    $new_key_name = $key[0];
+            }
+
+            $params['component']     = $component;
+            $params['new_key_name']  = $new_key_name;
+            $params['new_key_value'] = $value;
+            $res = $GLOBALS['db']->query($sql, $params);
+            if (Jaws_Error::IsError($res)) {
+                _log(JAWS_LOG_WARNING, $res->getMessage());
+                return new Jaws_Error($res->getMessage(), 0, JAWS_ERROR_ERROR);
+            }
+
         }
+
+        // convert acl key name to new format
 
         return true;
     }
