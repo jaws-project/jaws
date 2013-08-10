795d564cb02ff3a72afc2d9b69d85e66631dec2e
Ali Fazelzadeh <afz@php.net>
2013-08-10 09:33:27 +0430

    [Jaws_HTTPRequest]: Added get/post methods

--- include/Jaws/HTTPRequest.php
+++ include/Jaws/HTTPRequest.php
@@ -1,4 +1,5 @@
 <?php
+require_once PEAR_PATH. 'HTTP/Request.php';
 /**
  * Class that deals like a wrapper between Jaws and pear/HTTP_Request
  *
@@ -15,6 +16,12 @@ class Jaws_HTTPRequest
      * @var array   $options    The request options
      */
     var $options = array();
+
+    /**
+     * @access  public
+     * @var     string   $user_agent    User Agent
+     */
+    var $user_agent = 'Jaws HTTPRequest class (http://jaws-project.com)';
 
     /**
      * Constructor
@@ -37,6 +44,65 @@ class Jaws_HTTPRequest
 
         // merge default and passed options
         $this->options = array_merge($this->options, $options);
+    }
+
+    /**
+     * Gets the URL content
+     *
+     * @access  public
+     * @param   string  $url        URL address
+     * @param   string  $response   Response body
+     * @return  mixed   Response code on success, otherwise Jaws_Error
+     */
+    function get($url, &$response)
+    {
+        $httpRequest = new HTTP_Request($url, $this->options);
+        $httpRequest->addHeader('User-Agent', $this->user_agent);
+        $httpRequest->setMethod(HTTP_REQUEST_METHOD_GET);
+        $result = $httpRequest->sendRequest();
+        if (PEAR::isError($result)) {
+            return Jaws_Error::raiseError(
+                $result->getMessage(),
+                $result->getCode(),
+                JAWS_ERROR_ERROR,
+                1
+            );
+        }
+
+        $response = $httpRequest->getResponseBody();
+        return $httpRequest->getResponseCode();
+    }
+
+    /**
+     * Posts data to the URL
+     *
+     * @access  public
+     * @param   string  $url    URL address
+     * @param   array   $params Associated name/data values
+     * @return  mixed   Response code on success, otherwise Jaws_Error
+     */
+    function post($url, $params = array(), &$response)
+    {
+        $httpRequest = new HTTP_Request($url, $this->options);
+        $httpRequest->addHeader('User-Agent', $this->user_agent);
+        $httpRequest->setMethod(HTTP_REQUEST_METHOD_POST);
+        // add post data
+        foreach($params as $key => $data) {
+            $httpRequest->addPostData($key, urlencode($data));
+        }
+
+        $result = $httpRequest->sendRequest();
+        if (PEAR::isError($result)) {
+            return Jaws_Error::raiseError(
+                $result->getMessage(),
+                $result->getCode(),
+                JAWS_ERROR_ERROR,
+                1
+            );
+        }
+
+        $response = $httpRequest->getResponseBody();
+        return $httpRequest->getResponseCode();
     }
 
 }
