360dd995191c061ac65b070c166fabf3190bbfb5
Ali Fazelzadeh <afz@php.net>
2013-08-15 09:34:36 +0430

    [Upgrade]: Fixed an issue during upgrade

--- upgrade/stages/08To0902.php
+++ upgrade/stages/08To0902.php
@@ -43,52 +43,57 @@ class Upgrader_08To0902 extends JawsUpgraderStage
         require_once JAWS_PATH . 'include/Jaws/DB.php';
         $GLOBALS['db'] = new Jaws_DB($_SESSION['upgrade']['Database']);
         if (Jaws_Error::IsError($GLOBALS['db'])) {
-            _log(JAWS_LOG_DEBUG,"There was a problem connecting to the database, please check the details and try again");
-            return new Jaws_Error(_t('UPGRADE_DB_RESPONSE_CONNECT_FAILED'), 0, JAWS_ERROR_WARNING);
+            return Jaws_Error::raiseError(
+                _t('UPGRADE_DB_RESPONSE_CONNECT_FAILED'),
+                0,
+                JAWS_ERROR_WARNING
+            );
         }
 
         // upgrade core database schema
         $old_schema = JAWS_PATH . 'upgrade/schema/0.9.0.1.xml';
         $new_schema = JAWS_PATH . 'upgrade/schema/0.9.0.2.xml';
         if (!file_exists($old_schema)) {
-            return new Jaws_Error(_t('GLOBAL_ERROR_SQLFILE_NOT_EXISTS', '0.9.0.1.xml'),0 , JAWS_ERROR_ERROR);
+            return Jaws_Error::raiseError(
+                _t('GLOBAL_ERROR_SQLFILE_NOT_EXISTS', '0.9.0.1.xml'),
+                0,
+                JAWS_ERROR_ERROR
+            );
         }
 
         if (!file_exists($new_schema)) {
-            return new Jaws_Error(_t('GLOBAL_ERROR_SQLFILE_NOT_EXISTS', '0.9.0.2.xml'),0 , JAWS_ERROR_ERROR);
+            return Jaws_Error::raiseError(
+                _t('GLOBAL_ERROR_SQLFILE_NOT_EXISTS', '0.9.0.2.xml'),
+                0,
+                JAWS_ERROR_ERROR
+            );
         }
 
         _log(JAWS_LOG_DEBUG,"Upgrading core schema");
         $result = $GLOBALS['db']->installSchema($new_schema, '', $old_schema);
         if (Jaws_Error::isError($result)) {
-            _log(JAWS_LOG_ERROR, $result->getMessage());
             if ($result->getCode() !== MDB2_ERROR_ALREADY_EXISTS) {
-                return new Jaws_Error($result->getMessage(), 0, JAWS_ERROR_ERROR);
+                return Jaws_Error::raiseError($result->getMessage(), 0, JAWS_ERROR_ERROR);
             }
         }
 
         // convert acl key name to new format
-        $sql = 'SELECT [old_key_name], [old_key_value] FROM [[acl]]';
+        $sql = 'SELECT [key_name], [key_value] FROM [[old_acl]]';
         $keys = $GLOBALS['db']->queryAll($sql, array(), null, MDB2_FETCHMODE_DEFAULT, true);
         if (Jaws_Error::isError($keys)) {
-            _log(JAWS_LOG_WARNING, $keys->getMessage());
-            return new Jaws_Error($keys->getMessage(), 0, JAWS_ERROR_ERROR);
+            return Jaws_Error::raiseError($keys->getMessage(), 0, JAWS_ERROR_ERROR);
         }
 
-        unset($keys['/last_update']);
-        $aclSQL = '
-            UPDATE [[acl]] SET
-                [component] = {component},
-                [new_key_name] = {new_key_name},
-                [key_subkey] = {key_subkey},
-                [key_value2] = {new_key_value},
-                [user] = {user},
-                [group] = {group}
-            WHERE [old_key_name] = {old_key_name}';
         $usrSQL = 'SELECT [id] FROM [[users]] WHERE [username] = {username}';
+        $aclSQL = '
+            INSERT INTO [[new_acl]]
+                ([component], [key_name], [key_subkey], [key_value], [max_value], [user], [group])
+            VALUES
+                ({component}, {key_name}, {key_subkey}, {key_value}, {max_value}, {user}, {group})';
 
         $params = array();
         $usersArray = array();
+        unset($keys['/last_update']);
         foreach ($keys as $key => $value) {
             $value = ($value == 'true');
             $params['old_key_name'] = $key;
@@ -107,7 +112,6 @@ class Upgrader_08To0902 extends JawsUpgraderStage
                         continue;
                     }
                     $user = (int)$usersArray[$key[1]];
-
                     break;
 
                 case 'groups':
@@ -124,52 +128,51 @@ class Upgrader_08To0902 extends JawsUpgraderStage
                     $new_key_name = $key[2];
             }
 
-            $params['component']     = $component;
-            $params['new_key_name']  = $new_key_name;
-            $params['key_subkey']    = '';
-            $params['new_key_value'] = (int)$value;
+            $params['component']  = $component;
+            $params['key_name']   = $new_key_name;
+            $params['key_subkey'] = '';
+            $params['key_value']  = (int)$value;
+            $params['max_value']  = 0;
             $params['user']  = $user;
             $params['group'] = $group;
             $res = $GLOBALS['db']->query($aclSQL, $params);
             if (Jaws_Error::IsError($res)) {
-                _log(JAWS_LOG_WARNING, $res->getMessage());
-                return new Jaws_Error($res->getMessage(), 0, JAWS_ERROR_ERROR);
+                if (MDB2_ERROR_CONSTRAINT != $res->getCode()) {
+                    return Jaws_Error::raiseError($res->getMessage(), 0, JAWS_ERROR_ERROR);
+                }
             }
-
         }
 
-        // delete outdated registry keys
-        $sql = '
-            DELETE
-            FROM [[acl]]
-            WHERE
-                [old_key_name] = {key1}';
-        $params = array();
-        $params['key1'] = '/last_update';
-
-        $res = $GLOBALS['db']->query($sql, $params);
-        if (Jaws_Error::IsError($res)) {
-            _log(JAWS_LOG_ERROR, $res->getMessage());
-            return $res;
+        _log(JAWS_LOG_DEBUG,"Droping old acl table");
+        $result = $GLOBALS['db']->dropTable('old_acl');
+        if (Jaws_Error::isError($result)) {
+            return $result;
         }
 
         // upgrade core database schema - next step
         $old_schema = JAWS_PATH . 'upgrade/schema/0.9.0.2.xml';
         $new_schema = JAWS_PATH . 'upgrade/schema/schema.xml';
         if (!file_exists($old_schema)) {
-            return new Jaws_Error(_t('GLOBAL_ERROR_SQLFILE_NOT_EXISTS', '0.9.0.2.xml'),0 , JAWS_ERROR_ERROR);
+            return Jaws_Error::raiseError(
+                _t('GLOBAL_ERROR_SQLFILE_NOT_EXISTS', '0.9.0.2.xml'),
+                0,
+                JAWS_ERROR_ERROR
+            );
         }
 
         if (!file_exists($new_schema)) {
-            return new Jaws_Error(_t('GLOBAL_ERROR_SQLFILE_NOT_EXISTS', 'schema.xml'),0 , JAWS_ERROR_ERROR);
+            return Jaws_Error::raiseError(
+                _t('GLOBAL_ERROR_SQLFILE_NOT_EXISTS', 'schema.xml'),
+                0,
+                JAWS_ERROR_ERROR
+            );
         }
 
         _log(JAWS_LOG_DEBUG,"Upgrading core schema");
         $result = $GLOBALS['db']->installSchema($new_schema, '', $old_schema);
         if (Jaws_Error::isError($result)) {
-            _log(JAWS_LOG_ERROR, $result->getMessage());
             if ($result->getCode() !== MDB2_ERROR_ALREADY_EXISTS) {
-                return new Jaws_Error($result->getMessage(), 0, JAWS_ERROR_ERROR);
+                return Jaws_Error::raiseError($result->getMessage(), 0, JAWS_ERROR_ERROR);
             }
         }
 
