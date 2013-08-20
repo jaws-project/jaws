dff7c7b2e105a541a3ce9f249f183268a0d2ea82
Mojtaba Ebrahimi <irangame@gmail.com>
2013-08-20 21:02:36 +0430

    [Friends]: fixed typo

--- Friends/Model/Admin/Friends.php
+++ Friends/Model/Admin/Friends.php
@@ -1,10 +1,11 @@
 <?php
 /**
- * Blocks Admin Gadget
+ * Friend Gadget
  *
- * @category   GadgetModelAdmin
- * @package    Blocks
+ * @category   GadgetModel
+ * @package    Friend
  * @author     Jonathan Hernandez <ion@suavizado.com>
+ * @author     Pablo Fischer <pablo@pablo.com.mx>
  * @copyright  2004-2013 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
