1675509a48511d66e98971681a139fc9a99154cf
Ali Fazelzadeh <afz@php.net>
2013-08-23 16:05:19 +0430

    Fixed check HTML.php

--- gadgets/Components/Model/Gadgets.php
+++ gadgets/Components/Model/Gadgets.php
@@ -94,7 +94,7 @@ class Components_Model_Gadgets extends Jaws_Gadget_Model
                         'updated'     => (bool)$gUpdated,
                         'disabled'    => strpos($disabled_gadgets, ",$gadget,") !==false,
                         'has_layout'  => $objGadget->_has_layout,
-                        'has_html'    => file_exists($gDir . $gadget . DIRECTORY_SEPARATOR . 'HTML.php'),
+                        'has_html'    => !is_null($objGadget->default_action),
                 );
             }
 
