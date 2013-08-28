c744c8e52bfa13d82c38cdff136a23d1dccc09a4
Ali Fazelzadeh <afz@php.net>
2013-08-28 16:59:34 +0430

    [Blocks]: removed constructor method

--- gadgets/Blocks/AdminAjax.php
+++ gadgets/Blocks/AdminAjax.php
@@ -11,19 +11,6 @@
 class Blocks_AdminAjax extends Jaws_Gadget_HTML
 {
     /**
-     * Constructor
-     *
-     * @access  public
-     * @param   object $gadget Jaws_Gadget object
-     * @return  void
-     */
-    function Blocks_AdminAjax($gadget)
-    {
-        parent::Jaws_Gadget_HTML($gadget);
-        $this->_Model = $this->gadget->load('Model')->load('AdminModel');
-    }
-
-    /**
      * Get Block
      *
      * @access  public
