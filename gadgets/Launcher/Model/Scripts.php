01a1fe837106ccc46438bab16d6c567611bf2935
Mohsen Khahani <mkhahani@gmail.com>
2013-08-29 07:59:42 +0430

    [Launcher]: missed commit

---
+++ gadgets/Launcher/Model/Scripts.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Launcher Gadget
+ *
+ * @category   GadgetModel
+ * @package    Launcher
+ * @author     Jonathan Hernandez <ion@suavizado.com>
+ * @copyright  2006-2013 Jaws Development Group
+ * @license    http://www.gnu.org/copyleft/gpl.html
+ */
+class Launcher_Model_Scripts extends Jaws_Gadget_Model
+{
+    /**
+     * Get all scripts
+     *
+     * @access  public
+     * @return  array   An array of all the scripts
+     */
+    function GetScripts()
+    {
+        $result = array();
+        $path = JAWS_PATH . 'gadgets/Launcher/scripts/';
+        $adr = scandir($path);
+        foreach($adr as $file) {
+            if (substr($file, -4) == '.php') {
+                $result[$file] = substr($file, 0, -4);
+            }
+        }
+        sort($result);
+        return $result;
+    }
+}
