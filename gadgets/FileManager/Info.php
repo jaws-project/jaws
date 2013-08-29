6d6e011e23e105ec2708378380d75b4208c4006e
Mohsen Khahani <mkhahani@gmail.com>
2013-08-29 07:37:49 +0430

    [FileManager]: add gadget Info file

---
+++ gadgets/FileManager/Info.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * FileManager gadget info
+ *
+ * @category    GadgetInfo
+ * @package     FileManager
+ * @author      Mohsen Khahani <mkhahani@gmail.com>
+ * @copyright   2013 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ */
+class FileManager_Info extends Jaws_Gadget
+{
+    /**
+     * Gadget version
+     *
+     * @var     string
+     * @access  private
+     */
+    var $version = '0.0.1';
+
+}
