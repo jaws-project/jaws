f66cf532574e60c43fb60d86c691cdb9fcc0be88
Mojtaba Ebrahimi <irangame@gmail.com>
2013-08-30 13:00:11 +0430

    [PrivateMessage]: in progress

---
+++ gadgets/PrivateMessage/Actions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * PrivateMessage Actions
+ *
+ * @category    GadgetActions
+ * @package     PrivateMessage
+ * @author      Mojtaba Ebrahimi <ebrahimi@zehneziba.ir>
+ * @copyright   2013 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/lesser.html
+ */
+$actions = array();
+
+$actions['NavigationArea'] = array(
+    'normal' => true,
+    'layout' => true,
+    'file' => 'NavigationArea',
+);
---
+++ gadgets/PrivateMessage/AdminActions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * PrivateMessage Actions
+ *
+ * @category    GadgetActions
+ * @package     PrivateMessage
+ * @author      Mojtaba Ebrahimi <ebrahimi@zehneziba.ir>
+ * @copyright   2013 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/lesser.html
+ */
+$actions = array();
+
+$actions['Properties'] = array(
+    'normal' => true,
+    'file' => 'Properties',
+);
