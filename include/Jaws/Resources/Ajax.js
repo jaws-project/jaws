33948fe9cff1e94d07a603e0cbaadaac6548c171
Ali Fazelzadeh <afz@php.net>
2013-08-31 10:37:13 +0430

    [Jaws_ajax]: moved this prototype to mootools core

--- include/Jaws/Resources/Ajax.js
+++ include/Jaws/Resources/Ajax.js
@@ -246,46 +246,6 @@ function getEditorValue(name)
 
     return $(name).value;
 }
-/**
- * Extends the Element native object to include some shortcut methods
- */
-Element.implement({
-
-    isDisplayed: function(){
-        return this.getStyle('display') != 'none';
-    },
-
-    isVisible: function(){
-        var w = this.offsetWidth,
-            h = this.offsetHeight;
-        return (w == 0 && h == 0) ? false : (w > 0 && h > 0) ? true : this.style.display != 'none';
-    },
-
-    toggle: function(){
-        return this[this.isDisplayed() ? 'hide' : 'show']();
-    },
-
-    hide: function(){
-        var d;
-        try {
-            //IE fails here if the element is not in the dom
-            d = this.getStyle('display');
-        } catch(e){}
-        if (d == 'none') return this;
-        return this.store('element:_originalDisplay', d || '').setStyle('display', 'none');
-    },
-
-    show: function(display){
-        if (!display && this.isDisplayed()) return this;
-        display = display || this.retrieve('element:_originalDisplay') || 'block';
-        return this.setStyle('display', (display == 'none') ? 'block' : display);
-    },
-
-    swapClass: function(remove, add){
-        return this.removeClass(remove).addClass(add);
-    }
-
-});
 
 /**
  * Javascript blank string prototype
