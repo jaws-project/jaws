229246ce017e1c182f56c40333606d68dabf6d4e
Ali Fazelzadeh <afz@php.net>
2013-09-01 14:28:47 +0430

    [Jaws_Gadget_HTML]: filter Ajax actions same as common get/post methods

--- include/Jaws/Gadget/HTML.php
+++ include/Jaws/Gadget/HTML.php
@@ -92,7 +92,7 @@ class Jaws_Gadget_HTML
 
         $output = '';
         $request =& Jaws_Request::getInstance();
-        $method = $request->get('method', 'get');
+        $method = Jaws_Gadget_HTML::filter($request->get('method', 'get'));
         if (method_exists($objAjax, $method)) {
             $params = $request->getAll('post');
             $output = call_user_func_array(array($objAjax, $method), $params);
