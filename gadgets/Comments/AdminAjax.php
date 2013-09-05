5e368f4dddd43fc0fe4c5ebb6a5a323d64115776
Ali Fazelzadeh <afz@php.net>
2013-09-06 00:17:10 +0430

    [Comments]: Applied new changes of passing params to Ajax methods

--- gadgets/Comments/AdminAjax.php
+++ gadgets/Comments/AdminAjax.php
@@ -15,15 +15,12 @@ class Comments_AdminAjax extends Jaws_Gadget_HTML
      * Search for comments and return the data in an array
      *
      * @access  public
-     * @param   int     $limit   Data limit
-     * @param   string  $gadget
-     * @param   string  $search  Search word
-     * @param   int  $status  comment status (approved=1, waiting=2, spam=3)
      * @return  array   Data array
      */
-    function SearchComments($limit, $gadget, $search, $status)
+    function SearchComments()
     {
         $this->gadget->CheckPermission('ManageComments');
+        @list($limit, $gadget, $search, $status) = jaws()->request->getAll('post');
         $cHTML = $GLOBALS['app']->LoadGadget('Comments', 'AdminHTML');
         return $cHTML->GetDataAsArray($gadget, "javascript:editComment(this, '{id}')", $search, $status, $limit, true);
     }
@@ -32,13 +29,11 @@ class Comments_AdminAjax extends Jaws_Gadget_HTML
      * Get total posts of a comment search
      *
      * @access  public
-     * @param   string  $gadget
-     * @param   string  $search  Search word
-     * @param   string  $status  Spam status (approved=1, waiting=2, spam=3)
      * @return  int     Total of posts
      */
-    function SizeOfCommentsSearch($gadget, $search, $status)
+    function SizeOfCommentsSearch()
     {
+        @list($gadget, $search, $status) = jaws()->request->getAll('post');
         $cModel = $GLOBALS['app']->LoadGadget('Comments', 'Model', 'Comments');
         return $cModel->GetCommentsCount($gadget, '', '', $search, $status);
     }
@@ -47,11 +42,11 @@ class Comments_AdminAjax extends Jaws_Gadget_HTML
      * Get information of a Comment
      *
      * @access  public
-     * @param   int     $id Comment ID
      * @return  array   Comment info array
      */
-    function GetComment($id)
+    function GetComment()
     {
+        @list($id) = jaws()->request->getAll('post');
         $cModel = $GLOBALS['app']->LoadGadget('Comments', 'Model', 'Comments');
         $comment = $cModel->GetComment($id);
         if (Jaws_Error::IsError($comment)) {
@@ -65,19 +60,12 @@ class Comments_AdminAjax extends Jaws_Gadget_HTML
      * Update comment information
      *
      * @access  public
-     * @param   string  $gadget
-     * @param   int     $id         Comment ID
-     * @param   string  $name       Name
-     * @param   string  $email      Email address
-     * @param   string  $url
-     * @param   string  $message    Message content
-     * @param   string  $reply
-     * @param   $status
      * @return  array   Response array (notice or error)
      */
-    function UpdateComment($gadget, $id, $name, $email, $url, $message, $reply, $status)
+    function UpdateComment()
     {
         $this->gadget->CheckPermission('ManageComments');
+        @list($gadget, $id, $name, $email, $url, $message, $reply, $status) = jaws()->request->getAll('post');
         // TODO: Fill permalink In New Versions, Please!!
         $cModel = $GLOBALS['app']->LoadGadget('Comments', 'Model', 'EditComments');
         $res = $cModel->updateComment($gadget, $id, $name, $email, $url, $message, $reply, '', $status);
@@ -94,12 +82,12 @@ class Comments_AdminAjax extends Jaws_Gadget_HTML
      * Does a massive delete on comments
      *
      * @access  public
-     * @param   array   $ids     Comment ids
      * @return  array   Response array (notice or error)
      */
-    function DeleteComments($ids)
+    function DeleteComments()
     {
         $this->gadget->CheckPermission('ManageComments');
+        $ids = jaws()->request->get('0:array', 'post');
         $cModel = $GLOBALS['app']->LoadGadget('Comments', 'Model', 'DeleteComments');
         $res = $cModel->DeleteMassiveComment($ids);
         if (Jaws_Error::IsError($res)) {
@@ -115,14 +103,12 @@ class Comments_AdminAjax extends Jaws_Gadget_HTML
      * Mark as different type a group of ids
      *
      * @access  public
-     * @param   string  $gadget
-     * @param   array   $ids        Ids of comments
-     * @param   string  $status     New status
      * @return  array   Response array (notice or error)
      */
-    function MarkAs($gadget, $ids, $status)
+    function MarkAs()
     {
         $this->gadget->CheckPermission('ManageComments');
+        @list($gadget, $ids, $status) = jaws()->request->get(array('0', '1:array', '2'), 'post');
         $cModel = $GLOBALS['app']->LoadGadget('Comments', 'Model', 'EditComments');
         $res = $cModel->MarkAs($gadget, $ids, $status);
         if (Jaws_Error::IsError($res)) {
@@ -138,13 +124,12 @@ class Comments_AdminAjax extends Jaws_Gadget_HTML
      * Update Settings
      *
      * @access  public
-     * @param   string  $allowComments  Allow comments?
-     * @param   string  $allowDuplicate Allow duplicated comments?
      * @return  array   Response array (notice or error)
      */
-    function SaveSettings($allowComments, $allowDuplicate)
+    function SaveSettings()
     {
         $this->gadget->CheckPermission('Settings');
+        @list($allowComments, $allowDuplicate) = jaws()->request->getAll('post');
         $cModel = $GLOBALS['app']->LoadGadget('Comments', 'AdminModel', 'Settings');
         $res = $cModel->SaveSettings($allowComments, $allowDuplicate);
         if (Jaws_Error::IsError($res)) {
