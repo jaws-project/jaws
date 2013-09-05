10fda64206a0f092ae1c16cbe6fc042fe2ba5316
Ali Fazelzadeh <afz@php.net>
2013-09-06 00:19:28 +0430

    [Shoutbox]: Fixed working with comments datagrid

--- gadgets/Shoutbox/Actions/Admin/Comments.php
+++ gadgets/Shoutbox/Actions/Admin/Comments.php
@@ -20,6 +20,7 @@ class Shoutbox_Actions_Admin_Comments extends Shoutbox_AdminHTML
      */
     function Comments()
     {
+        $this->AjaxMe('script.js');
         $GLOBALS['app']->Layout->AddScriptLink('gadgets/Comments/resources/script.js');
 
         $cHTML = $GLOBALS['app']->LoadGadget('Comments', 'AdminHTML');
