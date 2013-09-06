77b428195c244a5e2d3578bd43554b971858c8c2
Mojtaba Ebrahimi <irangame@gmail.com>
2013-09-06 09:54:59 +0430

    [Tms]: Apply new changes in Ajax

--- gadgets/Tms/AdminAjax.php
+++ gadgets/Tms/AdminAjax.php
@@ -13,12 +13,13 @@ class Tms_AdminAjax extends Jaws_Gadget_HTML
     /**
      * Gets information of given theme
      *
-     * @access  public
-     * @param   string  $theme  Name of the theme
-     * @return  array   Theme info
+     * @access   public
+     * @internal param  string  $theme  Name of the theme
+     * @return   array  Theme info
      */
-    function GetThemeInfo($theme)
+    function GetThemeInfo()
     {
+        @list($theme) = jaws()->request->getAll('post');
         $gadget = $GLOBALS['app']->LoadGadget('Tms', 'AdminHTML', 'Themes');
         return $gadget->GetThemeInfo($theme);
     }
