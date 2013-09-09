139b9dd65502bf332043b36b5b526c9edd3d3e40
Mohsen Khahani <mkhahani@gmail.com>
2013-09-09 21:11:33 +0430

    [Directory]: add language fir

---
+++ languages/en/gadgets/Directory.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Meta Data
+ *
+ * "Project-Id-Version: Directory"
+ * "Last-Translator: "
+ * "Language-Team: EN"
+ * "MIME-Version: 1.0"
+ * "Content-Type: text/plain; charset=UTF-8"
+ * "Content-Transfer-Encoding: 8bit"
+ */
+
+define('_EN_DIRECTORY_NAME', "Directory");
+define('_EN_DIRECTORY_DESCRIPTION', "Total file and directory management gadget");
+
+// Actions
+define('_EN_DIRECTORY_ACTION_TITLE', "Directory");
+define('_EN_DIRECTORY_ACTION_DEFAULT', "Default Action");
+define('_EN_DIRECTORY_ACTIONS_DISPLAY', "Display");
+define('_EN_DIRECTORY_ACTIONS_DISPLAY_DESC', "Displays directory");
+
+// Terms
+
+// Directories
+define('_EN_DIRECTORY_DIR_TITLE', "Title");
+define('_EN_DIRECTORY_DIR_DESC', "Description");
+define('_EN_DIRECTORY_DIR_PARENT', "Parent");
+
+// Files
+define('_EN_DIRECTORY_FILE_TITLE', "Title");
+define('_EN_DIRECTORY_FILE_PARENT', "Parent");
+
+// Sharing
+define('_EN_DIRECTORY_SHARE', "Share");
+
+// Messages
+define('_EN_DIRECTORY_ERROR_INCOMPLETE_DATA', "Some fields are necessary to be filled.");
+define('_EN_DIRECTORY_NOTICE_SHARE_UPDATED', "Shared file users have been updated.");
+define('_EN_DIRECTORY_ERROR_UPDATING_SHARE', "Could not update shared file users.");
+
+define('_EN_DIRECTORY_NOTICE_DIR_CREATED', "A new directory has been created.");
+define('_EN_DIRECTORY_NOTICE_DIR_UPDATED', "Directory has been updated.");
+define('_EN_DIRECTORY_NOTICE_DIR_DELETED', "Directory has been deleted.");
+define('_EN_DIRECTORY_ERROR_DIR_CREATE', "Creating directory has been failed.");
+define('_EN_DIRECTORY_ERROR_DIR_UPDATE', "Updating directory has been failed.");
+define('_EN_DIRECTORY_ERROR_DIR_DELETE', "Could not delete the directory.");
+
+define('_EN_DIRECTORY_NOTICE_FILE_CREATED', "A new file has been created.");
+define('_EN_DIRECTORY_NOTICE_FILE_UPDATED', "File has been updated.");
+define('_EN_DIRECTORY_NOTICE_FILE_DELETED', "File has been deleted.");
+define('_EN_DIRECTORY_ERROR_FILE_CREATE', "Creating file has been failed.");
+define('_EN_DIRECTORY_ERROR_FILE_UPDATE', "Updating file has been failed.");
+define('_EN_DIRECTORY_ERROR_FILE_DELETE', "Could not delete the file.");
+define('_EN_DIRECTORY_ERROR_FILE_UPLOAD', "No file is selected.");
