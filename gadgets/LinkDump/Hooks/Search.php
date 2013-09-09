9029b0e02915a9d8447ab0138c9ac8916a9d6e22
Ali Fazelzadeh <afz@php.net>
2013-09-09 13:09:11 +0430

    [LinkDump]: Fixed action name

--- gadgets/LinkDump/Hooks/Search.php
+++ gadgets/LinkDump/Hooks/Search.php
@@ -50,7 +50,7 @@ class LinkDump_Hooks_Search extends Jaws_Gadget_Hook
         foreach ($result as $r) {
             $link = array();
             $link['title']   = $r['title'];
-            $link['url']     = $GLOBALS['app']->Map->GetURLFor('LinkDump', 'Click', array('id' => $r['id']));
+            $link['url']     = $GLOBALS['app']->Map->GetURLFor('LinkDump', 'Link', array('id' => $r['id']));
             $link['outer']   = true;
             $link['image']   = 'gadgets/LinkDump/images/logo.png';
             $link['snippet'] = $r['description'];
