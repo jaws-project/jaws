85a2a9e7fc3da5bbe686c76c718d5b0d95c3a3e9
Ali Fazelzadeh <afz@php.net>
2013-09-10 00:26:17 +0430

    use native method

--- gadgets/Forums/Hooks/Menu.php
+++ gadgets/Forums/Hooks/Menu.php
@@ -21,7 +21,7 @@ class Forums_Hooks_Menu extends Jaws_Gadget_Hook
     {
         $urls = array();
         $urls[] = array(
-            'url'   => $GLOBALS['app']->Map->GetURLFor('Forums', 'Forums'),
+            'url'   => $this->gadget->urlMap('Forums'),
             'title' => _t('FORUMS_FORUMS')
         );
 
