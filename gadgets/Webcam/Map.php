cac464cc17bdc441d34374c0c8b2c02b0563538c
Mojtaba Ebrahimi <irangame@gmail.com>
2013-09-09 21:18:34 +0430

    [Webcam]: correct action name

--- gadgets/Webcam/Map.php
+++ gadgets/Webcam/Map.php
@@ -8,4 +8,4 @@
  * @copyright  2008-2013 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
-$maps[] = array('DefaultAction', 'webcams');
+$maps[] = array('Display', 'webcams');
