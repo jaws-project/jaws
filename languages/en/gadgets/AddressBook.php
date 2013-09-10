c151286d943226686dc1203e5e2e28e0d9a1de06
HamidReza Aboutalebi <abt_am@yahoo.com>
2013-09-10 20:11:13 +0330

    [AddressBook]: add language file en

---
+++ languages/en/gadgets/AddressBook.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Meta Data
+ *
+ * "Project-Id-Version: AddressBook"
+ * "Last-Translator: Hamid Reza Aboutalebi <hamid@aboutalebi.com>
+ * "Language-Team: EN"
+ * "MIME-Version: 1.0"
+ * "Content-Type: text/plain; charset=UTF-8"
+ * "Content-Transfer-Encoding: 8bit"
+ */
+define('_EN_ADDRESSBOOK_NAME', "Address Book");
+define('_EN_ADDRESSBOOK_DESCRIPTION', "AddressBook Desc");
+
+/* Actions */
+
+
+// ACL Keys
+
+
+/* Common */
+
+
+/* AddressBook */
+define('_EN_ADDRESSBOOK_ITEMS_NAME', "Name");
+define('_EN_ADDRESSBOOK_ITEMS_NICKNAME', "NickName");
+define('_EN_ADDRESSBOOK_ITEMS_TITLE', "Title");
+define('_EN_ADDRESSBOOK_ITEMS_ADDRESS', "Address");
+define('_EN_ADDRESSBOOK_ITEMS_URL', "URL");
+define('_EN_ADDRESSBOOK_ITEMS_NOTES', "Notes");
+define('_EN_ADDRESSBOOK_ADDRESSBOOK_MANAGE', "Manage Address Book");
+define('_EN_ADDRESSBOOK_ITEMS_ADD_NEW_TITLE', "Add New Address");
+define('_EN_ADDRESSBOOK_ITEMS_ADD', "New");
+define('_EN_ADDRESSBOOK_ITEMS_EDIT_TITLE', "Edit Address");
+define('_EN_ADDRESSBOOK_VIEW_ALL_ADDREESS_MY', "View My AddressBook");
+define('_EN_ADDRESSBOOK_VIEW_ALL_ADDREESS_USER', "View User AddressBook");
+define('_EN_ADDRESSBOOK_HOME_TELL', "Home Phone");
+define('_EN_ADDRESSBOOK_HOME_MOBILE', "Home Mobile");
+define('_EN_ADDRESSBOOK_HOME_FAX', "Home Fax");
+define('_EN_ADDRESSBOOK_WORK_TELL', "Work Phone");
+define('_EN_ADDRESSBOOK_WORK_MOBILE', "Work Mobile");
+define('_EN_ADDRESSBOOK_WORK_FAX', "Work Fax");
+define('_EN_ADDRESSBOOK_OTHER_TELL', "Other Phone");
+define('_EN_ADDRESSBOOK_OTHER_MOBILE', "Other Mobile");
+define('_EN_ADDRESSBOOK_OTHER_FAX', "Other Fax");
+define('_EN_ADDRESSBOOK_HOME_EMAIL', "Home Email");
+define('_EN_ADDRESSBOOK_WORK_EMAIL', "Work Email");
+define('_EN_ADDRESSBOOK_OTHER_EMAIL', "Other Email");
+define('_EN_ADDRESSBOOK_PERSON_IMAGE_UPLOAD', "Upload Image");
+define('_EN_ADDRESSBOOK_PERSON_IMAGE_DELETE', "Delete Image");
+define('_EN_ADDRESSBOOK_ITEMS_PUBLIC', "Public");
+define('_EN_ADDRESSBOOK_OTHER_DETAILS', "Other Details");
+define('_EN_ADDRESSBOOK_TEL_TITLE', "Phones");
+define('_EN_ADDRESSBOOK_EMAIL_TITLE', "Emails");
+
+/* Groups */
+define('_EN_ADDRESSBOOK_GROUP_TITLE', "Address Book Groups");
+define('_EN_ADDRESSBOOK_GROUPS_ADD', "Add New Group");
+define('_EN_ADDRESSBOOK_GROUP_ADD_NEW_TITLE', "Add New Group");
+define('_EN_ADDRESSBOOK_GROUP_EDIT_TITLE', "Edit Group");
+define('_EN_ADDRESSBOOK_GROUPS_MANAGE', "Manage Groups");
+
+// Responses
+define('_EN_ADDRESSBOOK_RESULT_NEW_GROUP_SAVED', "Group was successfully added");
+define('_EN_ADDRESSBOOK_RESULT_EDIT_GROUP_SAVED', "Group was successfully updated");
+define('_EN_ADDRESSBOOK_RESULT_EDIT_ADDRESS_SAVED', "Address was successfully updated");
+define('_EN_ADDRESSBOOK_RESULT_NEW_ADDRESS_SAVED', "Address was successfully added");
