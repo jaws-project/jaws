67134e9dc50de7509b489183c7c648f5b3f69eda
HamidReza Aboutalebi <abt_am@yahoo.com>
2013-09-21 13:50:02 -0800

    [AddressBook]: Update Language

--- languages/en/gadgets/AddressBook.php
+++ languages/en/gadgets/AddressBook.php
@@ -13,7 +13,8 @@ define('_EN_ADDRESSBOOK_NAME', "Address Book");
 define('_EN_ADDRESSBOOK_DESCRIPTION', "AddressBook Desc");
 
 /* Actions */
-
+define('_EN_ADDRESSBOOK_ACTIONS_GROUPS', "Address Book");
+define('_EN_ADDRESSBOOK_ACTIONS_GROUPS_DESC', "Display Links Of Management Area Of Address Books And List Of Groups");
 
 // ACL Keys
 
@@ -28,7 +29,7 @@ define('_EN_ADDRESSBOOK_ITEMS_TITLE', "Title");
 define('_EN_ADDRESSBOOK_ITEMS_ADDRESS', "Address");
 define('_EN_ADDRESSBOOK_ITEMS_URL', "URL");
 define('_EN_ADDRESSBOOK_ITEMS_NOTES', "Notes");
-define('_EN_ADDRESSBOOK_ADDRESSBOOK_MANAGE', "Manage Address Book");
+define('_EN_ADDRESSBOOK_ADDRESSBOOK_MANAGE', "Address Book");
 define('_EN_ADDRESSBOOK_ITEMS_ADD_NEW_TITLE', "Add New Address");
 define('_EN_ADDRESSBOOK_ITEMS_ADD', "New");
 define('_EN_ADDRESSBOOK_ITEMS_EDIT_TITLE', "Edit Address");
@@ -62,8 +63,17 @@ define('_EN_ADDRESSBOOK_ITEMS_USER_LINK', "Linked To");
 define('_EN_ADDRESSBOOK_TERM', "Term");
 define('_EN_ADDRESSBOOK_EMPTY_NAME_WARNING', "Please Enter 'First Name' Or 'Last Name'");
 define('_EN_ADDRESSBOOK_ADDRESS_REMOVE_FROM_GROUP', "Remove From Group");
-define('_EN_ADDRESSBOOK_EXPORT_VCARD', "Export To vCard");
 define('_EN_ADDRESSBOOK_IMPORT_VCARD', "Import From vCard");
+define('_EN_ADDRESSBOOK_IMPORT_VCART_TITLE', "Import vCard From *.vcf File");
+define('_EN_ADDRESSBOOK_IMPORT_VCART', "Import");
+define('_EN_ADDRESSBOOK_IMPORT_VCART_DESC', "Select a file with *.vcf format for import");
+define('_EN_ADDRESSBOOK_EXPORT_VCARD', "Export To vCard");
+define('_EN_ADDRESSBOOK_PUBLICS', "Publics");
+define('_EN_ADDRESSBOOK_ITEMS_IS_PUBLIC', "Public");
+define('_EN_ADDRESSBOOK_BACK_TO_MY_ADDRESS', "Back To My Address");
+define('_EN_ADDRESSBOOK_USER_ADDRESS_TITLE', "AddressBooks Of User: {0}");
+define('_EN_ADDRESSBOOK_ITEMS_VIEW_TITLE', "View Address");
+define('_EN_ADDRESSBOOK_LABEL_RALATED', "Related To");
 
 /* Groups */
 define('_EN_ADDRESSBOOK_GROUP', "Group");
@@ -71,7 +81,7 @@ define('_EN_ADDRESSBOOK_GROUP_TITLE', "Address Book Groups");
 define('_EN_ADDRESSBOOK_GROUPS_ADD', "Add New Group");
 define('_EN_ADDRESSBOOK_GROUP_ADD_NEW_TITLE', "Add New Group");
 define('_EN_ADDRESSBOOK_GROUP_EDIT_TITLE', "Edit Group");
-define('_EN_ADDRESSBOOK_GROUPS_MANAGE', "Manage Groups");
+define('_EN_ADDRESSBOOK_GROUPS_MANAGE', "Groups");
 define('_EN_ADDRESSBOOK_VIEW_GROUP_MEMBER', "View Members");
 define('_EN_ADDRESSBOOK_GROUP_MEMBERS_TITLE', "AddressBooks Of {0}");
 define('_EN_ADDRESSBOOK_GROUP_BACK_TO_GROUPS_LIST', "Back To Groups List");
@@ -86,3 +96,4 @@ define('_EN_ADDRESSBOOK_RESULT_DELETE_GROUP_COMPLETE', "The group has been delet
 define('_EN_ADDRESSBOOK_RESULT_DELETE_ADDRESS_COMPLETE', "The address has been deleted");
 define('_EN_ADDRESSBOOK_RESULT_DELETE_ADDRESS_RELATION_COMPLETE', "The relation between address and group was successfully removed");
 define('_EN_ADDRESSBOOK_RESULT_ADD_ADDRESS_RELATION_COMPLETE', "The relation between address and group was successfully added");
+define('_EN_ADDRESSBOOK_RESULT_IMPORT_COMPLETED', "Import Address Book Completed.");
