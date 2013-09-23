f064c0d4f351bc0707379906994d223e9b06fb98
Ali Fazelzadeh <afz@php.net>
2013-09-23 23:36:27 +0330

    Fixed js json encode

--- include/Jaws/Resources/Ajax.js
+++ include/Jaws/Resources/Ajax.js
@@ -787,27 +787,12 @@ function toJSON(v) {
             object: function (x) {
                 if (x) {
                     var a = [], b, f, i, l, v;
-                    if (x instanceof Array) {
-                        a[0] = '[';
-                        l = x.length;
-                        for (i = 0; i < l; i += 1) {
-                            v = x[i];
-                            f = s[typeof v];
-                            if (f) {
-                                v = f(v);
-                                if (typeof v == 'string') {
-                                    if (b) {
-                                        a[a.length] = ',';
-                                    }
-                                    a[a.length] = v;
-                                    b = true;
-                                }
-                            }
-                        }
-                        a[a.length] = ']';
-                    } else if ((x instanceof Object) || (typeof x) == 'object') {
+                    if ((x instanceof Object) || ((typeof x) == 'object') || (x instanceof Array)) {
                         a[0] = '{';
                         for (i in x) {
+                            if (!x.hasOwnProperty(i)) {
+                                continue;
+                            }
                             v = x[i];
                             f = s[typeof v];
                             if (f) {
