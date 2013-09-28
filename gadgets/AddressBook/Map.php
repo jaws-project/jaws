91ae73f02f8ea6b9aa0b00193c19bc551f4975bb
HamidReza Aboutalebi <abt_am@yahoo.com>
2013-09-28 10:26:24 -0800

    [AddressBook]: Add url map

--- gadgets/AddressBook/Map.php
+++ gadgets/AddressBook/Map.php
@@ -7,6 +7,15 @@
  * @author     Hamid Reza Aboutalebi <hamid@aboutalebi.com>
  * @copyright  2013 Jaws Development Group
  */
-$maps[] = array('UserAddress', 'addressbook[/{uid}][/page/{page}]',
+$maps[] = array('AddressBook',  'addressbook');
+$maps[] = array('AddAddress',   'addressbook/new');
+$maps[] = array('View',         'addressbook/view/{id}', array('id'   => '[[:digit:]]+'));
+$maps[] = array('EditAddress',  'addressbook/edit/{id}', array('id'   => '[[:digit:]]+'));
+$maps[] = array('VCardImport',  'addressbook/import');
+$maps[] = array('ManageGroups', 'addressbook/groups');
+$maps[] = array('AddGroup',     'addressbook/groups/new');
+$maps[] = array('EditGroup',     'addressbook/groups/edit/{id}', array('id'   => '[[:digit:]]+'));
+$maps[] = array('GroupMembers', 'addressbook/groups/members/{id}', array('id'   => '[[:digit:]]+'));
+$maps[] = array('UserAddress',  'addressbook[/{uid}][/page/{page}]',
                                 array('uid'   => '[\p{L}[:digit:]-_\.]+',
                                 'page' => '[[:digit:]]+',));
