e4d79dc9ddc516900003e18c9134fd00b9794468
Ali Fazelzadeh <afz@php.net>
2013-10-01 10:50:15 +0330

    [Users]: Fixed removing avatar after re-save personal information

--- gadgets/Users/Model/Personal.php
+++ gadgets/Users/Model/Personal.php
@@ -15,42 +15,14 @@ class Users_Model_Personal extends Jaws_Gadget_Model
      * Updates user profile
      *
      * @access  public
-     * @param   int      $uid       User ID
-     * @param   string   $fname     First name
-     * @param   string   $lname     Last name
-     * @param   string   $gender    User gender
-     * @param   string   $ssn       Social Security number
-     * @param   string   $dob       User birth date
-     * @param   string   $url       User URL
-     * @param   string   $avatar    User avatar filename
-     * @param   string   $signature
-     * @param   string   $about     About user
-     * @param   string   $experiences
-     * @param   string   $occupations
-     * @param   string   $interests
-     * @return  mixed    True on success or Jaws_Error on failure
+     * @param   int     $uid       User ID
+     * @param   array   $pData     First name
+     * @return  mixed   True on success or Jaws_Error on failure
      */
-    function UpdatePersonal($uid, $fname, $lname, $gender, $ssn, $dob, $url, $avatar, $signature, $about,
-                            $experiences, $occupations, $interests)
+    function UpdatePersonal($uid, $pData)
     {
-        require_once JAWS_PATH . 'include/Jaws/User.php';
         $jUser = new Jaws_User;
-        $pInfo = array(
-            'fname'        => $fname,
-            'lname'        => $lname,
-            'gender'       => $gender,
-            'ssn'          => $ssn,
-            'dob'          => $dob,
-            'url'          => $url,
-            'avatar'       => $avatar,
-            'signature'    => $signature,
-            'about'        => $about,
-            'experiences'  => $experiences,
-            'occupations'  => $occupations,
-            'interests'    => $interests
-        );
-
-        $result = $jUser->UpdatePersonal($uid, $pInfo);
+        $result = $jUser->UpdatePersonal($uid, $pData);
         return $result;
     }
 
