e91f356443cd85090cf82ab3cb17ac5b4c9a6959
Mojtaba Ebrahimi <mojtaba.ebrahimi@outlook.com>
2013-10-05 16:41:06 +0330

    [LinkDump]: updated Tags hook

--- gadgets/LinkDump/Hooks/Tags.php
+++ gadgets/LinkDump/Hooks/Tags.php
@@ -14,24 +14,21 @@ class LinkDump_Hooks_Tags extends Jaws_Gadget_Hook
      * Returns an array with the results of a tag content
      *
      * @access  public
-     * @param   string  $tag  Tag name
-     * @return  array   An array of entries that matches a certain pattern
+     * @param   array  $tag_items  Tag items
+     * @return  array  An array of entries that matches a certain pattern
      */
-    function Execute($tag)
+    function Execute($tag_items)
     {
+        if(!is_array($tag_items) || empty($tag_items)) {
+            return;
+        }
 
-        $table = Jaws_ORM::getInstance()->table('tags');
-        $table->select(
-            'linkdump_links.id as links_id:integer', 'linkdump_links.title',
-            'linkdump_links.description', 'linkdump_links.updatetime'
-        );
+        foreach($tag_items['link'] as $item) {
 
-        $table->join('tags_items', 'tags_items.tag', 'tags.id');
-        $table->join('linkdump_links', 'linkdump_links.id', 'tags_items.reference');
-        $table->where('tags.name', $tag)->and()->where('tags_items.gadget', 'LinkDump');
-        $table->and()->where('tags_items.action', 'link');
-
-        $result = $table->fetchAll();
+        }
+        $table = Jaws_ORM::getInstance()->table('linkdump_links');
+        $table->select('id:integer', 'title', 'description', 'updatetime');
+        $result = $table->where('id', $tag_items['link'], 'in')->fetchAll();
         if (Jaws_Error::IsError($result)) {
             return array();
         }
@@ -41,7 +38,7 @@ class LinkDump_Hooks_Tags extends Jaws_Gadget_Hook
         foreach ($result as $r) {
             $link = array();
             $link['title']   = $r['title'];
-            $link['url']     = $GLOBALS['app']->Map->GetURLFor('LinkDump', 'Link', array('id' => $r['links_id']));
+            $link['url']     = $GLOBALS['app']->Map->GetURLFor('LinkDump', 'Link', array('id' => $r['id']));
             $link['outer']   = true;
             $link['image']   = 'gadgets/LinkDump/images/logo.png';
             $link['snippet'] = $r['description'];
