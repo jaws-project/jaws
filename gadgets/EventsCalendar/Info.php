a97132515022707f649a87789afb372ce8f4e868
Mohsen Khahani <mkhahani@gmail.com>
2013-10-10 17:39:24 +0330

    [EventsCalendar]: initial commit

---
+++ gadgets/EventsCalendar/Info.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * EventsCalendar gadget info
+ *
+ * @category    GadgetInfo
+ * @package     EventsCalendar
+ * @author      Mohsen Khahani <mkhahani@gmail.com>
+ * @copyright   2013 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ */
+class EventsCalendar_Info extends Jaws_Gadget
+{
+    /**
+     * Default ACL value of frontend gadget access
+     *
+     * @var     bool
+     * @access  protected
+     */
+    var $default_acl = false;
+
+    /**
+     * Gadget version
+     *
+     * @var     string
+     * @access  private
+     */
+    var $version = '1.0.0';
+
+    /**
+     * Default front-end action name
+     *
+     * @var     string
+     * @access  protected
+     */
+    var $default_action = 'EventsCalendar';
+
+    /**
+     * Default back-end action name
+     *
+     * @var     string
+     * @access  protected
+     */
+    var $default_admin_action = 'EventsCalendar';
+}
---
+++ gadgets/EventsCalendar/Installer.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * EventsCalendar Installer
+ *
+ * @category    GadgetModel
+ * @package     EventsCalendar
+ * @author      Mohsen Khahani <mkhahani@gmail.com>
+ * @copyright   2013 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ */
+class EventsCalendar_Installer extends Jaws_Gadget_Installer
+{
+    /**
+     * Gadget Registry keys
+     *
+     * @var     array
+     * @access  private
+     */
+    var $_RegKeys = array(
+        array('notes_limit', '10'),
+    );
+
+    /**
+     * Gadget ACLs
+     *
+     * @var     array
+     * @access  private
+     */
+    var $_ACLKeys = array(
+    );
+
+    /**
+     * Installs the gadget
+     *
+     * @access  public
+     * @return  mixed   True on successful installation, Jaws_Error otherwise
+     */
+    function Install()
+    {
+        $result = $this->installSchema('schema.xml');
+        if (Jaws_Error::IsError($result)) {
+            return $result;
+        }
+
+        return true;
+    }
+
+    /**
+     * Uninstalls the gadget
+     *
+     * @access  public
+     * @return  mixed   True on success and Jaws_Error otherwise
+     */
+    function Uninstall()
+    {
+        $tables = array('ec_events', 'ec_users');
+        foreach ($tables as $table) {
+            $result = $GLOBALS['db']->dropTable($table);
+            if (Jaws_Error::IsError($result)) {
+                $gName  = _t('EVENTSCALENDAR_NAME');
+                $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $gName);
+                return new Jaws_Error($errMsg, $gName);
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Upgrades the gadget
+     *
+     * @access  public
+     * @param   string  $old    Current version (in registry)
+     * @param   string  $new    New version (in the $gadgetInfo file)
+     * @return  mixed   True on Success or Jaws_Error on Failure
+     */
+    function Upgrade($old, $new)
+    {
+        return true;
+    }
+}
