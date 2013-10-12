c37636c07392cac27e8788609832487ee48932d4
Ali Fazelzadeh <afz@php.net>
2013-10-12 20:50:21 +0330

    [Weather]: Fixed default action

--- gadgets/Weather/Info.php
+++ gadgets/Weather/Info.php
@@ -20,4 +20,12 @@ class Weather_Info extends Jaws_Gadget
      */
     var $version = '0.9.0';
 
+    /**
+     * Default front-end action name
+     *
+     * @var     string
+     * @access  protected
+     */
+    var $default_action = 'AllRegionsWeather';
+
 }
