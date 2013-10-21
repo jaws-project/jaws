b5770de4f25d5bae355e7236c374c94eb7921957
Ali Fazelzadeh <afz@php.net>
2013-10-21 15:08:09 +0330

    [Menu]: Fixed typo

--- gadgets/Menu/AdminAjax.php
+++ gadgets/Menu/AdminAjax.php
@@ -249,7 +249,7 @@ class Menu_AdminAjax extends Jaws_Gadget_HTML
             if (Jaws_Gadget::IsGadgetUpdated($request)) {
                 $objGadget = $GLOBALS['app']->LoadGadget($request, 'Info');
                 if (!Jaws_Error::IsError($objGadget)) {
-                    $objHook = $objGadget->loadHook->load('Menu');
+                    $objHook = $objGadget->loadHook('Menu');
                     if (!Jaws_Error::IsError($objHook)) {
                         return $objHook->Execute();
                     }
