2b6ef89d04a040d885b6f024abc820e1d7495ef4
Ali Fazelzadeh <afz@php.net>
2013-10-24 00:49:16 +0330

    [Banner]: Using new method for load model from other gadgets

--- gadgets/Banner/Installer.php
+++ gadgets/Banner/Installer.php
@@ -87,7 +87,7 @@ class Banner_Installer extends Jaws_Gadget_Installer
     function Upgrade($old, $new)
     {
         // Update layout actions
-        $layoutModel = $GLOBALS['app']->loadGadget('Layout', 'AdminModel', 'Layout');
+        $layoutModel = Jaws_Gadget::getInstance('Layout')->loadAdminModel('Layout');
         if (!Jaws_Error::isError($layoutModel)) {
             $layoutModel->EditGadgetLayoutAction('Banner', 'Display', 'Banners', 'Banners');
         }
