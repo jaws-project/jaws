bf9b7339dec96dd6e9beda3a5403c1cd18d43928
Ali Fazelzadeh <afz@php.net>
2013-10-24 00:54:33 +0330

    [ServerTime]: Using new method for load model from other gadgets

--- gadgets/ServerTime/Installer.php
+++ gadgets/ServerTime/Installer.php
@@ -53,7 +53,7 @@ class ServerTime_Installer extends Jaws_Gadget_Installer
     function Upgrade($old, $new)
     {
         // Update layout actions
-        $layoutModel = $GLOBALS['app']->loadGadget('Layout', 'AdminModel', 'Layout');
+        $layoutModel = Jaws_Gadget::getInstance('Layout')->loadAdminModel('Layout');
         if (!Jaws_Error::isError($layoutModel)) {
             $layoutModel->EditGadgetLayoutAction('ServerTime', 'Display', 'Display', 'ServerTime');
         }
