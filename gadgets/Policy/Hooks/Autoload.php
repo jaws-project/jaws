ce5535f79b31be2ab3b00f32107e6d1e75428886
Ali Fazelzadeh <afz@php.net>
2013-10-23 09:38:32 +0330

    [Policy]: Autoload hook extend from Jaws_Gadget_Hook

--- gadgets/Policy/Hooks/Autoload.php
+++ gadgets/Policy/Hooks/Autoload.php
@@ -9,7 +9,7 @@
  * @copyright  2007-2013 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/lesser.html
  */
-class Policy_Autoload
+class Policy_Hooks_Autoload extends Jaws_Gadget_Hook
 {
     /**
      * Autoload load method
