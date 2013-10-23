b7ecf49abcdb0d746f6f3c18b2d8e4a2c8549a1c
Ali Fazelzadeh <afz@php.net>
2013-10-24 00:50:45 +0330

    [FileBrowser]: Using new method for load model from other gadgets

--- gadgets/FileBrowser/Installer.php
+++ gadgets/FileBrowser/Installer.php
@@ -93,7 +93,7 @@ class FileBrowser_Installer extends Jaws_Gadget_Installer
     {
         if (version_compare($old, '0.9.0', '<')) {
             // Update layout actions
-            $layoutModel = $GLOBALS['app']->loadGadget('Layout', 'AdminModel', 'Layout');
+            $layoutModel = Jaws_Gadget::getInstance('Layout')->loadAdminModel('Layout');
             if (!Jaws_Error::isError($layoutModel)) {
                 $layoutModel->EditGadgetLayoutAction('FileBrowser', 'InitialFolder', 'InitialFolder', 'Directory');
             }
