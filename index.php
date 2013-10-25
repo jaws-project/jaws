ab6fec4b45d0678f9a4439355cd65fa3999b6672
Ali Fazelzadeh <afz@php.net>
2013-10-25 12:13:43 +0330

    Fixed php error

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ if (empty($ReqError) && $GLOBALS['app']->Map->Parse()) {
 
     if (!empty($ReqGadget)) {
         if (Jaws_Gadget::IsGadgetEnabled($ReqGadget)) {
-            $objGadget = Jaws_Gadget::getInstance($ReqGadget);
+            $objGadget = Jaws_Gadget::getInstance($ReqGadget)->loadAction();
             if (Jaws_Error::IsError($objGadget)) {
                 Jaws_Error::Fatal("Error loading gadget: $ReqGadget");
             }
