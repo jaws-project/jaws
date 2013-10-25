d07377e5fb4c04dafdf142b7d290221a622faabf
Ali Fazelzadeh <afz@php.net>
2013-10-25 14:14:45 +0330

    load old admin action class files

--- gadgets/StaticPage/Actions/Admin/Translation.php
+++ gadgets/StaticPage/Actions/Admin/Translation.php
@@ -1,4 +1,5 @@
 <?php
+require_once JAWS_PATH. 'gadgets/StaticPage/AdminAction.php';
 /**
  * StaticPage Gadget Admin
  *
--- gadgets/SysInfo/Actions/Admin/SysInfo.php
+++ gadgets/SysInfo/Actions/Admin/SysInfo.php
@@ -1,4 +1,5 @@
 <?php
+require_once JAWS_PATH. 'gadgets/SysInfo/AdminAction.php';
 /**
  * SysInfo Core Gadget Admin
  *
