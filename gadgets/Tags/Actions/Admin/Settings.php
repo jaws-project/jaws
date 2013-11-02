438813dc3e04ab84f4d9dd758a9d8e5110573755
Ali Fazelzadeh <afz@php.net>
2013-11-02 11:33:08 +0330

    [Tags]: Applied new autoload class file

--- gadgets/Tags/Actions/Admin/Settings.php
+++ gadgets/Tags/Actions/Admin/Settings.php
@@ -1,5 +1,4 @@
 <?php
-require_once JAWS_PATH. 'gadgets/Tags/Actions/Admin/Default.php';
 /**
  * Tags Gadget Admin
  *
--- gadgets/Tags/Actions/Tags.php
+++ gadgets/Tags/Actions/Tags.php
@@ -1,5 +1,4 @@
 <?php
-require_once JAWS_PATH. 'gadgets/Tags/Actions/Default.php';
 /**
  * Tags Gadget
  *
