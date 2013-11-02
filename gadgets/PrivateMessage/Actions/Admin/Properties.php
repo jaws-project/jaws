a67401d15c162fb1416d6c6e4430ea0512d53796
Ali Fazelzadeh <afz@php.net>
2013-11-02 11:34:35 +0330

    [PrivateMessage]: Applied new autoload class file

--- gadgets/PrivateMessage/Actions/Admin/Properties.php
+++ gadgets/PrivateMessage/Actions/Admin/Properties.php
@@ -1,5 +1,4 @@
 <?php
-require_once JAWS_PATH. 'gadgets/PrivateMessage/Actions/Admin/Default.php';
 /**
  * PrivateMessage Gadget Admin
  *
--- gadgets/PrivateMessage/Actions/AllMessages.php
+++ gadgets/PrivateMessage/Actions/AllMessages.php
@@ -1,5 +1,4 @@
 <?php
-require_once JAWS_PATH. 'gadgets/PrivateMessage/Actions/Default.php';
 /**
  * PrivateMessage Gadget
  *
