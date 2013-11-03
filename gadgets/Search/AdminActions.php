5492c2964e55dd4071c5ebad13d5198193f3cc4b
Mojtaba Ebrahimi <irangame@gmail.com>
2013-11-03 23:04:23 +0330

    [Search]: tail of last commit

---
+++ gadgets/Search/AdminActions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Weather Actions file
+ *
+ * @category    GadgetActions
+ * @package     Weather
+ * @author      Mojtaba Ebrahimi <ebrahimi@zehneziba.ir>
+ * @copyright   2012-2013 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ */
+$actions = array();
+
+$actions['Settings'] = array(
+    'normal' => true,
+    'file' => 'Settings',
+);
