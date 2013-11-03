72cf8411efea3dab8959efd3901f8dcdc7e269cd
Ali Fazelzadeh <afz@php.net>
2013-11-03 23:22:26 +0330

    [EventsCalendar]: Fixed default admin action

--- gadgets/EventsCalendar/Info.php
+++ gadgets/EventsCalendar/Info.php
@@ -34,11 +34,4 @@ class EventsCalendar_Info extends Jaws_Gadget
      */
     var $default_action = 'ViewYear';
 
-    /**
-     * Default back-end action name
-     *
-     * @var     string
-     * @access  protected
-     */
-    var $default_admin_action = 'Events';
 }
