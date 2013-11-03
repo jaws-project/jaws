dc8ce9b1132ee59d5b271ad698462d5f7f410e20
Mojtaba Ebrahimi <irangame@gmail.com>
2013-11-03 23:38:04 +0330

    Added AdminActions

---
+++ gadgets/Launcher/AdminActions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Launcher Actions
+ *
+ * @category    GadgetActions
+ * @package     Launcher
+ * @author      Mojtaba Ebrahimi <ebrahimi@zehneziba.ir>
+ * @copyright   2013 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ */
+$actions = array();
---
+++ gadgets/Notepad/AdminActions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Notepad Actions
+ *
+ * @category    GadgetActions
+ * @package     Notepad
+ * @author      Mojtaba Ebrahimi <ebrahimi@zehneziba.ir>
+ * @copyright   2013 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ */
+$actions = array();
