0f3f1f73d7b4786aabf73f5bda6b50ff14613741
Ali Fazelzadeh <afz@php.net>
2013-11-08 19:51:41 +0330

    [Search]: replaced loadHook/loadEvent/loadInstaller with hook->load()/event->load()/installer->load() methods

--- gadgets/Search/Model/Search.php
+++ gadgets/Search/Model/Search.php
@@ -54,7 +54,7 @@ class Search_Model_Search extends Jaws_Gadget_Model
                 if (Jaws_Error::IsError($objGadget)) {
                     continue;
                 }
-                $objHook = $objGadget->loadHook('Search');
+                $objHook = $objGadget->hook->load('Search');
                 if (Jaws_Error::IsError($objHook)) {
                     continue;
                 }
