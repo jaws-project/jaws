b636d20705732b0c233a28a750af904503970272
Ali Fazelzadeh <afz@php.net>
2013-11-09 18:16:56 +0330

    [Components]: Fixed js error

--- gadgets/Components/Resources/script.js
+++ gadgets/Components/Resources/script.js
@@ -13,7 +13,7 @@
  */
 var ComponentsCallback = {
     installgadget: function (response) {
-        if (response[0][type] == 'response_notice') {
+        if (response[0]['type'] == 'response_notice') {
             components[selectedComponent].state =
                 components[selectedComponent].core_gadget ? 'core' : 'installed';
             buildComponentList();
@@ -23,7 +23,7 @@ var ComponentsCallback = {
     },
 
     upgradegadget: function (response) {
-        if (response[0][type] == 'response_notice') {
+        if (response[0]['type'] == 'response_notice') {
             components[selectedComponent].state =
                 components[selectedComponent].core_gadget ? 'core' : 'installed';
             buildComponentList();
@@ -33,7 +33,7 @@ var ComponentsCallback = {
     },
 
     uninstallgadget: function (response) {
-        if (response[0][type] == 'response_notice') {
+        if (response[0]['type'] == 'response_notice') {
             components[selectedComponent].state = 'notinstalled';
             buildComponentList();
             closeUI();
@@ -42,7 +42,7 @@ var ComponentsCallback = {
     },
 
     enablegadget: function (response) {
-        if (response[0][type] == 'response_notice') {
+        if (response[0]['type'] == 'response_notice') {
             components[selectedComponent].state = 'installed';
             components[selectedComponent].disabled = false;
             buildComponentList();
@@ -52,7 +52,7 @@ var ComponentsCallback = {
     },
 
     disablegadget: function (response) {
-        if (response[0][type] == 'response_notice') {
+        if (response[0]['type'] == 'response_notice') {
             components[selectedComponent].state = 'installed';
             components[selectedComponent].disabled = true;
             buildComponentList();
@@ -62,7 +62,7 @@ var ComponentsCallback = {
     },
 
     installplugin: function (response) {
-        if (response[0][type] == 'response_notice') {
+        if (response[0]['type'] == 'response_notice') {
             components[selectedComponent].state = 'installed';
             buildComponentList();
             closeUI();
@@ -71,7 +71,7 @@ var ComponentsCallback = {
     },
 
     uninstallplugin: function (response) {
-        if (response[0][type] == 'response_notice') {
+        if (response[0]['type'] == 'response_notice') {
             components[selectedComponent].state = 'notinstalled';
             buildComponentList();
             closeUI();
@@ -80,7 +80,7 @@ var ComponentsCallback = {
     },
 
     updatepluginusage: function (response) {
-        if (response[0][type] == 'response_notice') {
+        if (response[0]['type'] == 'response_notice') {
             usageCache = $('plugin_usage').clone(true, true);
             if (regCache) {
                 regCache = null;
@@ -91,7 +91,7 @@ var ComponentsCallback = {
     },
 
     updateregistry: function (response) {
-        if (response[0][type] == 'response_notice') {
+        if (response[0]['type'] == 'response_notice') {
             regChanges = {};
             regCache = $('component_registry').clone(true, true);
             if (usageCache) {
@@ -103,7 +103,7 @@ var ComponentsCallback = {
     },
 
     updateacl: function (response) {
-        if (response[0][type] == 'response_notice') {
+        if (response[0]['type'] == 'response_notice') {
             aclChanges = {};
             aclCache = $('component_acl').clone(true, true);
         }
