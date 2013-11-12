e6b2cf8af210db8f3e3904f0b9c7ea32060c35b7
Ali Fazelzadeh <afz@php.net>
2013-11-12 15:53:43 +0330

    [Poll]: renamed moved class name

--- gadgets/Poll/Actions/Admin/Ajax.php
+++ gadgets/Poll/Actions/Admin/Ajax.php
@@ -9,7 +9,7 @@
  * @copyright  2005-2013 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
-class Poll_AdminAjax extends Jaws_Gadget_Action
+class Poll_Actions_Admin_Ajax extends Jaws_Gadget_Action
 {
     /**
      * Get a Poll
