9c6734b9654e0b54deda748e3ef7b4bbed5154c8
Ali Fazelzadeh <afz@php.net>
2013-11-12 15:53:43 +0330

    [Quotes]: renamed moved class name

--- gadgets/Quotes/Actions/Admin/Ajax.php
+++ gadgets/Quotes/Actions/Admin/Ajax.php
@@ -8,7 +8,7 @@
  * @copyright  2007-2013 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
-class Quotes_AdminAjax extends Jaws_Gadget_Action
+class Quotes_Actions_Admin_Ajax extends Jaws_Gadget_Action
 {
     /**
      * Gets data of the quote
