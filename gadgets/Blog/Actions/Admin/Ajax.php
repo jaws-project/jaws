0a4af67d657c3fc96867bd0a6d8b4a490dafe4c0
Ali Fazelzadeh <afz@php.net>
2013-11-12 15:53:35 +0330

    [Blog]: renamed moved class name

--- gadgets/Blog/Actions/Admin/Ajax.php
+++ gadgets/Blog/Actions/Admin/Ajax.php
@@ -9,7 +9,7 @@
  * @copyright  2005-2013 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
-class Blog_AdminAjax extends Jaws_Gadget_Action
+class Blog_Actions_Admin_Ajax extends Jaws_Gadget_Action
 {
 
     /**
