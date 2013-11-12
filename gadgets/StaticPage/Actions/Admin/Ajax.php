0518930b5dac16642a0043b6aa883f3a61ae7db7
Ali Fazelzadeh <afz@php.net>
2013-11-12 15:53:46 +0330

    [StaticPage]: renamed moved class name

--- gadgets/StaticPage/Actions/Admin/Ajax.php
+++ gadgets/StaticPage/Actions/Admin/Ajax.php
@@ -8,7 +8,7 @@
  * @copyright  2005-2013 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
-class StaticPage_AdminAjax extends Jaws_Gadget_Action
+class StaticPage_Actions_Admin_Ajax extends Jaws_Gadget_Action
 {
     /**
      * Deletes the page and all of its translations
