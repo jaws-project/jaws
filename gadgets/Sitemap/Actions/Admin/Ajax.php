f8c47d13b7f4b123a177ba54cb402c37dd1d6414
Ali Fazelzadeh <afz@php.net>
2013-11-12 15:53:45 +0330

    [Sitemap]: renamed moved class name

--- gadgets/Sitemap/Actions/Admin/Ajax.php
+++ gadgets/Sitemap/Actions/Admin/Ajax.php
@@ -9,7 +9,7 @@
  * @copyright  2006-2013 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
-class Sitemap_AdminAjax extends Jaws_Gadget_Action
+class Sitemap_Actions_Admin_Ajax extends Jaws_Gadget_Action
 {
     /**
      * Constructor
