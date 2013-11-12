b4061ba447dee34c2e52f7e160deee66bc60b3a8
Ali Fazelzadeh <afz@php.net>
2013-11-12 15:53:47 +0330

    [Tms]: renamed moved class name

--- gadgets/Tms/Actions/Admin/Ajax.php
+++ gadgets/Tms/Actions/Admin/Ajax.php
@@ -8,7 +8,7 @@
  * @copyright  2007-2013 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/lesser.html
  */
-class Tms_AdminAjax extends Jaws_Gadget_Action
+class Tms_Actions_Admin_Ajax extends Jaws_Gadget_Action
 {
     /**
      * Gets information of given theme
