5269ba3ef36288ba479824ed7be8fab799ff6c0e
Ali Fazelzadeh <afz@php.net>
2013-11-14 12:36:51 +0330

    [Emblems]: merged admin & index actions into one file

--- gadgets/Emblems/Actions.php
+++ gadgets/Emblems/Actions.php
@@ -14,3 +14,19 @@ $actions['Display'] = array(
     'layout' => true,
     'file'   => 'Emblems'
 );
+
+/**
+ * Admin actions
+ */
+$admin_actions['Emblems'] = array(
+    'normal' => true,
+    'file'   => 'Emblems'
+);
+$admin_actions['AddEmblem'] = array(
+    'normal' => true,
+    'file'   => 'Emblems'
+);
+$admin_actions['EditEmblem'] = array(
+    'normal' => true,
+    'file'   => 'Emblems'
+);
