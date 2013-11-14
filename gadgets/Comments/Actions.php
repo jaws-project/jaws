69f24b50660697d9e74ad6404cf9ce5f88c58699
Ali Fazelzadeh <afz@php.net>
2013-11-14 15:31:12 +0330

    [Comments]: cosmetics

--- gadgets/Comments/Actions.php
+++ gadgets/Comments/Actions.php
@@ -8,41 +8,37 @@
  * @copyright   2012-2013 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/lesser.html
  */
-$actions = array();
 
+/**
+ * Index actions
+ */
 $actions['Comments'] = array(
     'normal' => true,
     'file'   => 'Comments',
 );
-
 $actions['PostMessage'] = array(
     'normal' => true,
     'file'   => 'Comments',
 );
-
 $actions['Preview'] = array(
     'normal' => true,
     'file'   => 'Comments',
 );
-
 $actions['RecentComments'] = array(
     'normal' => true,
     'layout' => true,
     'file'   => 'RecentComments',
     'parametric' => true,
 );
-
 $actions['FeedsLink'] = array(
     'layout' => true,
     'parametric' => true,
     'file'   => 'Feeds',
 );
-
 $actions['RecentCommentsRSS'] = array(
     'standalone' => true,
     'file' => 'Feeds',
 );
-
 $actions['RecentCommentsAtom'] = array(
     'standalone' => true,
     'file' => 'Feeds',
