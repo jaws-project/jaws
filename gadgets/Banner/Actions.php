c306431ff10c8ce590b85ccfe086a2938851deaf
Ali Fazelzadeh <afz@php.net>
2013-11-14 12:36:46 +0330

    [Banner]: merged admin & index actions into one file

--- gadgets/Banner/Actions.php
+++ gadgets/Banner/Actions.php
@@ -17,3 +17,23 @@ $actions['Click'] = array(
     'normal' => true,
     'file' => 'Banners',
 );
+
+/**
+ * Admin actions
+ */
+$admin_actions['Banners'] = array(
+    'normal' => true,
+    'file' => 'Banners',
+);
+$admin_actions['Groups'] = array(
+    'normal' => true,
+    'file' => 'Groups',
+);
+$admin_actions['Reports'] = array(
+    'normal' => true,
+    'file' => 'Reports',
+);
+$admin_actions['UploadBanner'] = array(
+    'normal' => true,
+    'file' => 'Banners',
+);
