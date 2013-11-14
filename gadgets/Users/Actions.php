de404a91bbce9f4291ea01d3ecb562c43b060f8a
Ali Fazelzadeh <afz@php.net>
2013-11-14 15:31:23 +0330

    [Users]: cosmetics

--- gadgets/Users/Actions.php
+++ gadgets/Users/Actions.php
@@ -9,8 +9,10 @@
  * @copyright   2004-2013 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/lesser.html
  */
-$actions = array();
 
+/**
+ * Index actions
+ */
 $actions['LoginBox'] = array(
     'normal' => true,
     'layout' => true,
