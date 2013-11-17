d5d7b4db9a4f580fe08901c18ff586b3d9e57613
Ali Fazelzadeh <afz@php.net>
2013-11-17 15:35:04 +0330

    [Contact]: Applied new changes of Ajax methods definition

--- gadgets/Contact/Actions.php
+++ gadgets/Contact/Actions.php
@@ -61,3 +61,71 @@ $admin_actions['UploadFile'] = array(
     'standalone' => true,
     'file' => 'Mailer'
 );
+$admin_actions['GetContact'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['UpdateContact'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['UpdateReply'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['DeleteContact'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['GetReply'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['ReplyUI'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['GetRecipient'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['InsertRecipient'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['UpdateRecipient'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['DeleteRecipient'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['UpdateProperties'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['GetContacts'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['GetContactsCount'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['GetUsers'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['GetMessagePreview'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['SendEmail'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['GetData'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
