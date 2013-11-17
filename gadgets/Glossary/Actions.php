b7008bb95bace8b1b0b78d4f9fb324a5417c536b
Ali Fazelzadeh <afz@php.net>
2013-11-17 15:35:07 +0330

    [Glossary]: Applied new changes of Ajax methods definition

--- gadgets/Glossary/Actions.php
+++ gadgets/Glossary/Actions.php
@@ -32,3 +32,23 @@ $admin_actions['Term'] = array(
     'normal' => true,
     'file' => 'Term',
 );
+$admin_actions['GetTerm'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['NewTerm'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['UpdateTerm'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['DeleteTerm'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['ParseText'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
