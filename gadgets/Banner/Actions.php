2c364fe7de57ce417ebb64641cd19a4aab0df3d1
Ali Fazelzadeh <afz@php.net>
2013-11-17 15:35:03 +0330

    [Banner]: Applied new changes of Ajax methods definition

--- gadgets/Banner/Actions.php
+++ gadgets/Banner/Actions.php
@@ -39,3 +39,71 @@ $admin_actions['UploadBanner'] = array(
     'normal' => true,
     'file' => 'Banners',
 );
+$admin_actions['GetBanner'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['GetBanners'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['GetGroup'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['GetGroups'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['InsertBanner'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['UpdateBanner'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['InsertGroup'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['UpdateGroup'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['AddBannersToGroup'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['DeleteBanner'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['ResetViews'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['ResetClicks'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['DeleteGroup'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['GetGroupUI'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['GetGroupBannersUI'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['getBannersDataGrid'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
+$admin_actions['GetBannersCount'] = array(
+    'standalone' => true,
+    'file' => 'Ajax',
+);
