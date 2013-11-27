cf1b0d2a09d608981836921720961c463dbbae9f
Ali Fazelzadeh <afz@php.net>
2013-11-27 12:04:29 +0330

    [Shoutbox]: use native urlMap instead global GetURLFor

--- gadgets/Shoutbox/Hooks/Menu.php
+++ gadgets/Shoutbox/Hooks/Menu.php
@@ -19,7 +19,7 @@ class Shoutbox_Hooks_Menu extends Jaws_Gadget_Hook
      */
     function Execute()
     {
-        $urls[] = array('url'   => $GLOBALS['app']->Map->GetURLFor('Shoutbox', 'Comments'),
+        $urls[] = array('url'   => $this->gadget->urlMap('Comments'),
                         'title' => _t('SHOUTBOX_NAME'));
         return $urls;
     }
