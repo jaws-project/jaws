d02f014305c52606f4b716882605bf0f0364e8ad
Ali Fazelzadeh <afz@php.net>
2013-12-01 23:33:23 +0330

    [Settings]: Added actions log priority level

--- gadgets/Settings/Actions.php
+++ gadgets/Settings/Actions.php
@@ -15,48 +15,60 @@
 $admin_actions['BasicSettings'] = array(
     'normal' => true,
     'file' => 'Basic',
+    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['AdvancedSettings'] = array(
     'normal' => true,
     'file' => 'Advanced',
+    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['MetaSettings'] = array(
     'normal' => true,
     'file' => 'Meta',
+    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['MailSettings'] = array(
     'normal' => true,
     'file' => 'Mail',
+    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['FTPSettings'] = array(
     'normal' => true,
     'file' => 'FTP',
+    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['ProxySettings'] = array(
     'normal' => true,
     'file' => 'Proxy',
+    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['UpdateBasicSettings'] = array(
     'standalone' => true,
     'file' => 'Ajax',
+    'loglevel' => JAWS_WARNING,
 );
 $admin_actions['UpdateAdvancedSettings'] = array(
     'standalone' => true,
     'file' => 'Ajax',
+    'loglevel' => JAWS_WARNING,
 );
 $admin_actions['UpdateMetaSettings'] = array(
     'standalone' => true,
     'file' => 'Ajax',
+    'loglevel' => JAWS_WARNING,
 );
 $admin_actions['UpdateMailSettings'] = array(
     'standalone' => true,
     'file' => 'Ajax',
+    'loglevel' => JAWS_WARNING,
 );
 $admin_actions['UpdateFTPSettings'] = array(
     'standalone' => true,
     'file' => 'Ajax',
+    'loglevel' => JAWS_WARNING,
 );
 $admin_actions['UpdateProxySettings'] = array(
     'standalone' => true,
     'file' => 'Ajax',
+    'loglevel' => JAWS_WARNING,
 );
