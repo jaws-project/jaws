bd18edf81e6871d8affea597107a5eaa18f5a7bd
Mojtaba Ebrahimi <mojtaba.ebrahimi@outlook.com>
2013-12-02 20:53:48 +0330

    [Sitemap]: tail of last commit

---
+++ gadgets/Sitemap/Model/Admin/Ping.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Sitemap Gadget
+ *
+ * @category   GadgetModel
+ * @package    Sitemap
+ * @author     Jonathan Hernandez <ion@suavizado.com>
+ * @author     Pablo Fischer <pablo@pablo.com.mx>
+ * @copyright  2006-2013 Jaws Development Group
+ * @license    http://www.gnu.org/copyleft/gpl.html
+ */
+class Sitemap_Model_Admin_Ping extends Jaws_Gadget_Model
+{
+
+    /**
+     * Pings the sitemap.xml file to many search engines
+     *
+     * @access  public
+     * @return  bool    True
+     */
+    function PingSearchEngines()
+    {
+        $url = $this->gadget->urlMap('SitemapXML', array(), true);
+//        $url = htmlentities($this->gadget->urlMap('GetXML'), ENT_QUOTES, 'UTF-8');
+        $searchEngines = array(
+            'http://www.google.com/webmasters/tools/ping?sitemap={url}' => 'get',
+            'http://www.bing.com/ping?sitemap={url}' => 'get',
+//            'http://submissions.ask.com/ping?sitemap={url}' => 'get'
+        );
+
+        require_once PEAR_PATH . 'HTTP/Request.php';
+
+        $httpRequest = new HTTP_Request();
+        foreach ($searchEngines as $engine => $method) {
+            $method = strtolower($method);
+            if ($method == 'post') {
+                $httpRequest->setMethod(HTTP_REQUEST_METHOD_POST);
+            } else {
+                $httpRequest->setMethod(HTTP_REQUEST_METHOD_GET);
+            }
+            $engine = str_replace('{url}', $url, $engine);
+
+            $httpRequest->setURL($engine);
+            $resRequest = $httpRequest->sendRequest();
+
+            if (PEAR::isError($resRequest) || (int)$httpRequest->getResponseCode() <> 200) {
+                $GLOBALS['log']->Log(JAWS_LOG_INFO, 'Could not ping sitemap URL to: ' . $engine);
+            }
+        }
+        return true;
+    }
+
+}
