0e3a544f210e0e962941cb52690c94b39c63bf57
Ali Fazelzadeh <afz@php.net>
2013-12-07 19:30:45 +0330

    [EventsCalendar]: - renamed gadget name to gadget title, - removed passed extra parameter to Jaws_Error

--- gadgets/EventsCalendar/Installer.php
+++ gadgets/EventsCalendar/Installer.php
@@ -57,9 +57,8 @@ class EventsCalendar_Installer extends Jaws_Gadget_Installer
         foreach ($tables as $table) {
             $result = $GLOBALS['db']->dropTable($table);
             if (Jaws_Error::IsError($result)) {
-                $gName  = _t('EVENTSCALENDAR_NAME');
-                $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $gName);
-                return new Jaws_Error($errMsg, $gName);
+                $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $this->gadget->title);
+                return new Jaws_Error($errMsg);
             }
         }
 
