fcd35096c957de04ceeb13af480ec7601f0fd130
Ali Fazelzadeh <afz@php.net>
2013-12-07 19:30:42 +0330

    [AddressBook]: - renamed gadget name to gadget title, - removed passed extra parameter to Jaws_Error

--- gadgets/AddressBook/Installer.php
+++ gadgets/AddressBook/Installer.php
@@ -52,9 +52,8 @@ class AddressBook_Installer extends Jaws_Gadget_Installer
         foreach ($tables as $table) {
             $result = $GLOBALS['db']->dropTable($table);
             if (Jaws_Error::IsError($result)) {
-                $gName  = _t('ADDRESSBOOK_NAME');
-                $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $gName);
-                return new Jaws_Error($errMsg, $gName);
+                $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $this->gadget->title);
+                return new Jaws_Error($errMsg);
             }
         }
 
