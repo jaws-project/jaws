e4774072fabdd88ab496586450d8fe322876cbb8
Ali Fazelzadeh <afz@php.net>
2013-12-07 19:30:48 +0330

    [Logs]: - renamed gadget name to gadget title, - removed passed extra parameter to Jaws_Error

--- gadgets/Logs/Installer.php
+++ gadgets/Logs/Installer.php
@@ -70,9 +70,8 @@ class Logs_Installer extends Jaws_Gadget_Installer
         foreach ($tables as $table) {
             $result = $GLOBALS['db']->dropTable($table);
             if (Jaws_Error::IsError($result)) {
-                $gName  = _t('LOGS_TITLE');
-                $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $gName);
-                return new Jaws_Error($errMsg, $gName);
+                $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $this->gadget->title);
+                return new Jaws_Error($errMsg);
             }
         }
 
