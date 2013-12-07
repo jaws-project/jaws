db0e6c89988878446161768a4b9946f91737f812
Mohsen Khahani <mkhahani@gmail.com>
2013-12-07 19:14:39 +0330

    [Notepad]: add Menu Hooks

---
+++ gadgets/Notepad/Hooks/Menu.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Notepad - URL List gadget hook
+ *
+ * @category    GadgetHook
+ * @package     Notepad
+ * @author      Mohsen Khahani <mkhahani@gmail.com>
+ * @copyright   2013 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ */
+class Notepad_Hooks_Menu extends Jaws_Gadget_Hook
+{
+    /**
+     * Returns an array with all available items the Menu gadget can use
+     *
+     * @access  public
+     * @return  array   URLs array
+     */
+    function Execute()
+    {
+        $urls[] = array('url' => $this->gadget->urlMap('Notepad'),
+                        'title' => _t('NOTEPAD_NAME'));
+        return $urls;
+    }
+}
