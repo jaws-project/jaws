04631bbb19cf2b4a2a30120acdcafa36335a6126
Ali Fazelzadeh <afz@php.net>
2013-12-09 19:36:20 +0330

    [Jaws_Auth_Default]: Added detail of validation, merging auth and get attributes

--- include/Jaws/Auth/Default.php
+++ include/Jaws/Auth/Default.php
@@ -12,73 +12,97 @@
 class Jaws_Auth_Default
 {
     /**
-     * User model
-     * @access  private
-     */
-    var $_Model;
-
-    /**
-     * Authentication ID
-     * @access  private
-     */
-    var $_AuthID = 0;
-
-    /**
-     * Constructor
-     *
-     * @access  public
-     */
-    function Jaws_Auth_Default()
-    {
-        $this->_Model = $GLOBALS['app']->loadObject('Jaws_User');
-    }
-
-    /**
      * Authenticate user/password
      *
      * @access  public
      */
     function Auth($user, $password)
     {
-        $result = $this->_Model->Valid($user, $password, false);
-        if (!Jaws_Error::IsError($result)) {
-            $this->_AuthID = $result['id'];
-            return $this->_AuthID;
+        $userModel = $GLOBALS['app']->loadObject('Jaws_User');
+        $result = $userModel->GetUser($user, true, true, true, true);
+        if (Jaws_Error::IsError($result)) {
+            return $result;
         }
 
-        return $result;
-    }
-
-    /**
-     * Attributes of logged user
-     *
-     * @access  public
-     */
-    function GetAttributes()
-    {
-        $info = $this->_Model->GetUser($this->_AuthID, true, true, true);
-        if (Jaws_Error::IsError($info) || !isset($info['id'])) {
-            return false;
-        } else {
-            $groups = $this->_Model->GetGroupsOfUser($info['id']);
-            if (Jaws_Error::IsError($groups)) {
-                return false;
-            }
-
-            $info['groups'] = $groups;
-            $info['avatar'] = $this->_Model->GetAvatar(
-                $info['avatar'],
-                $info['email'],
-                48,
-                $info['last_update']
+        if (empty($result)) {
+            return Jaws_Error::raiseError(
+                _t('GLOBAL_ERROR_LOGIN_WRONG'),
+                __FUNCTION__,
+                JAWS_ERROR_NOTICE
             );
-            $info['superadmin'] = $info['superadmin'];
-            $info['internal']   = true;
-            // Update login time
-            $this->_Model->updateLoginTime($this->_AuthID);
         }
 
-        return $info;
+        // bad_password_count & lockedout time
+        $max_password_bad_count = $GLOBALS['app']->Registry->fetch('password_bad_count', 'Policy');
+        $password_lockedout_time = $GLOBALS['app']->Registry->fetch('password_lockedout_time', 'Policy');
+        if ($result['bad_password_count'] >= $max_password_bad_count &&
+           ((time() - $result['last_access']) <= $password_lockedout_time))
+        {
+            return Jaws_Error::raiseError(
+                _t('GLOBAL_ERROR_LOGIN_LOCKED_OUT'),
+                __FUNCTION__,
+                JAWS_ERROR_NOTICE
+            );
+        }
+
+        // check password
+        if ($result['password'] !== Jaws_User::GetHashedPassword($password, $result['password'])) {
+            $userModel->updateLastAccess($result['id'], false);
+            return Jaws_Error::raiseError(
+                _t('GLOBAL_ERROR_LOGIN_WRONG'),
+                __FUNCTION__,
+                JAWS_ERROR_NOTICE
+            );
+        }
+        unset($result['password']);
+
+        // status
+        if ($result['status'] !== 1) {
+            return Jaws_Error::raiseError(
+                _t('GLOBAL_ERROR_LOGIN_STATUS_'. $result['status']),
+                __FUNCTION__,
+                JAWS_ERROR_NOTICE
+            );
+        }
+
+        // expiry date
+        if (!empty($result['expiry_date']) && $result['expiry_date'] <= time()) {
+            return Jaws_Error::raiseError(
+                _t('GLOBAL_ERROR_LOGIN_EXPIRED'),
+                __FUNCTION__,
+                JAWS_ERROR_NOTICE
+            );
+        }
+
+        // logon hours
+        $wdhour = explode(',', $GLOBALS['app']->UTC2UserTime(time(), 'w,G', true));
+        $lhByte = hexdec($result['logon_hours']{$wdhour[0]*6 + intval($wdhour[1]/4)});
+        if ((pow(2, fmod($wdhour[1], 4)) & $lhByte) == 0) {
+            return Jaws_Error::raiseError(
+                _t('GLOBAL_ERROR_LOGIN_LOGON_HOURS'),
+                __FUNCTION__,
+                JAWS_ERROR_NOTICE
+            );
+        }
+
+        // fetch user groups
+        $groups = $userModel->GetGroupsOfUser($result['id']);
+        if (Jaws_Error::IsError($groups)) {
+            $groups = array();
+        }
+
+        $result['groups'] = $groups;
+        $result['avatar'] = $userModel->GetAvatar(
+            $result['avatar'],
+            $result['email'],
+            48,
+            $result['last_update']
+        );
+        $result['internal'] = true;
+
+        // update last access
+        $userModel->updateLastAccess($result['id'], true);
+        return $result;
     }
 
 }
