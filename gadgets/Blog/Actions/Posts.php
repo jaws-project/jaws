fcfd0c24cecfaafecf11a9ab2f502976945f370e
Ali Fazelzadeh <afz@php.net>
2013-12-16 15:42:25 +0330

    [Blog]: cosmetics

--- gadgets/Blog/Actions/Posts.php
+++ gadgets/Blog/Actions/Posts.php
@@ -152,64 +152,66 @@ class Blog_Actions_Posts extends Blog_Actions_Default
                 $title = _t('BLOG_RECENT_POSTS_BY_CATEGORY');
             }
         }
+        $entries = $pModel->GetRecentEntries($cat, (int)$limit);
+        if (Jaws_Error::IsError($entries) || empty($entries)) {
+            return false;
+        }
+
         $tpl = $this->gadget->template->load('RecentPosts.html');
         $tpl->SetBlock('recent_posts');
         $tpl->SetVariable('cat',   empty($cat)? '0' : $cat);
         $tpl->SetVariable('title', $title);
-        $entries = $pModel->GetRecentEntries($cat, (int)$limit);
-        if (!Jaws_Error::IsError($entries)) {
-            $date = Jaws_Date::getInstance();
-            foreach ($entries as $e) {
-                $tpl->SetBlock('recent_posts/item');
+        $date = Jaws_Date::getInstance();
+        foreach ($entries as $e) {
+            $tpl->SetBlock('recent_posts/item');
 
-                $id = empty($e['fast_url']) ? $e['id'] : $e['fast_url'];
-                $perm_url = $this->gadget->urlMap('SingleView', array('id' => $id));
+            $id = empty($e['fast_url']) ? $e['id'] : $e['fast_url'];
+            $perm_url = $this->gadget->urlMap('SingleView', array('id' => $id));
 
-                $summary = $e['summary'];
-                $text    = $e['text'];
+            $summary = $e['summary'];
+            $text    = $e['text'];
 
-                // for compatibility with old versions
-                $more_pos = Jaws_UTF8::strpos($text, '[more]');
-                if ($more_pos !== false) {
-                    $summary = Jaws_UTF8::substr($text, 0, $more_pos);
-                    $text    = Jaws_UTF8::str_replace('[more]', '', $text);
+            // for compatibility with old versions
+            $more_pos = Jaws_UTF8::strpos($text, '[more]');
+            if ($more_pos !== false) {
+                $summary = Jaws_UTF8::substr($text, 0, $more_pos);
+                $text    = Jaws_UTF8::str_replace('[more]', '', $text);
 
-                    // Update this entry to split summary and body of post
-                    $pModel->SplitEntry($e['id'], $summary, $text);
-                }
-
-                $summary = empty($summary)? $text : $summary;
-                $summary = $this->gadget->ParseText($summary);
-                $text    = $this->gadget->ParseText($text);
-
-                if (Jaws_UTF8::trim($text) != '') {
-                    $tpl->SetBlock('recent_posts/item/read-more');
-                    $tpl->SetVariable('url', $perm_url);
-                    $tpl->SetVariable('read_more', _t('BLOG_READ_MORE'));
-                    $tpl->ParseBlock('recent_posts/item/read-more');
-                }
-
-                $tpl->SetVariable('url', $perm_url);
-                $tpl->SetVariable('title', $e['title']);
-                $tpl->SetVariable('text', $summary);
-                $tpl->SetVariable('username', $e['username']);
-                $tpl->SetVariable('posted_by', _t('BLOG_POSTED_BY'));
-                $tpl->SetVariable('name', $e['nickname']);
-                $tpl->SetVariable(
-                    'author-url',
-                    $this->gadget->urlMap('ViewAuthorPage', array('id' => $e['username']))
-                );
-                $tpl->SetVariable('createtime', $date->Format($e['publishtime']));
-                $tpl->SetVariable('createtime-monthname', $date->Format($e['publishtime'], 'MN'));
-                $tpl->SetVariable('createtime-month', $date->Format($e['publishtime'], 'm'));
-                $tpl->SetVariable('createtime-day', $date->Format($e['publishtime'], 'd'));
-                $tpl->SetVariable('createtime-year', $date->Format($e['publishtime'], 'Y'));
-                $tpl->SetVariable('createtime-time', $date->Format($e['publishtime'], 'g:ia'));
-                $tpl->ParseBlock('recent_posts/item');
+                // Update this entry to split summary and body of post
+                $pModel->SplitEntry($e['id'], $summary, $text);
             }
-        }
-        $tpl->ParseBlock('recent_posts');
 
+            $summary = empty($summary)? $text : $summary;
+            $summary = $this->gadget->ParseText($summary);
+            $text    = $this->gadget->ParseText($text);
+
+            if (Jaws_UTF8::trim($text) != '') {
+                $tpl->SetBlock('recent_posts/item/read-more');
+                $tpl->SetVariable('url', $perm_url);
+                $tpl->SetVariable('read_more', _t('BLOG_READ_MORE'));
+                $tpl->ParseBlock('recent_posts/item/read-more');
+            }
+
+            $tpl->SetVariable('url', $perm_url);
+            $tpl->SetVariable('title', $e['title']);
+            $tpl->SetVariable('text', $summary);
+            $tpl->SetVariable('username', $e['username']);
+            $tpl->SetVariable('posted_by', _t('BLOG_POSTED_BY'));
+            $tpl->SetVariable('name', $e['nickname']);
+            $tpl->SetVariable(
+                'author-url',
+                $this->gadget->urlMap('ViewAuthorPage', array('id' => $e['username']))
+            );
+            $tpl->SetVariable('createtime', $date->Format($e['publishtime']));
+            $tpl->SetVariable('createtime-monthname', $date->Format($e['publishtime'], 'MN'));
+            $tpl->SetVariable('createtime-month', $date->Format($e['publishtime'], 'm'));
+            $tpl->SetVariable('createtime-day', $date->Format($e['publishtime'], 'd'));
+            $tpl->SetVariable('createtime-year', $date->Format($e['publishtime'], 'Y'));
+            $tpl->SetVariable('createtime-time', $date->Format($e['publishtime'], 'g:ia'));
+            $tpl->ParseBlock('recent_posts/item');
+        }
+
+        $tpl->ParseBlock('recent_posts');
         return $tpl->Get();
     }
 
