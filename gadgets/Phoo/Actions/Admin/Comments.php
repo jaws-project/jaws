11c769879a4d3dc4c7e382fafda9286104a73e64
Ali Fazelzadeh <afz@php.net>
2013-12-19 17:02:35 +0330

    [Phoo]: Fixed gadget name

--- gadgets/Phoo/Actions/Admin/Comments.php
+++ gadgets/Phoo/Actions/Admin/Comments.php
@@ -13,19 +13,7 @@
 class Phoo_Actions_Admin_Comments extends Phoo_Actions_Admin_Default
 {
     /**
-     * Prepares the comments datagrid of an advanced search
-     *
-     * @access  public
-     * @return  string  The XHTML template content of a datagrid
-     */
-    function CommentsDatagrid()
-    {
-        $cHtml = Jaws_Gadget::getInstance('Comments')->action->loadAdmin('Comments');
-        return $cHtml->Get($this->gadget->name);
-    }
-
-    /**
-     * Displays blog comments manager
+     * Displays photo comments manager
      *
      * @access  public
      * @return  string  XHTML template content
@@ -41,7 +29,7 @@ class Phoo_Actions_Admin_Comments extends Phoo_Actions_Admin_Default
         $GLOBALS['app']->Layout->AddScriptLink('gadgets/Comments/Resources/script.js');
 
         $cHTML = Jaws_Gadget::getInstance('Comments')->action->loadAdmin('Comments');
-        return $cHTML->Comments('phoo', $this->MenuBar('ManageComments'));
+        return $cHTML->Comments($this->gadget->name, $this->MenuBar('ManageComments'));
     }
 
 }
