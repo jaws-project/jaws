025ab84cf06eedb4dc2cff12cadf9dbc82cc4e48
Ali Fazelzadeh <afz@php.net>
2013-12-29 16:24:58 +0330

    Allowed upgrade from 0.9.0 and 0.9.1

--- upgrade/stages/Report.php
+++ upgrade/stages/Report.php
@@ -34,6 +34,8 @@ class Upgrader_Report extends JawsUpgraderStage
             array('version' => '1.0.0', 'stage' => '6'),
             array('version' => '0.9.3', 'stage' => null),
             array('version' => '0.9.2', 'stage' => null),
+            array('version' => '0.9.1', 'stage' => null),
+            array('version' => '0.9.0', 'stage' => null),
         );
 
         _log(JAWS_LOG_DEBUG,"Checking/Reporting previous missed installations");
@@ -100,7 +102,7 @@ class Upgrader_Report extends JawsUpgraderStage
      */
     function Run()
     {
-        if (version_compare($_SESSION['upgrade']['InstalledVersion'], '0.9.2' , '<')) {
+        if (version_compare($_SESSION['upgrade']['InstalledVersion'], '0.9.0' , '<')) {
             return Jaws_Error::raiseError(_t('UPGRADE_REPORT_NOT_SUPPORTED'), 0, JAWS_ERROR_WARNING);
         }
 
