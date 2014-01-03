6416218eafc3dfc3e7807ec5df68c1cf558712b9
Ali Fazelzadeh <afz@php.net>
2014-01-03 17:55:00 +0330

    [Webcam]: @2014

--- gadgets/Webcam/Model/Admin/Webcam.php
+++ gadgets/Webcam/Model/Admin/Webcam.php
@@ -6,7 +6,7 @@
  * @package    Webcam
  * @author     Jonathan Hernandez <ion@suavizado.com>
  * @author     Pablo Fischer <pablo@pablo.com.mx>
- * @copyright  2004-2013 Jaws Development Group
+ * @copyright  2004-2014 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class Webcam_Model_Admin_Webcam extends Jaws_Gadget_Model
