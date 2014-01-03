622c0c95f63c865825e91700bd60168e856327b4
Ali Fazelzadeh <afz@php.net>
2014-01-03 17:55:19 +0330

    [Emoticons]: @2014

--- plugins/Emoticons/Plugin.php
+++ plugins/Emoticons/Plugin.php
@@ -5,7 +5,7 @@
  * @category   Plugin
  * @package    Emoticons
  * @author     Pablo Fischer <pablo@pablo.com.mx>
- * @copyright  2004-2013 Jaws Development Group
+ * @copyright  2004-2014 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class Emoticons_Plugin extends Jaws_Plugin
