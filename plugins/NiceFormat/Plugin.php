b553dc01439b6f4c1eaede87ccf09b95420583ff
Ali Fazelzadeh <afz@php.net>
2014-01-03 17:55:21 +0330

    [NiceFormat]: @2014

--- plugins/NiceFormat/Plugin.php
+++ plugins/NiceFormat/Plugin.php
@@ -5,7 +5,7 @@
  * @category   Plugin
  * @package    NiceFormat
  * @author     Pablo Fischer <pablo@pablo.com.mx>
- * @copyright  2004-2013 Jaws Development Group
+ * @copyright  2004-2014 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class NiceFormat_Plugin extends Jaws_Plugin
