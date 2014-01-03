2d98f41ac71d757f8d09c435fb09110adf0c19bd
Ali Fazelzadeh <afz@php.net>
2014-01-03 17:55:18 +0330

    [AccessLimiter]: @2014

--- plugins/AccessLimiter/Plugin.php
+++ plugins/AccessLimiter/Plugin.php
@@ -5,7 +5,7 @@
  * @category   Plugin
  * @package    AccessLimiter
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2009-2013 Jaws Development Group
+ * @copyright  2009-2014 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class AccessLimiter_Plugin extends Jaws_Plugin
