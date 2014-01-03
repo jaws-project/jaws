1262270396bf70592165beb552856ac70acddadb
Ali Fazelzadeh <afz@php.net>
2014-01-03 17:54:59 +0330

    [VisitCounter]: @2014

--- gadgets/VisitCounter/Hooks/Autoload.php
+++ gadgets/VisitCounter/Hooks/Autoload.php
@@ -6,7 +6,7 @@
  * @package    VisitCounter
  * @author     Amir Mohammad Saied <amir@php.net>
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2007-2013 Jaws Development Group
+ * @copyright  2007-2014 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class VisitCounter_Hooks_Autoload extends Jaws_Gadget_Hook
