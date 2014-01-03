c43b6243222255e1a5046841c9eecfaff4850211
Ali Fazelzadeh <afz@php.net>
2014-01-03 17:54:56 +0330

    [Skeleton]: @2014

--- gadgets/Skeleton/Info.php
+++ gadgets/Skeleton/Info.php
@@ -5,7 +5,7 @@
  * @category   GadgetModel
  * @package    Skeleton
  * @author     Jon Wood <jon@substance-it.co.uk>
- * @copyright  2004-2013 Jaws Development Group
+ * @copyright  2004-2014 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class Skeleton_Info extends Jaws_Gadget
