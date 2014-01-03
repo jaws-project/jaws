5c3e7fab0931dfc6e1f7dac0e73a995a4db84a90
Ali Fazelzadeh <afz@php.net>
2014-01-03 17:54:57 +0330

    [SysInfo]: @2014

--- gadgets/SysInfo/Actions/Admin/SysInfo.php
+++ gadgets/SysInfo/Actions/Admin/SysInfo.php
@@ -5,7 +5,7 @@
  * @category   GadgetAdmin
  * @package    SysInfo
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2008-2013 Jaws Development Group
+ * @copyright  2008-2014 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/lesser.html
  */
 class SysInfo_Actions_Admin_SysInfo extends SysInfo_Actions_Admin_Default
