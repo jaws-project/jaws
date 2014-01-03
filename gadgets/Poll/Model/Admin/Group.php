569b9740bbaf5ebfb4df46697eae1b6d00b602cb
Ali Fazelzadeh <afz@php.net>
2014-01-03 17:54:53 +0330

    [Poll]: @2014

--- gadgets/Poll/Model/Admin/Group.php
+++ gadgets/Poll/Model/Admin/Group.php
@@ -6,7 +6,7 @@
  * @package    Poll
  * @author     Pablo Fischer <pablo@pablo.com.mx>
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2005-2013 Jaws Development Group
+ * @copyright  2005-2014 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class Poll_Model_Admin_Group extends Poll_Model_Group
