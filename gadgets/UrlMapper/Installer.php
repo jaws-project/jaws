164d642a4170751a54977957f35c2c7b22380617
Ali Fazelzadeh <afz@php.net>
2014-01-08 00:58:35 +0330

    [UrlMapper]: Added upgrade method

--- gadgets/UrlMapper/Installer.php
+++ gadgets/UrlMapper/Installer.php
@@ -72,40 +72,11 @@ class UrlMapper_Installer extends Jaws_Gadget_Installer
      */
     function Upgrade($old, $new)
     {
-        if (version_compare($old, '1.0.0', '<')) {
-            $result = $this->installSchema('schema.xml', '', '0.3.2.xml');
+        if (version_compare($old, '1.1.0', '<')) {
+            $result = $this->installSchema('schema.xml', '', '1.0.0.xml');
             if (Jaws_Error::IsError($result)) {
                 return $result;
             }
-
-            // Update all gadgets maps
-            $umapModel = $this->gadget->model->loadAdmin('Maps');
-            $gadgets = $GLOBALS['app']->Registry->fetch('gadgets_installed_items');
-            $gadgets = array_filter(explode(',', $gadgets));
-            foreach ($gadgets as $gadget) {
-                $res = $umapModel->UpdateGadgetMaps($gadget);
-                if (Jaws_Error::IsError($res)) {
-                    return $res;
-                }
-            }
-
-            $mapsTable = Jaws_ORM::getInstance()->table('url_maps');
-            $mapsTable->delete()->where('vars_regexps', null, 'is null')->exec();
-
-            // ACL keys
-            $this->gadget->acl->insert('ManageMaps');
-            $this->gadget->acl->insert('ManageAliases');
-            $this->gadget->acl->insert('ManageErrorMaps');
-            $this->gadget->acl->insert('ManageProperties');
-            $this->gadget->acl->delete('EditMaps');
-
-            // Remove old event listener
-            $this->gadget->event->delete();
-            // Install listener for Add/Upgrade/Removing gadget's maps
-            $this->gadget->event->insert('InstallGadget');
-            $this->gadget->event->insert('UpgradeGadget');
-            $this->gadget->event->insert('UninstallGadget');
-            $this->gadget->event->insert('HTTPError');
         }
 
         return true;
