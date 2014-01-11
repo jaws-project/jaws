7d64a4de948d4645f443d8ab621bc1b9389408dc
Ali Fazelzadeh <afz@php.net>
2014-01-12 02:52:49 +0330

    [VisitCounter]: clenup old upgrade stuff

--- gadgets/VisitCounter/Info.php
+++ gadgets/VisitCounter/Info.php
@@ -18,7 +18,7 @@ class VisitCounter_Info extends Jaws_Gadget
      * @var     string
      * @access  private
      */
-    var $version = '0.9.0';
+    var $version = '1.0.0';
 
     /**
      * Default front-end action name
