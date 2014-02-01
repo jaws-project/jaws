7683ec7bdaf98c2d053eef49f59160f64c56f3cf
Ali Fazelzadeh <afz@php.net>
2014-02-01 05:31:30 +0330

    [PrivateMessage]: revert removed default admin action

---
+++ gadgets/PrivateMessage/Actions/Admin/Default.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * PrivateMessage Gadget Admin
+ *
+ * @category    GadgetAdmin
+ * @package     PrivateMessage
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2013-2014 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ */
+class PrivateMessage_Actions_Admin_Default extends Jaws_Gadget_Action
+{
+    /**
+     * Builds the users menubar
+     *
+     * @access  public
+     * @param   string  $action   Selected action
+     * @return  string  XHTML menubar
+     */
+    function MenuBar($action)
+    {
+        $actions = array('Properties');
+        if (!in_array($action, $actions)) {
+            $action = 'Properties';
+        }
+
+        $menubar = new Jaws_Widgets_Menubar();
+        if ($this->gadget->GetPermission('ManageProperties')) {
+            $menubar->AddOption('Properties',
+                                _t('GLOBAL_PROPERTIES'),
+                                BASE_SCRIPT . '?gadget=PrivateMessage&amp;action=Properties',
+                                STOCK_PREFERENCES);
+        }
+        $menubar->Activate($action);
+        return $menubar->Get();
+    }
+
+}
---
+++ gadgets/PrivateMessage/Actions/Admin/Properties.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * PrivateMessage Gadget Admin
+ *
+ * @category    GadgetAdmin
+ * @package     PrivateMessage
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2013-2014 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ */
+class PrivateMessage_Actions_Admin_Properties extends PrivateMessage_Actions_Admin_Default
+{
+    /**
+     * Builds admin properties UI
+     *
+     * @access  public
+     * @return  string  XHTML form
+     */
+    function Properties()
+    {
+        $this->gadget->CheckPermission('ManageProperties');
+        $this->AjaxMe('script.js');
+
+        return;
+    }
+
+}
