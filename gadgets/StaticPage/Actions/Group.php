4ad7e7ba6c385b6c5e4c494aa0f98f3bb2128094
Ali Fazelzadeh <afz@php.net>
2014-02-05 21:04:21 +0330

    [StaticPage]: Fixed conflict between its normal and layout actions

--- gadgets/StaticPage/Actions/Group.php
+++ gadgets/StaticPage/Actions/Group.php
@@ -59,8 +59,8 @@ class StaticPage_Actions_Group extends Jaws_Gadget_Action
      */
     function GroupPages($gid = 0, $orderBy = 1)
     {
-        $get = jaws()->request->fetch(array('gid', 'order'), 'get');
-        if (!empty($get['gid'])) {
+        if (empty($gid)) {
+            $get = $this->gadget->request->fetch(array('gid', 'order'), 'get');
             $gid = Jaws_XSS::defilter($get['gid']);
             $orderBy = $get['order'];
         }
