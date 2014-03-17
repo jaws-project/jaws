855645aa50a03d026c7c298e00f5777249cd8337
Ali Fazelzadeh <afz@php.net>
2014-03-17 13:22:03 +0430

    [Users]: set preferences size/direction

--- gadgets/Users/Actions/Preferences.php
+++ gadgets/Users/Actions/Preferences.php
@@ -73,16 +73,17 @@ class Users_Actions_Preferences extends Jaws_Gadget_Action
                 $tpl->SetVariable('key_title', $options[$key_name]['title']);
                 if (@isset($options[$key_name]['values'])) {
                     $element =& Piwi::CreateWidget('Combo', $key_name);
-                    $element->SetID('');
                     $element->AddOption(_t('USERS_ADVANCED_OPTS_NOT_YET'), '');
                     foreach ($options[$key_name]['values'] as $value => $title) {
                         $element->AddOption($title, $value);
                     }
                 } else {
                     $element =& Piwi::CreateWidget('Entry', $key_name);
-                    $element->SetID('');
                 }
 
+                $element->SetID('');
+                $options[$key_name]['size'] = @$options[$key_name]['size']?: 'x-large';
+                $element->SetClass($options[$key_name]['size']. (isset($options[$key_name]['ltr'])? ' ltr' : ''));
                 $element->SetValue(isset($customized[$key_name])? $customized[$key_name] : '');
                 $tpl->SetVariable('input_value', $element->Get());
 
