4e4b01566106288c4d23a4093280cfdcfb662fb6
Ali Fazelzadeh <afz@php.net>
2014-03-17 23:36:53 +0430

    [Forums]: cosmetics

--- gadgets/Forums/Hooks/Activity.php
+++ gadgets/Forums/Hooks/Activity.php
@@ -24,7 +24,7 @@ class Forums_Hooks_Activity extends Jaws_Gadget_Hook
         $topicCount = $model->GetUserTopicCount($uid);
 
         if ($topicCount > 0) {
-            $entity[0]['title'] = _t('FORUMS_TOPICS');
+            $entity[0]['title'] = _t('FORUMS_TOPIC');
             $entity[0]['count'] = $topicCount;
             $entity[0]['url'] = $this->gadget->urlMap('UserTopics', array('user' => $uname));
         }
@@ -32,7 +32,7 @@ class Forums_Hooks_Activity extends Jaws_Gadget_Hook
         $model = $this->gadget->model->load('Posts');
         $postCount = $model->GetUserPostsCount($uid);
         if ($postCount > 0) {
-            $entity[1]['title'] = _t('FORUMS_POSTS');
+            $entity[1]['title'] = _t('FORUMS_POST');
             $entity[1]['count'] = $postCount;
             $entity[1]['url'] = $this->gadget->urlMap('UserPosts', array('user' => $uname));
         }
