ba521cf41d56f16b8ab7bfcf478d106529113666
Ali Fazelzadeh <afz@php.net>
2014-08-14 17:09:49 +0430

    [Notepad]: compatibility with new changes in Jaws_DB

--- gadgets/Notepad/Installer.php
+++ gadgets/Notepad/Installer.php
@@ -55,7 +55,7 @@ class Notepad_Installer extends Jaws_Gadget_Installer
     {
         $tables = array('notepad', 'notepad_users');
         foreach ($tables as $table) {
-            $result = $GLOBALS['db']->dropTable($table);
+            $result = Jaws_DB::getInstance()->dropTable($table);
             if (Jaws_Error::IsError($result)) {
                 $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $this->gadget->title);
                 return new Jaws_Error($errMsg);
