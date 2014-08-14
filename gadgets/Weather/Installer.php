e496d61aaa3cb406031e76cf9a09cce6af1c8cd8
Ali Fazelzadeh <afz@php.net>
2014-08-14 17:09:54 +0430

    [Weather]: compatibility with new changes in Jaws_DB

--- gadgets/Weather/Installer.php
+++ gadgets/Weather/Installer.php
@@ -67,7 +67,7 @@ class Weather_Installer extends Jaws_Gadget_Installer
      */
     function Uninstall()
     {
-        $result = $GLOBALS['db']->dropTable('weather');
+        $result = Jaws_DB::getInstance()->dropTable('weather');
         if (Jaws_Error::IsError($result)) {
             $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $this->gadget->title);
             return new Jaws_Error($errMsg);
