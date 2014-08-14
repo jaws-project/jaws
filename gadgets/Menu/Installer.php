996327dba8649250a6e05844210f3051cfbaeb9c
Ali Fazelzadeh <afz@php.net>
2014-08-14 17:09:49 +0430

    [Menu]: compatibility with new changes in Jaws_DB

--- gadgets/Menu/Installer.php
+++ gadgets/Menu/Installer.php
@@ -77,7 +77,7 @@ class Menu_Installer extends Jaws_Gadget_Installer
         $tables = array('menus',
                         'menus_groups');
         foreach ($tables as $table) {
-            $result = $GLOBALS['db']->dropTable($table);
+            $result = Jaws_DB::getInstance()->dropTable($table);
             if (Jaws_Error::IsError($result)) {
                 $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $this->gadget->title);
                 return new Jaws_Error($errMsg);
