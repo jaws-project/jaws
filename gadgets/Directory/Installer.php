fd5671755d9e36b7c34eecc4c0888ba6615bff41
Ali Fazelzadeh <afz@php.net>
2014-08-14 17:09:46 +0430

    [Directory]: compatibility with new changes in Jaws_DB

--- gadgets/Directory/Installer.php
+++ gadgets/Directory/Installer.php
@@ -51,7 +51,7 @@ class Directory_Installer extends Jaws_Gadget_Installer
     {
         $tables = array('directory');
         foreach ($tables as $table) {
-            $result = $GLOBALS['db']->dropTable($table);
+            $result = Jaws_DB::getInstance()->dropTable($table);
             if (Jaws_Error::IsError($result)) {
                 $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $this->gadget->title);
                 return new Jaws_Error($errMsg);
