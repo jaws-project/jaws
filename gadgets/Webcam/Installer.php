0c6c73ccb0f0150a4c6c511fd15bdf2cd8a10b9a
Ali Fazelzadeh <afz@php.net>
2014-08-14 17:09:54 +0430

    [Webcam]: compatibility with new changes in Jaws_DB

--- gadgets/Webcam/Installer.php
+++ gadgets/Webcam/Installer.php
@@ -57,7 +57,7 @@ class Webcam_Installer extends Jaws_Gadget_Installer
      */
     function Uninstall()
     {
-        $result = $GLOBALS['db']->dropTable('webcam');
+        $result = Jaws_DB::getInstance()->dropTable('webcam');
         if (Jaws_Error::IsError($result)) {
             $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $this->gadget->title);
             return new Jaws_Error($errMsg);
