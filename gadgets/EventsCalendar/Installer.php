5c41d8212d4d467cf92292adbc0b8063a0b611ac
Ali Fazelzadeh <afz@php.net>
2014-08-14 17:09:47 +0430

    [EventsCalendar]: compatibility with new changes in Jaws_DB

--- gadgets/EventsCalendar/Installer.php
+++ gadgets/EventsCalendar/Installer.php
@@ -55,7 +55,7 @@ class EventsCalendar_Installer extends Jaws_Gadget_Installer
     {
         $tables = array('ec_events', 'ec_recurrences', 'ec_users');
         foreach ($tables as $table) {
-            $result = $GLOBALS['db']->dropTable($table);
+            $result = Jaws_DB::getInstance()->dropTable($table);
             if (Jaws_Error::IsError($result)) {
                 $errMsg = _t('GLOBAL_ERROR_GADGET_NOT_UNINSTALLED', $this->gadget->title);
                 return new Jaws_Error($errMsg);
