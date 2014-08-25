d819cce0156c51b68b7e174bed31c8f7bcd8e53b
Ali Fazelzadeh <afz@php.net>
2014-08-25 12:01:35 +0430

    [Blog]: update for comments hook

--- gadgets/Blog/Hooks/Comments.php
+++ gadgets/Blog/Hooks/Comments.php
@@ -4,8 +4,8 @@
  *
  * @category   GadgetHook
  * @package    Blog
- * @author     Pablo Fischer <pablo@pablo.com.mx>
- * @copyright  2007-2014 Jaws Development Group
+ * @author     Mojtaba Ebrahimi <ebrahimi@zehneziba.ir>
+ * @copyright  2014 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class Blog_Hooks_Comments extends Jaws_Gadget_Hook
@@ -15,24 +15,28 @@ class Blog_Hooks_Comments extends Jaws_Gadget_Hook
      *
      * @access  public
      * @param   string  $action     Action name
-     * @param   int     $id         Reference id
+     * @param   int     $reference  Reference id
      * @return  array   entry info
      */
-    function Execute($action, $id)
+    function Execute($action, $reference)
     {
-        $entryInfo = array();
+        $result = array();
         if ($action == 'Post') {
-            //Blog model
             $pModel = $this->gadget->model->load('Posts');
-            $entry = $pModel->GetEntry($id);
-
-            $url = $this->gadget->urlMap(
-                'SingleView',
-                array('id' => empty($entry['fast_url']) ? $entry['id'] : $entry['fast_url'])
-            );
-            $entryInfo = array('title' => $entry['title'], 'url' => $url);
+            $post = $pModel->GetEntry($reference);
+            if (!Jaws_Error::IsError($post) && !empty($post)) {
+                $url = $this->gadget->urlMap(
+                    'SingleView',
+                    array('id' => empty($post['fast_url']) ? $post['id'] : $post['fast_url'])
+                );
+                $result = array(
+                    'title' => $post['title'],
+                    'url' => $url
+                );
+            }
         }
 
-        return $entryInfo;
+        return $result;
     }
+
 }
