234e6812afe5890f5148accb51ab191317575034
Mojtaba Ebrahimi <mojtaba.ebrahimi@outlook.com>
2014-08-29 11:36:05 +0430

    [Shoutbox]: added new comments hook

---
+++ gadgets/Shoutbox/Hooks/Comments.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Shoutbox - Comments gadget hook
+ *
+ * @category   GadgetHook
+ * @package    Shoutbox
+ * @author     Mojtaba Ebrahimi <ebrahimi@zehneziba.ir>
+ * @copyright  2014 Jaws Development Group
+ * @license    http://www.gnu.org/copyleft/gpl.html
+ */
+class Shoutbox_Hooks_Comments extends Jaws_Gadget_Hook
+{
+    /**
+     * Returns an array about a Shoutbox
+     *
+     * @access  public
+     * @param   string  $action     Action name
+     * @param   int     $reference  Reference id
+     * @return  array   entry info
+     */
+    function Execute($action, $reference)
+    {
+        return array(
+            'title' => '',
+            'url' => ''
+        );
+    }
+}
