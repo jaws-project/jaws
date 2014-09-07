e6107e41075fe62ce7da69b1afad90d2f9bccdcc
Mohsen Khahani <mkhahani@gmail.com>
2014-09-07 18:47:37 +0430

    cosmetic

--- gadgets/Notification/Events/Notify.php
+++ gadgets/Notification/Events/Notify.php
@@ -14,7 +14,8 @@ class Notification_Events_Notify extends Jaws_Gadget_Event
      * Grabs notification and sends it out via available drivers
      *
      * @access  public
-     * @params  array   $params  [user, title, summary, description, priority, send]
+     * @params  string  $shouter    The shouting gadget
+     * @params  array   $params     [user, title, summary, description, priority, send]
      */
     function Execute($shouter, $params)
     {
@@ -22,11 +23,13 @@ class Notification_Events_Notify extends Jaws_Gadget_Event
             return;
         }
 
-        $drivers = glob(JAWS_PATH . 'include/Jaws/Notification/*.php');
-
         $jUser = new Jaws_User;
-        $user = $jUser->GetUser($params['user'], false, false, true);
+        $user = $jUser->GetUser($params['user'], true, false, true);
+        if (Jaws_Error::IsError($user) || empty($user)) {
+            return;
+        }
 
+        $drivers = glob(JAWS_PATH . 'include/Jaws/Notification/*.php');
         foreach ($drivers as $driver) {
             $driver = basename($driver, '.php');
             $options = unserialize($this->gadget->registry->fetch($driver . '_options'));
