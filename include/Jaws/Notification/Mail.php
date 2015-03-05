b5d7c335eb8473dc7fd7288db667a62195e990df
Ali Fazelzadeh <afz@php.net>
2015-03-05 17:51:30 +0330

    [Jaws/Notification/Mail]: Added default template for mail notification

--- include/Jaws/Notification/Mail.php
+++ include/Jaws/Notification/Mail.php
@@ -17,6 +17,14 @@ class Jaws_Notification_Mail
      */
     private $object;
 
+    /**
+     * Site attributes
+     *
+     * @access  private
+     * @var     array
+     */
+    private $attributes = array();
+
 
     /**
      * constructor
@@ -28,6 +36,15 @@ class Jaws_Notification_Mail
     public function __construct($options)
     {
         $this->object = Jaws_Mail::getInstance('notification');
+        // fetch all registry keys related to site attributes
+        $this->attributes = $GLOBALS['app']->Registry->fetchAll('Settings', false);
+        Jaws_Translate::getInstance()->LoadTranslation(
+            'Global',
+            JAWS_COMPONENT_OTHERS,
+            $this->attributes['site_language']
+        );
+        $this->attributes['site_url']       = $GLOBALS['app']->GetSiteURL('/');
+        $this->attributes['site_direction'] = _t_lang($this->attributes['site_language'], 'GLOBAL_LANG_DIRECTION');
     }
 
 
@@ -35,13 +52,13 @@ class Jaws_Notification_Mail
      * Sends notify to user
      *
      * @access  public
-     * @param   array   $users          Users properties associated array
-     * @param   string  $title          Notification title
-     * @param   string  $summary        Notification summary
-     * @param   string  $description    Notification description
+     * @param   array   $users      Users properties associated array
+     * @param   string  $title      Notification title
+     * @param   string  $summary    Notification summary
+     * @param   string  $content    Notification content
      * @return  mixed   Jaws_Error on failure
      */
-    function notify($users, $title, $summary, $description)
+    function notify($users, $title, $summary, $content)
     {
         $this->object->reset();
         $this->object->SetFrom();
@@ -49,7 +66,24 @@ class Jaws_Notification_Mail
             $this->object->AddRecipient($user['email']);
         }
         $this->object->SetSubject($title);
-        $this->object->SetBody($description);
+
+        $tpl = new Jaws_Template();
+        $tpl->loadRTLDirection = $this->attributes['site_direction'] == 'rtl';
+        $tpl->Load('Notification.html', 'include/Jaws/Resources');
+        $tpl->SetBlock('notification');
+        $tpl->SetVariable('site-url',       $this->attributes['site_url']);
+        $tpl->SetVariable('site-direction', $this->attributes['site_direction']);
+        $tpl->SetVariable('site-name',      $this->attributes['site_name']);
+        $tpl->SetVariable('site-slogan',    $this->attributes['site_slogan']);
+        $tpl->SetVariable('site-comment',   $this->attributes['site_comment']);
+        $tpl->SetVariable('site-author',    $this->attributes['site_author']);
+        $tpl->SetVariable('site-license',   $this->attributes['site_license']);
+        $tpl->SetVariable('site-copyright', $this->attributes['site_copyright']);
+        $tpl->SetVariable('content', $content);
+        $tpl->ParseBlock('notification');
+        $this->object->SetBody($tpl->Get());
+        unset($tpl);
+
         return $this->object->send();
     }
 
