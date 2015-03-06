b0c5cbd51533559a7f5c419dfbd69e5b2ab7f3bc
Ali Fazelzadeh <afz@php.net>
2015-03-07 00:47:06 +0330

    [Notification]: Fixed default admin action

---
+++ gadgets/Notification/Actions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Notification Actions
+ *
+ * @category    GadgetActions
+ * @package     Notification
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2015 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/lesser.html
+ */
+
+/**
+ * Admin actions
+ */
+$admin_actions['Settings'] = array(
+    'normal' => true,
+    'file' => 'Settings',
+);
---
+++ gadgets/Notification/Actions/Admin/Settings.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Notification Gadget Admin
+ *
+ * @category    GadgetAdmin
+ * @package     Notification
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2015 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/lesser.html
+ */
+class Notification_Actions_Admin_Settings extends PrivateMessage_Actions_Admin_Default
+{
+    /**
+     * Builds admin settings UI
+     *
+     * @access  public
+     * @return  string  XHTML form
+     */
+    function Settings()
+    {
+        return;
+    }
+
+}
