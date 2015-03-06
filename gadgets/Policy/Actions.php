a0663a842416796ede9f8c1024f0b720fdb3bf4a
Ali Fazelzadeh <afz@php.net>
2015-03-06 21:53:30 +0330

    [Policy]: lesser log level value for some actions

--- gadgets/Policy/Actions.php
+++ gadgets/Policy/Actions.php
@@ -26,27 +26,22 @@ $actions['Captcha'] = array(
 $admin_actions['IPBlocking'] = array(
     'normal' => true,
     'file' => 'IP',
-    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['AgentBlocking'] = array(
     'normal' => true,
     'file' => 'Agent',
-    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['Encryption'] = array(
     'normal' => true,
     'file' => 'Encryption',
-    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['AntiSpam'] = array(
     'normal' => true,
     'file' => 'AntiSpam',
-    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['AdvancedPolicies'] = array(
     'normal' => true,
     'file' => 'AdvancedPolicies',
-    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['GetIPRange'] = array(
     'standalone' => true,
@@ -55,17 +50,17 @@ $admin_actions['GetIPRange'] = array(
 $admin_actions['AddIPRange'] = array(
     'standalone' => true,
     'file' => 'Ajax',
-    'loglevel' => JAWS_WARNING,
+    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['EditIPRange'] = array(
     'standalone' => true,
     'file' => 'Ajax',
-    'loglevel' => JAWS_WARNING,
+    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['DeleteIPRange'] = array(
     'standalone' => true,
     'file' => 'Ajax',
-    'loglevel' => JAWS_WARNING,
+    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['GetAgent'] = array(
     'standalone' => true,
@@ -74,42 +69,42 @@ $admin_actions['GetAgent'] = array(
 $admin_actions['AddAgent'] = array(
     'standalone' => true,
     'file' => 'Ajax',
-    'loglevel' => JAWS_WARNING,
+    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['EditAgent'] = array(
     'standalone' => true,
     'file' => 'Ajax',
-    'loglevel' => JAWS_WARNING,
+    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['DeleteAgent'] = array(
     'standalone' => true,
     'file' => 'Ajax',
-    'loglevel' => JAWS_WARNING,
+    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['IPBlockingBlockUndefined'] = array(
     'standalone' => true,
     'file' => 'Ajax',
-    'loglevel' => JAWS_WARNING,
+    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['AgentBlockingBlockUndefined'] = array(
     'standalone' => true,
     'file' => 'Ajax',
-    'loglevel' => JAWS_WARNING,
+    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['UpdateEncryptionSettings'] = array(
     'standalone' => true,
     'file' => 'Ajax',
-    'loglevel' => JAWS_WARNING,
+    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['UpdateAntiSpamSettings'] = array(
     'standalone' => true,
     'file' => 'Ajax',
-    'loglevel' => JAWS_WARNING,
+    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['UpdateAdvancedPolicies'] = array(
     'standalone' => true,
     'file' => 'Ajax',
-    'loglevel' => JAWS_WARNING,
+    'loglevel' => JAWS_NOTICE,
 );
 $admin_actions['getData'] = array(
     'standalone' => true,
