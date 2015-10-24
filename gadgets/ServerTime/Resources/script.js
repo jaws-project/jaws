50f7cf55da675b78cc08addc43cecd54ade425b6
mkhahani <mkhahani@gmail.com>
2015-10-24 18:54:10 +0330

    [ServerTime] convert to jQuery

--- gadgets/ServerTime/Resources/script.js
+++ gadgets/ServerTime/Resources/script.js
@@ -4,24 +4,24 @@
  * @category   Ajax
  * @package    ServerTime
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2005-2014 Jaws Development Group
+ * @copyright  2005-2015 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 /**
  * Use async mode, create Callback
  */
-var ServerTimeCallback = { 
+var ServerTimeCallback = {
     UpdateProperties: function(response) {
         ServerTimeAjax.showResponse(response);
     }
-}
+};
 
 /**
  * Update the properties
  */
 function updateProperties(form)
 {
-    ServerTimeAjax.callAsync('UpdateProperties', $('date_format').value);
+    ServerTimeAjax.callAsync('UpdateProperties', $('#date_format').val());
 
 }
 
