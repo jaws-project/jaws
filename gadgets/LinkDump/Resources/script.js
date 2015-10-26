2abb8bfaa6ef2e1a335815425f13543632270afc
Ali Fazelzadeh <afz@php.net>
2015-10-27 00:15:48 +0330

    [LinkDump]: @2015

--- gadgets/LinkDump/Resources/script.js
+++ gadgets/LinkDump/Resources/script.js
@@ -5,7 +5,7 @@
  * @package    LinkDump
  * @author     Amir Mohammad Saied <amirsaied@gmail.com>
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2005-2014 Jaws Development Group
+ * @copyright  2005-2015 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 /**
