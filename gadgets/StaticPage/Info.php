54f25f65d1f34dbbff7a78a6874d1b687d89e246
Ali Fazelzadeh <afz@php.net>
2015-10-27 00:16:21 +0330

    [StaticPage]: @2015

--- gadgets/StaticPage/Info.php
+++ gadgets/StaticPage/Info.php
@@ -5,7 +5,7 @@
  * @category   GadgetInfo
  * @package    StaticPage
  * @author     Jon Wood <jon@jellybob.co.uk>
- * @copyright  2004-2014 Jaws Development Group
+ * @copyright  2004-2015 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class StaticPage_Info extends Jaws_Gadget
