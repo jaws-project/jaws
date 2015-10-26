242f4df9db69374bbf7fd14ca10c07f19dce7e72
Ali Fazelzadeh <afz@php.net>
2015-10-27 00:23:12 +0330

    @2015

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
  * @author     Pablo Fischer <pablo@pablo.com.mx>
  * @author     Helgi Þormar <dufuz@php.net>
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2005-2014 Jaws Development Group
+ * @copyright  2005-2015 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/lesser.html
  */
 define('JAWS_SCRIPT', 'index');
