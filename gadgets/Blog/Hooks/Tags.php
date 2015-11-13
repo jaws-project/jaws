14233b9367769fce8b2304b87fb15bfd732ee978
Ali Fazelzadeh <afz@php.net>
2015-11-13 15:16:18 +0330

    [Blog]: Updated Tags Hook

--- gadgets/Blog/Hooks/Tags.php
+++ gadgets/Blog/Hooks/Tags.php
@@ -14,18 +14,19 @@ class Blog_Hooks_Tags extends Jaws_Gadget_Hook
      * Returns an array with the results of a tag content
      *
      * @access  public
-     * @param   array  $tag_items  Tag items
-     * @return  array  An array of entries that matches a certain pattern
+     * @param   string  $action     Action name
+     * @param   array   $references Array of References
+     * @return  array   An array of entries that matches a certain pattern
      */
-    function Execute($tag_items)
+    function Execute($action, $references)
     {
-        if(!is_array($tag_items) || empty($tag_items)) {
-            return;
+        if(empty($action) || !is_array($references) ||empty($references)) {
+            return false;
         }
 
         $table = Jaws_ORM::getInstance()->table('blog');
         $table->select('id:integer', 'fast_url', 'title', 'summary', 'text', 'updatetime');
-        $result = $table->where('id', $tag_items['post'], 'in')->fetchAll();
+        $result = $table->where('id', $references, 'in')->fetchAll();
         if (Jaws_Error::IsError($result)) {
             return array();
         }
@@ -40,7 +41,7 @@ class Blog_Hooks_Tags extends Jaws_Gadget_Hook
             $post['image']   = 'gadgets/Blog/Resources/images/logo.png';
             $post['snippet'] = $r['summary'];
             $post['date']    = $date->ToISO($r['updatetime']);
-            $posts[] = $post;
+            $posts[$r['id']] = $post;
         }
 
         return $posts;
