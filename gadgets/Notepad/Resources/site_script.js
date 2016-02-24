e370d1d92c02c40747f83d5c56245c929ab03415
Ali Fazelzadeh <afz@php.net>
2016-02-24 09:17:03 +0330

    [Notepad]: Fixed get content value when using tinyMCE/CKEditor

--- gadgets/Notepad/Resources/site_script.js
+++ gadgets/Notepad/Resources/site_script.js
@@ -74,6 +74,7 @@ function submitNote(form)
         form.title.focus();
         return;
     }
+    form.content.value = getEditorValue('content');
     if (form.content.value === '') {
         alert(errorIncompleteData);
         form.content.focus();
