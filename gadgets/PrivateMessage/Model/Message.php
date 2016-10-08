ef9c1f89a48561f23b24e1231b7e3767e123dcc0
Ali Fazelzadeh <afz@php.net>
2016-10-08 08:54:26 +0330

    [PrivateMessage]: remove double '' in sql like pattern

--- gadgets/PrivateMessage/Model/Message.php
+++ gadgets/PrivateMessage/Model/Message.php
@@ -575,7 +575,7 @@ class PrivateMessage_Model_Message extends Jaws_Gadget_Model
                 }
             }
             if (isset($filters['term']) && !empty($filters['term'])) {
-                $filters['term'] = '%' . $filters['term'] . '%';
+                $filters['term'] = array('%$%', $filters['term']);
                 $table->and()->openWhere('pm_messages.subject', $filters['term'], 'like')->or();
                 $table->closeWhere('pm_messages.body', $filters['term'], 'like');
             }
@@ -643,7 +643,7 @@ class PrivateMessage_Model_Message extends Jaws_Gadget_Model
                 }
             }
             if (isset($filters['term']) && !empty($filters['term'])) {
-                $filters['term'] = '%' . $filters['term'] . '%';
+                $filters['term'] = array('%$%', $filters['term']);
                 $table->and()->openWhere('pm_messages.subject', $filters['term'], 'like')->or();
                 $table->closeWhere('pm_messages.body', $filters['term'], 'like');
             }
