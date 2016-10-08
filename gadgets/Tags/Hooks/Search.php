0aaef40592f5a76d06838a522a5901756cd1e5e5
Ali Fazelzadeh <afz@php.net>
2016-10-08 10:18:50 +0330

    [Tags]: Search hook migrate to Jaws_ORM

--- gadgets/Tags/Hooks/Search.php
+++ gadgets/Tags/Hooks/Search.php
@@ -15,42 +15,30 @@ class Tags_Hooks_Search extends Jaws_Gadget_Hook
      */
     function GetOptions() {
         return array(
-                    array('[title]', '[description]', '[meta_keywords]', '[meta_description]'),
-                    );
+            'tags' => array('title', 'description', 'meta_keywords', 'meta_description'),
+        );
     }
 
     /**
      * Returns an array with the results of a search
      *
      * @access  public
-     * @param   string  $pSql   Prepared search(WHERE) SQL
+     * @param   string  $table  Table name
+     * @param   object  $objORM Jaws_ORM instance object
      * @return  array   An array of entries that matches a certain pattern
      */
-    function Execute($pSql = '')
+    function Execute($table, &$objORM)
     {
-        $params = array();
-
-        $sql = '
-            SELECT
-               [id],[name], [title], [description], [meta_keywords], [meta_description]
-            FROM [[tags]]
-            ';
-
+        $objORM->table('tags');
+        $objORM->select('id', 'name', 'title', 'description', 'meta_keywords', 'meta_description');
+        $objORM->openWhere('user', 0);
         if ($GLOBALS['app']->Session->Logged()) {
-            $params['user'] = $GLOBALS['app']->Session->GetAttribute('user');
-            $wStr = '([user]=0 OR [user]={user})';
-        } else {
-            $wStr = '[user]=0';
+            $objORM->or()->where('user', (int)$GLOBALS['app']->Session->GetAttribute('user'));
         }
-
-        $sql .= ' WHERE ' . $wStr;
-        $sql .= ' AND ' . $pSql;
-        $sql .= ' ORDER BY [id] desc';
-
-        $types = array('integer', 'text', 'text', 'text', 'text', 'text');
-        $result = Jaws_DB::getInstance()->queryAll($sql, $params, $types);
+        $objORM->closeWhere();
+        $result = $objORM->orderBy('id')->fetchAll();
         if (Jaws_Error::IsError($result)) {
-            return array();
+            return false;
         }
 
         $tags = array();
@@ -65,7 +53,7 @@ class Tags_Hooks_Search extends Jaws_Gadget_Hook
             $tag['image']   = 'gadgets/Tags/Resources/images/logo.png';
             $tag['snippet'] = $t['description'];
             $tag['date']    = null;
-            $tags[$t['id']]   = $tag;
+            $tags[$t['id']] = $tag;
         }
 
         return $tags;
