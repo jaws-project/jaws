6c767ea90608728263b558d16c50a5c482eb5b59
Ali Fazelzadeh <afz@php.net>
2016-11-28 17:59:13 +0330

    [Directory]: Preparing personal file storage

--- gadgets/Directory/Info.php
+++ gadgets/Directory/Info.php
@@ -31,7 +31,7 @@ class Directory_Info extends Jaws_Gadget
      * @var     string
      * @access  private
      */
-    var $version = '1.1.0';
+    var $version = '1.2.0';
 
     /**
      * Recommended gadgets
--- gadgets/Directory/Installer.php
+++ gadgets/Directory/Installer.php
@@ -86,7 +86,14 @@ class Directory_Installer extends Jaws_Gadget_Installer
     function Upgrade($old, $new)
     {
         if (version_compare($old, '1.1.0', '<')) {
-            $result = $this->installSchema('schema.xml', '', '1.0.0.xml');
+            $result = $this->installSchema('1.1.0.xml', '', '1.0.0.xml');
+            if (Jaws_Error::IsError($result)) {
+                return $result;
+            }
+        }
+
+        if (version_compare($old, '1.2.0', '<')) {
+            $result = $this->installSchema('schema.xml', '', '1.1.0.xml');
             if (Jaws_Error::IsError($result)) {
                 return $result;
             }
