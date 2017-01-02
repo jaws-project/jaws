f9236fd960355253d7f3a77a5a4af5a5492a640d
Ali Fazelzadeh <afz@php.net>
2017-01-02 16:54:59 +0330

    [PrivateMessage]: Added some useful links to menu hook

--- gadgets/PrivateMessage/Hooks/Menu.php
+++ gadgets/PrivateMessage/Hooks/Menu.php
@@ -19,9 +19,53 @@ class PrivateMessage_Hooks_Menu extends Jaws_Gadget_Hook
     function Execute()
     {
         $urls   = array();
-        $urls[] = array('url'   => $this->gadget->urlMap('PrivateMessage'),
-                        'title' => _t('PRIVATEMESSAGE_ACTIONS_PRIVATEMESSAGE'));
-
+        $urls[] = array(
+            'url'   => $this->gadget->urlMap('PrivateMessage'),
+            'title' => _t('PRIVATEMESSAGE_ACTIONS_PRIVATEMESSAGE')
+        );
+        $urls[] = array(
+            'url' => $this->gadget->urlMap(
+                'Messages',
+                array('folder' => PrivateMessage_Info::PRIVATEMESSAGE_FOLDER_INBOX)
+            ),
+            'title' => _t('PRIVATEMESSAGE_INBOX')
+        );
+        $urls[] = array(
+            'url' => $this->gadget->urlMap(
+                'Messages',
+                array('folder' => PrivateMessage_Info::PRIVATEMESSAGE_FOLDER_DRAFT)
+            ),
+            'title' => _t('PRIVATEMESSAGE_DRAFT')
+        );
+        $urls[] = array(
+            'url' => $this->gadget->urlMap(
+                'Messages',
+                array('folder' => PrivateMessage_Info::PRIVATEMESSAGE_FOLDER_ARCHIVED)
+            ),
+            'title' => _t('PRIVATEMESSAGE_ARCHIVED')
+        );
+        $urls[] = array(
+            'url' => $this->gadget->urlMap(
+                'Messages',
+                array('folder' => PrivateMessage_Info::PRIVATEMESSAGE_FOLDER_OUTBOX)
+            ),
+            'title' => _t('PRIVATEMESSAGE_OUTBOX')
+        );
+        $urls[] = array(
+            'url' => $this->gadget->urlMap(
+                'Messages',
+                array('folder' => PrivateMessage_Info::PRIVATEMESSAGE_FOLDER_TRASH)
+            ),
+            'title' => _t('PRIVATEMESSAGE_TRASH')
+        );
+        $urls[] = array(
+            'url'   => $this->gadget->urlMap('Messages'),
+            'title' => _t('PRIVATEMESSAGE_ALL_MESSAGES')
+        );
+        $urls[] = array(
+            'url'   => $this->gadget->urlMap('Compose'),
+            'title' => _t('PRIVATEMESSAGE_COMPOSE_MESSAGE')
+        );
         return $urls;
     }
 
