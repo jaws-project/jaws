19273e404421cc082f07bd5f2e763fbe7849fc5f
Ali Fazelzadeh <afz@php.net>
2017-01-05 19:30:40 +0330

    [Users]: Added some useful links to menu hooks

--- gadgets/Users/Hooks/Menu.php
+++ gadgets/Users/Hooks/Menu.php
@@ -15,8 +15,44 @@ class Users_Hooks_Menu extends Jaws_Gadget_Hook
      */
     function Execute()
     {
-        $urls[] = array('url'   => $this->gadget->urlMap('LoginBox'),
-                        'title' => _t('USERS_LOGIN_TITLE'));
+        $urls = array();
+        $urls[] = array(
+            'url'   => $this->gadget->urlMap('LoginBox'),
+            'title' => _t('USERS_LOGIN_TITLE')
+        );
+
+/*
+        $urls[] = array(
+            'url'   => $this->gadget->urlMap('Profile', array('user' => $uInfo['username'])),
+            'title' => _t('USERS_PROFILE')
+        );
+*/
+
+        $urls[] = array(
+            'url'        => $this->gadget->urlMap('FriendsGroups'),
+            'title'      => _t('USERS_FRIENDS'),
+            'acl_key'    => 'ManageFriends',
+            'acl_subkey' => ''
+        );
+
+/*
+        $urls[] = array(
+            'url'   => $this->gadget->urlMap('Dashboard', array('user' => $logged_user), false, 'Layout'),
+            'title' => _t('USERS_DASHBOARD_USER')
+        );
+*/
+        $urls[] = array(
+            'url'        => $this->gadget->urlMap('Dashboard', array('user' => 0), false, 'Layout'),
+            'title'      => _t('USERS_DASHBOARD_GLOBAL'),
+            'acl_key'    => 'AccessDashboard',
+            'acl_subkey' => ''
+        );
+
+        $urls[] = array(
+            'url'    => $this->gadget->urlMap('Logout'),
+            'title'  => _t('GLOBAL_LOGOUT'),
+            'logged' => true,
+        );
         return $urls;
     }
 
