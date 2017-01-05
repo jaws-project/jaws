0b355a3e043501fc22f38406e70033fcbeb306c7
Ali Fazelzadeh <afz@php.net>
2017-01-05 19:36:15 +0330

    [ControlPanel]: Added hook menu

---
+++ gadgets/ControlPanel/Hooks/Menu.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * ControlPanel - URL List gadget hook
+ *
+ * @category   GadgetHook
+ * @package    ControlPanel
+ */
+class ControlPanel_Hooks_Menu extends Jaws_Gadget_Hook
+{
+    /**
+     * Returns an array with all available items the Menu gadget can use
+     *
+     * @access  public
+     * @return  array   List of URLs
+     */
+    function Execute()
+    {
+        $urls = array();
+        $admin_script = $this->gadget->registry->fetch('admin_script', 'Settings');
+        $urls[] = array(
+            'url'   => empty($admin_script)? 'admin.php' : $admin_script,
+            'title' => _t('USERS_CONTROLPANEL'),
+            'acl_key'    => 'default_admin',
+            'acl_subkey' => ''
+        );
+
+        return $urls;
+    }
+
+}
