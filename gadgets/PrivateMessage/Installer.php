97a348dc3ebf173f65a11d4d27236df73d6336b0
Ali Fazelzadeh <afz@php.net>
2017-01-22 10:51:18 +0330

    [PrivateMessage]: PHP7 compatibility...

--- gadgets/PrivateMessage/Installer.php
+++ gadgets/PrivateMessage/Installer.php
@@ -106,7 +106,7 @@ class PrivateMessage_Installer extends Jaws_Gadget_Installer
     function Upgrade($old, $new)
     {
         if (version_compare($old, '1.1.0', '<')) {
-            $result = $this->installSchema('schema.xml', '', '1.0.0.xml');
+            $result = $this->installSchema('schema.xml', array(), '1.0.0.xml');
             if (Jaws_Error::IsError($result)) {
                 return $result;
             }
