8b2f7a9a15e7bdad13b4ca73b6bb2e2b35661b4d
Ali Fazelzadeh <afz@php.net>
2017-01-22 10:51:12 +0330

    [Blog]: PHP7 compatibility...

--- gadgets/Blog/Installer.php
+++ gadgets/Blog/Installer.php
@@ -136,7 +136,7 @@ class Blog_Installer extends Jaws_Gadget_Installer
     function Upgrade($old, $new)
     {
         if (version_compare($old, '1.0.0', '<')) {
-            $result = $this->installSchema('1.0.0.xml', '', '0.9.0.xml');
+            $result = $this->installSchema('1.0.0.xml', array(), '0.9.0.xml');
             if (Jaws_Error::IsError($result)) {
                 return $result;
             }
@@ -171,7 +171,7 @@ class Blog_Installer extends Jaws_Gadget_Installer
         }
 
         if (version_compare($old, '1.1.0', '<')) {
-            $result = $this->installSchema('schema.xml', '', '1.0.0.xml');
+            $result = $this->installSchema('schema.xml', array(), '1.0.0.xml');
             if (Jaws_Error::IsError($result)) {
                 return $result;
             }
