b83898419dea95d9d7f69bbc69472b3ed301e689
Ali Fazelzadeh <afz@php.net>
2017-01-22 10:51:14 +0330

    [EventsCalendar]: PHP7 compatibility...

--- gadgets/EventsCalendar/Installer.php
+++ gadgets/EventsCalendar/Installer.php
@@ -76,7 +76,7 @@ class EventsCalendar_Installer extends Jaws_Gadget_Installer
     function Upgrade($old, $new)
     {
         if (version_compare($old, '1.1.0', '<')) {
-            $result = $this->installSchema('schema.xml', '', '1.0.0.xml');
+            $result = $this->installSchema('schema.xml', array(), '1.0.0.xml');
             if (Jaws_Error::IsError($result)) {
                 return $result;
             }
