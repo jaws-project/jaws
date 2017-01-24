d0f2f520356604aea4964d93220342c5199d8123
Mojtaba Ebrahimi <ebrahimi@zehneziba.ir>
2017-01-24 15:19:02 +0330

    implement AbuseReporter plugin

--- plugins/AbuseReporter/Plugin.php
+++ plugins/AbuseReporter/Plugin.php
@@ -12,22 +12,6 @@ class AbuseReporter_Plugin extends Jaws_Plugin
     var $_DefaultFrontendEnabled = true;
 
     /**
-     * Checks the string to see if parsing is required
-     *
-     * @access  public
-     * @param   string  $html   Input HTML
-     * @return  bool    Checking result
-     */
-    function NeedParsing($html)
-    {
-        if (stripos($html, 'JAWS_REPORT') !== false) {
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
      * Overrides, Parses the text
      *
      * @access  public
@@ -36,9 +20,8 @@ class AbuseReporter_Plugin extends Jaws_Plugin
      */
     function ParseText($html)
     {
-        if (!$this->NeedParsing($html)) {
-            return $html;
-        }
+        $GLOBALS['app']->Layout->addScript('gadgets/AbuseReporter/Resources/index.js');
+
         $html = str_replace('JAWS_REPORT', '', $html);
 
         $tpl = new Jaws_Template();
@@ -46,13 +29,15 @@ class AbuseReporter_Plugin extends Jaws_Plugin
         $tpl->SetBlock('Report');
         $tpl->SetVariable('content', $html);
 
+        $tpl->SetVariable('lbl_title', _t('PLUGINS_ABUSEREPORTER_REPORT'));
         $tpl->SetVariable('lbl_report', _t('PLUGINS_ABUSEREPORTER_REPORT'));
-
-        $tpl->SetVariable('lbl_comment', _t('ABUSEREPORTER_COMMENT'));
-        $tpl->SetVariable('lbl_type', _t('ABUSEREPORTER_TYPE'));
-        $tpl->SetVariable('lbl_priority', _t('ABUSEREPORTER_PRIORITY'));
         $tpl->SetVariable('lbl_send', _t('PLUGINS_ABUSEREPORTER_SEND'));
         $tpl->SetVariable('lbl_cancel', _t('GLOBAL_CANCEL'));
+
+        $tpl->SetVariable('gadget', $GLOBALS['app']->requestedGadget);
+        $tpl->SetVariable('action', $GLOBALS['app']->requestedAction);
+        $tpl->SetVariable('reference', '0');
+        $tpl->SetVariable('url', Jaws_Utils::getRequestURL(true));
 
         $tpl->ParseBlock('Report');
         return $tpl->Get();
