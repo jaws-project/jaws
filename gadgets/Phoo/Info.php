108f6b9fa9ba27183b124d2de2fa1597e988f836
Ali Fazelzadeh <afz@php.net>
2017-02-10 17:18:16 +0330

    [Phoo]: preparing upgrade

--- gadgets/Phoo/Info.php
+++ gadgets/Phoo/Info.php
@@ -18,7 +18,7 @@ class Phoo_Info extends Jaws_Gadget
      * @var     string
      * @access  private
      */
-    var $version = '1.2.0';
+    var $version = '1.3.0';
 
     /**
      * Recommended gadgets
