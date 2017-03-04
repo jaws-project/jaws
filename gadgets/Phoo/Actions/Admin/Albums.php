6a3d027c428f766dcc5f2181a3fa8758950728e8
Ali Fazelzadeh <afz@php.net>
2017-03-04 10:01:03 +0330

    [Phoo]: Applied new changes of cross platform definition variables

--- gadgets/Phoo/Actions/Admin/Albums.php
+++ gadgets/Phoo/Actions/Admin/Albums.php
@@ -25,7 +25,7 @@ class Phoo_Actions_Admin_Albums extends Phoo_Actions_Admin_Default
     function NewAlbum()
     {
         $this->AjaxMe('script.js');
-        $this->gadget->layout->setVariable('base_script', BASE_SCRIPT);
+        $this->gadget->define('base_script', BASE_SCRIPT);
 
         $this->gadget->CheckPermission('ManageAlbums');
 
@@ -148,7 +148,7 @@ class Phoo_Actions_Admin_Albums extends Phoo_Actions_Admin_Default
     {
         $this->gadget->CheckPermission('ManageAlbums');
         $this->AjaxMe('script.js');
-        $this->gadget->layout->setVariable('base_script', BASE_SCRIPT);
+        $this->gadget->define('base_script', BASE_SCRIPT);
         $model = $this->gadget->model->load('Albums');
 
         $get = jaws()->request->fetch(array('action', 'album'), 'get');
