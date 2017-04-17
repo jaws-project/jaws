27e2f624457e9f1c1d29c9a3c54982156c24b26c
Ali Fazelzadeh <afz@php.net>
2017-04-17 09:15:08 +0430

    [UrlMapper]: more length for map/regex

--- gadgets/UrlMapper/Installer.php
+++ gadgets/UrlMapper/Installer.php
@@ -80,7 +80,14 @@ class UrlMapper_Installer extends Jaws_Gadget_Installer
         }
 
         if (version_compare($old, '1.2.0', '<')) {
-            $result = $this->installSchema('schema.xml', array(), '1.1.0.xml');
+            $result = $this->installSchema('1.2.0.xml', array(), '1.1.0.xml');
+            if (Jaws_Error::IsError($result)) {
+                return $result;
+            }
+        }
+
+        if (version_compare($old, '1.3.0', '<')) {
+            $result = $this->installSchema('schema.xml', array(), '1.2.0.xml');
             if (Jaws_Error::IsError($result)) {
                 return $result;
             }
