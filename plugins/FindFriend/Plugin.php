42f0206385841599857ee8098f70156b5fe16664
Ali Fazelzadeh <afz@php.net>
2017-05-31 15:06:52 +0430

    [FindFriend_Plugin]: Applying new changes of Jaws_Plugin

--- plugins/FindFriend/Plugin.php
+++ plugins/FindFriend/Plugin.php
@@ -8,7 +8,7 @@
  * @copyright  2004-2015 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
-class FindFriend_Plugin extends Jaws_Plugin
+class FindFriend_Plugin
 {
     var $friendly = true;
     var $version = '0.3';
