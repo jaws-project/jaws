51bcc23c43a563cedc349326dde3b2736a713ac4
Ali Fazelzadeh <afz@php.net>
2017-05-31 15:57:27 +0430

    [SmartBridge_Plugin]: Fixed an issue

--- plugins/SmartBridge/Plugin.php
+++ plugins/SmartBridge/Plugin.php
@@ -14,37 +14,12 @@ class SmartBridge_Plugin
     var $version  = '0.2';
 
     /**
-     * Approved gadgest for links
+     * Approved gadgets for links
      *
      * @var     array
      * @access  private
      */
     var $_ApprovedGadgets = array('Blog', 'StaticPage', 'Phoo');
-
-    /**
-     * Jaws gadgets that are enabled
-     *
-     * @var     array
-     * @access  private
-     */
-    var $_EnabledGadgets  = array();
-
-    /**
-     * Main Constructor
-     *
-     * @access  public
-     * @return  void
-     */
-    function __construct($plugin)
-    {
-        parent::__construct($plugin);
-        $eg = $GLOBALS['app']->Registry->fetch('gadgets_enabled_items');
-        if (Jaws_Error::isError($eg)) {
-            $eg = array();
-        }
-
-        $this->_EnabledGadgets = explode(',', $eg);
-    }
 
     /**
      * Overrides, Gets the WebControl of this plugin
@@ -93,6 +68,11 @@ class SmartBridge_Plugin
      */
     function ParseText($html, $reference = 0, $action = '', $gadget = '')
     {
+        // only approved gadgets
+        if (!in_array($gadget, $this->_ApprovedGadgets)) {
+            return $html;
+        }
+
         if (!$this->NeedParsing($html)) {
             return $html;
         }
