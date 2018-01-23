3034371ab82640ff9f63d85b28a34ab09d60497a
Ali Fazelzadeh <afz@php.net>
2018-01-23 14:46:04 +0330

    [ControlPanel]: Upgraded js part to new structure

--- gadgets/ControlPanel/Resources/script.js
+++ gadgets/ControlPanel/Resources/script.js
@@ -4,64 +4,70 @@
  * @category    Ajax
  * @package     ControlPanel
  * @author      Ali Fazelzadeh <afz@php.net>
- * @copyright   2013-2015 Jaws Development Group
+ * @copyright   2013-2018 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/lesser.html
  */
-/**
- * Use async mode, create Callback
- */
-var ControlPanelCallback = {
-    JawsVersion: function(response) {
-        $('#latest_jaws_version').html(response);
-        if (!response.blank() && response !== $('#jaws_version').val()) {
+function Jaws_Gadget_ControlPanel() { return {
+    // web storage
+    storage : null,
+
+    // ASync callback method
+    AjaxCallback : {
+        JawsVersion: function(response) {
+            $('#latest_jaws_version').html(response);
+            if (!response.blank() && response !== $('#jaws_version').val()) {
+                $('#div.notify_version').css('display', 'block');
+            }
+        },
+    },
+
+    /**
+     * initialize sidebar
+     */
+    initSidebar: function() {
+        $('#sidebar h2').on('click', $.proxy(function (event) {
+            $(event.target).toggleClass('collapsed');
+            this.storage.update(
+                $('#sidebar').children().index($(event.target).parent()),
+                $(event.target).attr('class')
+            );
+            $(event.target).next('div').toggle();
+        }, this));
+
+        $('#sidebar').children().each($.proxy(function(i, el) {
+            if (this.storage.fetch(i)) {
+                $(el).children('h2').trigger('click');
+            }
+        }, this));
+    },
+
+    /**
+     * check Jaws version
+     */
+    checkVersion: function() {
+        // compare current version with latest jaws version
+        if (!$('#latest_jaws_version').text() &&
+            $('#latest_jaws_version').text() !== $('#jaws_version').val())
+        {
             $('#div.notify_version').css('display', 'block');
         }
-    }
-}
 
-/**
- * Initiates script
- */
-function init()
-{
-    $('#sidebar h2').on('click', function () {
-        $(this).toggleClass('collapsed');
-        ControlPanelStorage.update(
-            $('#sidebar').children().index($(this).parent()),
-            $(this).attr('class')
-        );
-        $(this).next('div').toggle();
-    });
-    $('#sidebar').children().each(function(i) {
-        if (ControlPanelStorage.fetch(i)) {
-            $(this).children('h2').trigger('click');
+        // check jaws project website for latest version
+        if ($('#do_checking').val() == 1) {
+            this.gadget.ajax.callAsync('JawsVersion');
         }
-    });
+    },
 
-    // compare current version with latest jaws version
-    if (!$('#latest_jaws_version').text() &&
-        $('#latest_jaws_version').text() !== $('#jaws_version').val())
-    {
-        $('#div.notify_version').css('display', 'block');
-    }
+    /**
+     * initialize gadget actions
+     */
+    init: function(mainGadget, mainAction) {
+        // default action
+        if (this.gadget.actions.indexOf('DefaultAction') >= 0) {
+            this.storage = new JawsStorage('ControlPanel');
+            this.initSidebar();
+            this.checkVersion();
+        }
+    },
 
-    // check jaws project website for latest version
-    if ($('#do_checking').val() == 1) {
-        ControlPanelAjax.callAsync('JawsVersion');
-    }
-}
-
-$(document).ready(function() {
-    switch (jaws.Defines.mainAction) {
-        case 'DefaultAction':
-            init();
-            break;
-
-        default:
-            break;
-    }
-    
-});
-
-var ControlPanelAjax = new JawsAjax('ControlPanel', ControlPanelCallback);
-var ControlPanelStorage = new JawsStorage('ControlPanel');
+}};
