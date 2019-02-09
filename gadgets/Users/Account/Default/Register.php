6b977102b7446e62178b0476e63ad8b74a932c0c
Ali Fazelzadeh <afz@php.net>
2019-02-09 13:30:52 +0330

    [Users]: Integrate registration process into account driver

---
+++ gadgets/Users/Account/Default/Register.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Users Core Gadget
+ *
+ * @category    Gadget
+ * @package     Users
+ */
+class Users_Account_Default_Register extends Users_Account_Default
+{
+    /**
+     * Register
+     *
+     * @access  public
+     * @return  void
+     */
+    function Register()
+    {
+        $rgstrData = $this->gadget->request->fetch(
+            array(
+                'domain', 'username', 'email', 'mobile', 'nickname', 'password', 'password_check',
+                'fname', 'lname', 'gender', 'ssn', 'dob', 'url', 'regstep', 'regkey', 'usecrypt'
+            ),
+            'post'
+        );
+
+        try {
+            // check captcha
+            $htmlPolicy = Jaws_Gadget::getInstance('Policy')->action->load('Captcha');
+            $resCheck = $htmlPolicy->checkCaptcha('login');
+            if (Jaws_Error::IsError($resCheck)) {
+                throw new Exception($resCheck->getMessage(), 401);
+            }
+
+            if (empty($rgstrData['regstep'])) {
+                $this->gadget->session->update('temp.register.user', '');
+                if ($rgstrData['password'] !== $rgstrData['password_check']) {
+                    throw new Exception(_t('USERS_USERS_PASSWORDS_DONT_MATCH'), 401);
+                }
+
+                if ($rgstrData['usecrypt']) {
+                    $JCrypt = Jaws_Crypt::getInstance();
+                    if (!Jaws_Error::IsError($JCrypt)) {
+                        $rgstrData['password'] = $JCrypt->decrypt($rgstrData['password']);
+                    }
+                } else {
+                    $rgstrData['password'] = Jaws_XSS::defilter($rgstrData['password']);
+                }
+
+                // set default domain if not set
+                if (is_null($rgstrData['domain'])) {
+                    $rgstrData['domain'] = (int)$this->gadget->registry->fetch('default_domain');
+                }
+
+                // validate url
+                if (!preg_match('|^\S+://\S+\.\S+.+$|i', $rgstrData['url'])) {
+                    $rgstrData['url'] = '';
+                }
+
+                $dob = null;
+                if (!empty($rgstrData['dob'])) {
+                    $dob = Jaws_Date::getInstance()->ToBaseDate(explode('-', $rgstrData['dob']), 'Y-m-d');
+                    $dob = $GLOBALS['app']->UserTime2UTC($dob, 'Y-m-d');
+                }
+                $rgstrData['dob'] = $dob;
+
+                $userData = $this->gadget->model->load('Registration')->InsertUser($rgstrData);
+                if (Jaws_Error::IsError($userData)) {
+                    throw new Exception($userData->getMessage(), 401);
+                }
+
+                if ($this->gadget->registry->fetch('anon_activation') == 'user')
+                {
+                    $rgstrData['regstep'] = 1;
+                    $this->gadget->session->update('temp.register.user', $userData);
+
+                    // send notification to user
+                    $this->gadget->action->load('Registration')->NotifyRegistrationKey($userData);
+
+                    throw new Exception(_t('GLOBAL_LOGINKEY_REQUIRED'), 206);
+                } else {
+                    $rgstrData['regstep'] = 2;
+                    throw new Exception(_t('USERS_REGISTRATION_REGISTERED'), 201);
+                }
+            } else {
+                // fetch user data from session
+                $userData = $this->gadget->session->fetch('temp.register.user');
+                if (empty($userData)) {
+                    $rgstrData['regstep'] = 0;
+                    throw new Exception(_t('GLOBAL_LOGINKEY_REQUIRED'), 401);
+                }
+
+                $regkey = $this->gadget->session->fetch('regkey');
+                if (!isset($regkey['text']) || ($regkey['time'] < (time() - 300))) {
+                    // send notification to user
+                    $this->gadget->action->load('Registration')->NotifyRegistrationKey($userData);
+
+                    throw new Exception(_t('GLOBAL_LOGINKEY_REQUIRED'), 206);
+                }
+
+                // check verification key
+                if ($regkey['text'] != $rgstrData['regkey']) {
+                    throw new Exception(_t('GLOBAL_LOGINKEY_REQUIRED'), 206);
+                }
+            }
+
+            $rgstrData['regstep'] = 2;
+            $this->gadget->session->delete('temp.register.user');
+            throw new Exception(_t('USERS_REGISTRATION_REGISTERED'), 201);
+
+        } catch (Exception $error) {
+            unset($rgstrData['password'], $rgstrData['password_check']);
+            $this->gadget->session->push(
+                $error->getMessage(),
+                'Registration.Response',
+                ($error->getCode() == 201)? RESPONSE_NOTICE : RESPONSE_ERROR,
+                $rgstrData
+            );
+
+            return Jaws_Error::raiseError($error->getMessage(), $error->getCode());
+        }
+    }
+
+    /**
+     * Register Error
+     *
+     * @access  public
+     * @return  string  XHTML content
+     */
+    function RegisterError($error, $authtype)
+    {
+        $urlParams = array();
+        if (!empty($authtype)) {
+            $urlParams['authtype'] = $authtype;
+        }
+
+        http_response_code($error->getCode());
+        return Jaws_Header::Location($this->gadget->urlMap('Registration', $urlParams));
+    }
+
+}
---
+++ gadgets/Users/Account/Default/Registration.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * Users Core Gadget
+ *
+ * @category    Gadget
+ * @package     Users
+ */
+class Users_Account_Default_Registration extends Users_Account_Default
+{
+    /**
+     * Builds the registration form
+     *
+     * @access  public
+     * @return  string  XHTML form
+     */
+    function Registration()
+    {
+        $this->AjaxMe('index.js');
+        // Load the template
+        $tpl = $this->gadget->template->load('Registration.html');
+        $tpl->SetBlock('registration');
+        $tpl->SetVariable('title', _t('USERS_REGISTER'));
+
+        $response = $this->gadget->session->pop('Registration.Response');
+        if (!isset($response['data'])) {
+            $reqpost = array(
+                'domain'   => $this->gadget->registry->fetch('default_domain'),
+                'regstep'  => 0,
+                'username' => '',
+                'email'    => '',
+                'mobile'   => '',
+                'url'      => '',
+                'nickname' => '',
+                'fname'    => '',
+                'lname'    => '',
+                'ssn'      => '',
+                'dob'      => '',
+                'gender'   => 0
+            );
+        } else {
+            $reqpost = $response['data'];
+        }
+
+        if ($reqpost['regstep'] == 2) {
+            $this->RegistrationStep3($tpl, $reqpost);
+        } else {
+            $tpl->SetBlock('registration/request');
+
+            if (empty($reqpost['regstep'])) {
+                $this->RegistrationStep1($tpl, $reqpost);
+            } else {
+                $this->RegistrationStep2($tpl, $reqpost);
+            }
+
+            //captcha
+            $mPolicy = Jaws_Gadget::getInstance('Policy')->action->load('Captcha');
+            $mPolicy->loadCaptcha($tpl, 'LoginBox', 'login');
+
+            $tpl->SetVariable('register', _t('USERS_REGISTER'));
+            $tpl->ParseBlock('registration/request');
+        }
+
+        if (!empty($response)) {
+            $tpl->SetVariable('response_type', $response['type']);
+            $tpl->SetVariable('response_text', $response['text']);
+        }
+
+        $tpl->ParseBlock('registration');
+        return $tpl->Get();
+    }
+
+    /**
+     * Get HTML registration step 1 form
+     *
+     * @access  public
+     * @return  string  XHTML template
+     */
+    private function RegistrationStep1(&$tpl, $reqpost)
+    {
+        $block = $tpl->GetCurrentBlockPath();
+        $tpl->SetBlock("$block/reg_step_1");
+
+        $tpl->SetVariable('domain',    $reqpost['domain']);
+        $tpl->SetVariable('username',  $reqpost['username']);
+        $tpl->SetVariable('email',     $reqpost['email']);
+        $tpl->SetVariable('mobile',    $reqpost['mobile']);
+        $tpl->SetVariable('url',       $reqpost['url']);
+        $tpl->SetVariable('nickname',  $reqpost['nickname']);
+        $tpl->SetVariable('fname',     $reqpost['fname']);
+        $tpl->SetVariable('lname',     $reqpost['lname']);
+        $tpl->SetVariable('ssn',       $reqpost['ssn']);
+        $tpl->SetVariable('dob',       $reqpost['dob']);
+        $tpl->SetVariable("selected_gender_{$reqpost['gender']}", 'selected="selected"');
+        $tpl->SetVariable('lbl_account_info',  _t('USERS_ACCOUNT_INFO'));
+        $tpl->SetVariable('lbl_username',      _t('USERS_USERS_USERNAME'));
+        $tpl->SetVariable('validusernames',    _t('USERS_REGISTRATION_VALID_USERNAMES'));
+        $tpl->SetVariable('lbl_email',         _t('GLOBAL_EMAIL'));
+        $tpl->SetVariable('lbl_mobile',        _t('USERS_CONTACTS_MOBILE_NUMBER'));
+        $tpl->SetVariable('lbl_url',           _t('GLOBAL_URL'));
+        $tpl->SetVariable('lbl_nickname',       _t('USERS_USERS_NICKNAME'));
+        $tpl->SetVariable('lbl_password',      _t('USERS_USERS_PASSWORD'));
+        $tpl->SetVariable('sendpassword',      _t('USERS_USERS_SEND_AUTO_PASSWORD'));
+        $tpl->SetVariable('lbl_checkpassword', _t('USERS_USERS_PASSWORD_VERIFY'));
+        $tpl->SetVariable('lbl_personal_info', _t('USERS_PERSONAL_INFO'));
+        $tpl->SetVariable('lbl_fname',         _t('USERS_USERS_FIRSTNAME'));
+        $tpl->SetVariable('lbl_lname',         _t('USERS_USERS_LASTNAME'));
+        $tpl->SetVariable('lbl_gender',        _t('USERS_USERS_GENDER'));
+        $tpl->SetVariable('lbl_ssn',           _t('USERS_USERS_SSN'));
+        $tpl->SetVariable('gender_0',          _t('USERS_USERS_GENDER_0'));
+        $tpl->SetVariable('gender_1',          _t('USERS_USERS_GENDER_1'));
+        $tpl->SetVariable('gender_2',          _t('USERS_USERS_GENDER_2'));
+        $tpl->SetVariable('lbl_dob',           _t('USERS_USERS_BIRTHDAY'));
+        $tpl->SetVariable('dob_sample',        _t('USERS_USERS_BIRTHDAY_SAMPLE'));
+
+        $tpl->ParseBlock("$block/reg_step_1");
+    }
+
+    /**
+     * Get HTML registration step 2 form
+     *
+     * @access  public
+     * @return  string  XHTML template
+     */
+    private function RegistrationStep2(&$tpl, $reqpost)
+    {
+        $block = $tpl->GetCurrentBlockPath();
+        $tpl->SetBlock("$block/reg_step_2");
+
+        $tpl->SetVariable('username', isset($reqpost['username'])? $reqpost['username'] : '');
+        $tpl->SetVariable('email',    isset($reqpost['email'])? $reqpost['email'] : '');
+        $tpl->SetVariable('mobile',   isset($reqpost['mobile'])? $reqpost['mobile'] : '');
+
+        $tpl->SetVariable('lbl_username', _t('GLOBAL_USERNAME'));
+        $tpl->SetVariable('lbl_email', _t('GLOBAL_EMAIL'));
+        $tpl->SetVariable('lbl_mobile', _t('USERS_CONTACTS_MOBILE_NUMBER'));
+        $tpl->SetVariable('lbl_regkey', _t('USERS_REGISTRATION_KEY'));
+
+        $tpl->ParseBlock("$block/reg_step_2");
+    }
+
+    /**
+     * Get HTML registration step 3
+     *
+     * @access  public
+     * @return  string  XHTML template
+     */
+    private function RegistrationStep3(&$tpl, $reqpost)
+    {
+        $block = $tpl->GetCurrentBlockPath();
+        $tpl->SetBlock("$block/reg_step_3");
+
+        $tpl->ParseBlock("$block/reg_step_3");
+    }
+
+}
