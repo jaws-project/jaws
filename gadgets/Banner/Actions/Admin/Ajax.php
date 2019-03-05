adfe8945d5d9232ece42b828a2f787660f9706b9
Ali Fazelzadeh <afz@php.net>
2019-03-05 22:22:41 +0330

    [Banner]: Fixed fetch template

--- gadgets/Banner/Actions/Admin/Ajax.php
+++ gadgets/Banner/Actions/Admin/Ajax.php
@@ -112,7 +112,7 @@ class Banner_Actions_Admin_Ajax extends Jaws_Gadget_Action
         @list($domain, $title, $url, $gid, $banner, $template, $views_limit,
             $clicks_limit, $start_time, $stop_time, $random, $published
         ) = $this->gadget->request->fetchAll('post');
-        $template = $this->gadget->request->fetch(4, 'post', 'strip_crlf');
+        $template = $this->gadget->request->fetch(5, 'post', 'strip_crlf');
         if ($this->gadget->registry->fetch('multi_domain', 'Users') != 'true') {
             $domain = 0;
         }
@@ -135,7 +135,7 @@ class Banner_Actions_Admin_Ajax extends Jaws_Gadget_Action
         @list($bid, $domain, $title, $url, $gid, $banner, $template, $views_limit,
             $clicks_limit, $start_time, $stop_time, $random, $published
         ) = $this->gadget->request->fetchAll('post');
-        $template = $this->gadget->request->fetch(5, 'post', 'strip_crlf');
+        $template = $this->gadget->request->fetch(6, 'post', 'strip_crlf');
         if ($this->gadget->registry->fetch('multi_domain', 'Users') != 'true') {
             $domain = 0;
         }
