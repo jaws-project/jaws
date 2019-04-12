05a75883a6a23d3b388a4eee0ff8d48e6469c607
afz <afzcode@gmail.com>
2019-04-12 11:27:48 +0430

    [Files]: using Jaws_Utils delete method

--- gadgets/FileBrowser/Model/Admin/Files.php
+++ gadgets/FileBrowser/Model/Admin/Files.php
@@ -148,7 +148,7 @@ class FileBrowser_Model_Admin_Files extends Jaws_Gadget_Model
                 return false;
             }
         } elseif (is_dir($filename)) {
-            $return = Jaws_FileManagement::FullRemoval($filename);
+            $return = Jaws_Utils::delete($filename);
             if (!$return) {
                 $GLOBALS['app']->Session->PushLastResponse(_t('FILEBROWSER_ERROR_CANT_DELETE_DIR', $file), RESPONSE_ERROR);
                 return false;
