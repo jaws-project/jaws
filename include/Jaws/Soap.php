0840cd5e369a8c660de091b1841e169754b4247f
afz <afzcode@gmail.com>
2019-05-13 10:39:23 +0430

    [Jaws_Soap_Client]: Added simple wrapper for soap client request for cache supporting

---
+++ include/Jaws/Soap.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Class that deals like a wrapper between Jaws and PHP::SoapClient/PHP::SoapServer
+ *
+ * @category    Application
+ * @package     Core
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2019 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/lesser.html
+ */
+class Jaws_Soap
+{
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @return  void
+     */
+    function __construct()
+    {
+    }
+
+}
---
+++ include/Jaws/Soap/Client.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Class that deals like a wrapper between Jaws and PHP/SoapClient
+ *
+ * @category    Application
+ * @package     Core
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2019 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/lesser.html
+ */
+class Jaws_Soap_Client extends Jaws_Soap
+{
+    /**
+     * @access  private
+     * @var int $expires    Cache expires time(second)
+     */
+    private $expires = 0;
+
+    /**
+     * @access  private
+     * @var int $refresh    Refresh/Update cache
+     */
+    private $refresh = false;
+
+    /**
+     * @access  private
+     * @var int $request_cache_key  Cache key
+     */
+    private $request_cache_key = 0;
+
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @return  void
+     */
+    function __construct($wsdl, $options = array())
+    {
+        if (!extension_loaded('soap')) {
+            return Jaws_Error::raiseError('SOAP extension is not available.', __CLASS__);
+        }
+
+        try {
+            $this->SoapClient = new SoapClient($wsdl, $options);
+        } catch (Exception $error) {
+            return Jaws_Error::raiseError($error->getMessage(), __CLASS__);
+        }
+    }
+
+    /**
+     * Set cache options
+     *
+     * @access  private
+     * @param   int     $expires    Cache expires time(second)
+     * @param   bool    $refresh    Refresh/Update cache
+     * @return  void
+     */
+    function setCacheOptions($expires = 0, $refresh = false)
+    {
+        $this->expires = $expires;
+        $this->refresh = $refresh;
+    }
+
+    /**
+     * Overloading __call magic method
+     *
+     * @access  private
+     * @param   string  $method     Method name
+     * @param   string  $arguments  Method parameters
+     * @return  mixed   Requested object otherwise Jaws_Error
+     */
+    function __call($method, $arguments)
+    {
+        // request cache key
+        $this->request_cache_key = Jaws_Cache::key($arguments);
+
+        try {
+            if ($this->refresh ||
+                false === $result = @unserialize($GLOBALS['app']->Cache->get($this->request_cache_key))
+            ) {
+                $result = call_user_func_array(array($this->SoapClient, $method), $arguments);
+                if (is_soap_fault($result)) {
+                    throw new Exception($result->faultstring, $result->faultcode);
+                }
+
+                // set cache
+                $GLOBALS['app']->Cache->set(
+                    $this->request_cache_key,
+                    serialize($result),
+                    $this->expires
+                );
+            }
+
+            return $result;
+        } catch (Exception $error) {
+            return Jaws_Error::raiseError(
+                $error->getMessage(),
+                __FUNCTION__,
+                JAWS_ERROR_ERROR,
+                1
+            );
+        }
+    }
+
+    /**
+     * Delete cache
+     *
+     * @access  public
+     * @param   int     $key    Cache key
+     * @return  mixed
+     */
+    function deleteCache($key = 0)
+    {
+        return $GLOBALS['app']->Cache->delete(empty($key)? $this->request_cache_key : $key);
+    }
+
+}
