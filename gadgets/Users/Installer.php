953da0e7f6374ff80fe3bab3a68df90fc33e4ad7
afz <afzcode@gmail.com>
2019-09-28 03:16:31 +0330

    [Users]: Added some new field for support contact photo and prevent duplicate contact

--- gadgets/Users/Installer.php
+++ gadgets/Users/Installer.php
@@ -293,7 +293,14 @@ class Users_Installer extends Jaws_Gadget_Installer
         }
 
         if (version_compare($old, '3.12.0', '<')) {
-            $result = $this->installSchema('schema.xml', array(), '3.5.0.xml');
+            $result = $this->installSchema('3.12.0.xml', array(), '3.5.0.xml');
+            if (Jaws_Error::IsError($result)) {
+                return $result;
+            }
+        }
+
+        if (version_compare($old, '3.14.0', '<')) {
+            $result = $this->installSchema('schema.xml', array(), '3.12.0.xml');
             if (Jaws_Error::IsError($result)) {
                 return $result;
             }
