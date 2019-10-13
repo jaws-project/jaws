6a45c18454e09aa3ac7533cc2ff9b98519d8d1f8
afz <afzcode@gmail.com>
2019-10-13 08:30:09 +0330

    [ControlPanel]: Applyed new changes related to push/pop response

--- gadgets/ControlPanel/Actions/Admin/ControlPanel.php
+++ gadgets/ControlPanel/Actions/Admin/ControlPanel.php
@@ -86,7 +86,7 @@ class ControlPanel_Actions_Admin_ControlPanel extends Jaws_Gadget_Action
             $tpl->ParseBlock('layout/warning');
         }
 
-        $responses = $this->app->session->PopLastResponse();
+        $responses = $this->gadget->session->pop();
         if ($responses) {
             $tpl->SetVariable('response_text', $responses[0]['text']);
             $tpl->SetVariable('response_type', $responses[0]['type']);
