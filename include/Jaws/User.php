6bca9593fae0f9680e5d1130be87f5a7374f9032
afz <afzcode@gmail.com>
2019-10-13 11:55:15 +0330

    [Jaws_User]: Fixed empty password check when trying update user

--- include/Jaws/User.php
+++ include/Jaws/User.php
@@ -1190,7 +1190,7 @@ class Jaws_User
         }
 
         // password & complexity
-        if (array_key_exists('password', $uData)) {
+        if (array_key_exists('password', $uData) && $uData['password']!== '') {
             $min = (int)$this->app->registry->fetch('password_min_length', 'Policy');
             if (!preg_match("/^[[:print:]]{{$min},24}$/", $uData['password'])) {
                 return Jaws_Error::raiseError(
