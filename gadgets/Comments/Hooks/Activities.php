421aa47a7abf5fd7b3a77b9c8ff7e8adf8503dea
afz <afzcode@gmail.com>
2019-10-25 19:19:09 +0330

    [Comments]: @2020

--- gadgets/Comments/Hooks/Activities.php
+++ gadgets/Comments/Hooks/Activities.php
@@ -5,7 +5,7 @@
  * @category    GadgetHook
  * @package     Users
  * @author      Mojtaba Ebrahimi <ebrahimi@zehneziba.ir>
- * @copyright   2008-2016 Jaws Development Group
+ * @copyright   2008-2020 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/lesser.html
  */
 class Comments_Hooks_Activities extends Jaws_Gadget_Hook
