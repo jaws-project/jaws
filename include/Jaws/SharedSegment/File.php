eb86ab56d84367126e78d22a88e4fe3d58f0a294
afz <afzcode@gmail.com>
2019-10-25 19:18:10 +0330

    @2020

--- include/Jaws/SharedSegment/File.php
+++ include/Jaws/SharedSegment/File.php
@@ -5,7 +5,7 @@
  * @category    FileMemory
  * @package     Core
  * @author      Ali Fazelzadeh <afz@php.net>
- * @copyright   2019 Jaws Development Group
+ * @copyright   2019-2020 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/lesser.html
  */
 class Jaws_SharedSegment_File extends Jaws_SharedSegment
