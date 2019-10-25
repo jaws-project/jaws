2d0e8f33f423e28deac48452dc14df29b884d513
afz <afzcode@gmail.com>
2019-10-25 19:20:41 +0330

    [AccessLimiter]: @2020

--- plugins/AccessLimiter/Plugin.php
+++ plugins/AccessLimiter/Plugin.php
@@ -5,7 +5,7 @@
  * @category   Plugin
  * @package    AccessLimiter
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2009-2015 Jaws Development Group
+ * @copyright  2009-2020 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class AccessLimiter_Plugin extends Jaws_Plugin
