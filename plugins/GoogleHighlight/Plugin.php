d3d74ea030120249f3ce0b0ecec1a4c4d52c7bc1
afz <afzcode@gmail.com>
2019-10-25 19:20:46 +0330

    [GoogleHighlight]: @2020

--- plugins/GoogleHighlight/Plugin.php
+++ plugins/GoogleHighlight/Plugin.php
@@ -5,7 +5,7 @@
  * @category   Plugin
  * @package    GoogleHighlight
  * @author     Pablo Fischer <pablo@pablo.com.mx>
- * @copyright  2004-2015 Jaws Development Group
+ * @copyright  2004-2020 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class GoogleHighlight_Plugin extends Jaws_Plugin
