377512798f9d5f5bd626c19dbc4a7b32f0a625e7
afz <afzcode@gmail.com>
2019-10-25 15:51:26 +0330

    [Shoutbox]: Renamed jaws/data path definition constant

--- gadgets/Shoutbox/Actions/Admin/Settings.php
+++ gadgets/Shoutbox/Actions/Admin/Settings.php
@@ -32,7 +32,7 @@ class Shoutbox_Actions_Admin_Settings extends Shoutbox_Actions_Admin_Default
             $form->Add(Piwi::CreateWidget('HiddenEntry', 'gadget', 'Shoutbox'));
             $form->Add(Piwi::CreateWidget('HiddenEntry', 'action', 'UpdateProperties'));
 
-            include_once JAWS_PATH . 'include/Jaws/Widgets/FieldSet.php';
+            include_once ROOT_JAWS_PATH . 'include/Jaws/Widgets/FieldSet.php';
             $fieldset = new Jaws_Widgets_FieldSet(_t('SHOUTBOX_SETTINGS'));
             $fieldset->SetDirection('vertical');
 
