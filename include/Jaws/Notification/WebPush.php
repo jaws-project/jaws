6a6c722c292bc0958993b4197aa1aaafc6fe39c3
afz <afzcode@gmail.com>
2019-11-02 13:19:55 +0330

    [Jaws_Notification_WebPush]: Added WebPush notification driver

---
+++ include/Jaws/Notification/WebPush.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Web push notification class
+ *
+ * @category    Notification
+ * @package     Core
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2019-2020 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/lesser.html
+ */
+class Jaws_Notification_WebPush extends Jaws_Notification
+{
+    /**
+     * Driver title
+     *
+     * @access  protected
+     * @var     string
+     */
+    protected $title = 'Jaws WebPush API';
+
+    /**
+     * Driver type
+     *
+     * @access  protected
+     * @var     int
+     */
+    protected $type = Jaws_Notification::WEB_DRIVER;
+
+    /**
+     * Store $webPush object instance
+     * @var     array
+     * @access  private
+     */
+    private $webPush;
+
+    /**
+     * constructor
+     *
+     * @access  protected
+     * @param   array $options Associated options array
+     */
+    protected function __construct($options = array())
+    {
+        parent::__construct();
+
+        require_once ROOT_JAWS_PATH . 'libraries/php/WebPush/WebPush.php';
+        require_once ROOT_JAWS_PATH . 'libraries/php/WebPush/Subscription.php';
+
+        $auth = array(
+            'VAPID' => array(
+                'subject' => $this->app->getSiteURL('/', false),
+                'publicKey' => Jaws_Gadget::getInstance('Notification')->registry->fetch('webpush_pub_key'),
+                'privateKey' => Jaws_Gadget::getInstance('Notification')->registry->fetch('webpush_pvt_key'),
+            ),
+        );
+
+        $this->webPush = new WebPush($auth);
+    }
+
+    /**
+     * Get options list
+     *
+     * @access  public
+     * @return  mixed   Jaws_Error on failure
+     */
+    function getDriverOptions()
+    {
+        return array();
+    }
+
+    /**
+     * Sends notify to user
+     *
+     * @access  public
+     * @param   array   $contacts       Contacts array
+     * @param   string  $title          Notification title
+     * @param   string  $summary        Notification summary
+     * @param   string  $content        Notification content
+     * @param   integer $time           Time of notify(timestamps)
+     * @param   string  $callback_url   Notification callback URL
+     * @param   string  $image          Notification image
+     * @param   string  $template       Notification template
+     * @return  mixed   Jaws_Error on failure
+     * @throws ErrorException
+     */
+    function notify($contacts, $title, $summary, $content, $time, $callback_url, $image, $template)
+    {
+        try {
+            foreach ($contacts as $subscriber) {
+                $subscriber['webpush'] = @unserialize($subscriber['webpush']);
+                if (!empty($subscriber['webpush'])) {
+                    $subscription = Subscription::create($subscriber['webpush']);
+                    $res = $this->webPush->sendNotification(
+                        $subscription,
+                        json_encode(
+                            array(
+                                'icon'    => $image,
+                                'title'   => $title,
+                                'body'    => $summary,
+                                'vibrate' => [],
+                            )
+                        )
+                    );
+                }
+            }
+            $this->webPush->flush();
+        } catch (Exception $error) {
+            return $error
+        }
+
+        return true;
+    }
+
+}
