5591ff6d1b7a750bd4a1daf43f0a22029bf350f5
afz <afzcode@gmail.com>
2019-12-13 15:23:15 +0330

    [Notification]: Applied new changes of show message/loading after call action via ajax

--- gadgets/Notification/Resources/script.js
+++ gadgets/Notification/Resources/script.js
@@ -13,25 +13,21 @@ var NotificationCallback = {
             getDG('notification_drivers_datagrid', $('#notification_drivers_datagrid')[0].getCurrentPage(), true);
             stopAction();
         }
-        NotificationAjax.showResponse(response);
-
     },
     UninstallNotificationDriver: function(response) {
         if (response['type'] == 'alert-success') {
             getDG('notification_drivers_datagrid', $('#notification_drivers_datagrid')[0].getCurrentPage(), true);
             stopAction();
         }
-        NotificationAjax.showResponse(response);
     },
     UpdateNotificationDriver: function(response) {
         if (response['type'] == 'alert-success') {
             getDG('notification_drivers_datagrid', $('#notification_drivers_datagrid')[0].getCurrentPage(), true);
             stopAction();
         }
-        NotificationAjax.showResponse(response);
     },
     SaveSettings: function(response) {
-        NotificationAjax.showResponse(response);
+        //
     }
 };
 
