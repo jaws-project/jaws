89bf1af5efa99f1f78447859d35abcfb8138121d
afz <afzcode@gmail.com>
2020-05-06 01:47:56 +0430

    [Jaws_Captcha_Simple]: Added support unicode characters

--- include/Jaws/Captcha/Simple.php
+++ include/Jaws/Captcha/Simple.php
@@ -12,6 +12,26 @@
 class Jaws_Captcha_Simple extends Jaws_Captcha
 {
     /**
+     * Install captcha driver
+     *
+     * @access  public
+     * @return  mixed   True on success otherwise Jaws_Error on failure
+     */
+    function install()
+    {
+        if (is_null($this->app->registry->fetch('captcha_simple_collection', 'Policy'))) {
+            $this->app->registry->insert(
+                'captcha_simple_collection',
+                'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+                false,
+                'Policy'
+            );
+        }
+
+        return true;
+    }
+
+    /**
      * Displays the captcha image
      *
      * @access  public
@@ -20,35 +40,37 @@ class Jaws_Captcha_Simple extends Jaws_Captcha
      */
     function image($key)
     {
-        $value  = Jaws_Utils::RandomText();
-        $result = $this->update($key, $value);
+        $collection = $this->app->registry->fetch('captcha_simple_collection', 'Policy');
+        $text = Jaws_Utils::RandomText(6, array('collection' => $collection));
+        $result = $this->update($key, $text);
         if (Jaws_Error::IsError($result)) {
             $value = '';
         }
+        $fake = Jaws_Utils::RandomText(6, array('collection' => $collection));
 
-        $bg = dirname(__FILE__) . '/resources/simple.bg.png';
-        $im = imagecreatefrompng($bg);
+        $text = implode('', array_map('Jaws_UTF8::chr', Jaws_Bidi::utf8Bidi($text, 'AL')));
+        $fake = implode('', array_map('Jaws_UTF8::chr', Jaws_Bidi::utf8Bidi($fake, 'AL')));
+
+        // create image
+        $im = imagecreatetruecolor(120, 28);
+        imagefill($im, 0, 0, imagecolorallocate($im, 255, 255, 255));
         imagecolortransparent($im, imagecolorallocate($im, 255, 255, 255));
-        // Write it in a random position..
-        $darkgray = imagecolorallocate($im, 0x10, 0x70, 0x70);
-        $x = 5; 
-        $y = 20;
-        $text_length = strlen($value);
-        for ($i = 0; $i < $text_length; $i++) {
-            $fnt = rand(7,10);
-            $y = rand(6, 10);
-            imagestring($im, $fnt, $x, $y, $value{$i} , $darkgray);
-            $x = $x + rand(15, 25);
-        }
+        $font = dirname(__FILE__) . '/resources/courbd.ttf';
 
-        header("Content-Type: image/png");
+        // shadow
+        $shadow = imagecolorallocate($im, 0xaa, 0xaa, 0xaa);
+        imagettftext($im, 18, 0, 8, 19, $shadow, $font, $fake);
+        // text
+        $color = imagecolorallocate($im, 0x55, 0x55, 0x55);
+        imagettftext($im, 18, 0, 11, 22, $color, $font, $text);
 
         ob_start();
         imagepng($im);
         $content = ob_get_contents();
         ob_end_clean();
-
         imagedestroy($im);
+
+        header('Content-Type: image/png');
         return $content;
     }
 
