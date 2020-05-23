0b2cbfbcb833d21a8a9c6ac7f264c1f9c1053ec6
afz <afzcode@gmail.com>
2020-05-23 17:18:14 +0430

    [PrivateMessage]: Applied new changes of Notify event

--- gadgets/PrivateMessage/Model/Message.php
+++ gadgets/PrivateMessage/Model/Message.php
@@ -425,10 +425,14 @@ class PrivateMessage_Model_Message extends Jaws_Gadget_Model
                     // send notification on new private message
                     if (!$is_notification) {
                         $params = array();
-                        $params['key'] = crc32('PrivateMessage' . $senderMessageId);
+                        $params['key']  = $senderMessageId;
+                        $params['name'] = 'PrivateMessage';
                         $params['title'] = _t('PRIVATEMESSAGE_NEW_MESSAGE_NOTIFICATION_TITLE');
                         $params['summary'] = _t('PRIVATEMESSAGE_NEW_MESSAGE_NOTIFICATION');
-                        $params['description'] = _t('PRIVATEMESSAGE_NEW_MESSAGE_NOTIFICATION_DESC', $data['subject']);
+                        $params['verbose'] = _t(
+                            'PRIVATEMESSAGE_NEW_MESSAGE_NOTIFICATION_DESC',
+                            $data['subject']
+                        );
                         $params['user'] = (int)$recipient_user;
                         $this->gadget->event->shout('Notify', $params);
                     }
