d90d95875e72254c24bf675039ca672399da15be
afz <afzcode@gmail.com>
2020-05-28 00:59:47 +0430

    [Jaws.js]: Added drill-down menu plugin

--- include/Jaws/Resources/Jaws.js
+++ include/Jaws/Resources/Jaws.js
@@ -150,6 +150,304 @@ jQuery.extend({
 })(jQuery);
 
 
+(function($) {
+    /*
+     * https://github.com/mewsoft
+    */
+    $.fn.drilldown = function(options) {
+        //set default options
+        var defaults = {
+            wrapper_class      : 'drilldown',
+            menu_class         : 'drilldown-menu',
+            submenu_class      : 'nav',
+            parent_class       : 'dd-parent',
+            parent_class_link  : 'dd-parent-a',
+            active_class       : 'active',
+            header_class_list  : 'breadcrumb',
+            header_class       : 'breadcrumbwrapper',
+            speed              : 'slow',
+            save_state         : true,
+            icon_class         : 'glyphicon glyphicon-chevron-right',
+            reset_text         : '<span class="glyphicon glyphicon-home"></span>', // All
+            default_text       : 'Menu Title',
+            show_end_nodes     : true, // drill to final empty nodes
+            header_tag         : 'div',// h3
+            header_tag_class   : 'list-group-item active', // hidden list-group-item active
+            storage            : null
+        };
+
+        //call in the default options
+        var options = $.extend(defaults, options);
+
+        //act upon the element that is passed into the design
+        return this.each(function(options) {
+
+            var $ddObj = this;
+            $($ddObj).addClass(defaults.menu_class);
+            $($ddObj).addClass(defaults.submenu_class);
+            $($ddObj).removeClass('hidden');
+            var $wrapperObj = '<div class="'+defaults.wrapper_class+'" />';
+            $($ddObj).wrap($wrapperObj);
+            var $wrapper = $($ddObj).parent();
+            var objIndex = $($wrapper).index('.'+defaults.wrapper_class);
+            var idHeader = defaults.header_class+'-'+objIndex;
+            var idWrapper = defaults.wrapper_class+'-'+objIndex;
+            $($wrapper).attr('id',idWrapper);
+            var $header = '<div id="'+idHeader+'" class="'+defaults.header_class+'"></div>';
+
+            setUpDrilldown();
+
+            // select storage saved selection
+            if(defaults.save_state == true) {
+                let keyId = defaults.wrapper_class+'-'+objIndex;
+                readStorage(keyId, $ddObj);
+            }
+
+            resetDrilldown($ddObj, $wrapper);
+
+            $(window).resize(function(){
+                resizeDrilldown($ddObj, $wrapper);
+            });
+
+            $('li a',$ddObj).click(function(e) {
+                $link = this;
+                $activeLi = $(this).parent('li').stop();
+                $siblingsLi = $($activeLi).siblings();
+
+                // Drilldown action
+                if($('> ul',$activeLi).length || defaults.show_end_nodes){
+                    if($($link).hasClass(defaults.active_class)){
+                        $('ul a',$activeLi).removeClass(defaults.active_class);
+                        resetDrilldown($ddObj, $wrapper);
+                    } else {
+                        actionDrillDown($activeLi, $wrapper, $ddObj);
+                    }
+                }
+
+                // scroll to top on click
+                $($wrapper,$ddObj).scrollTop(0);
+
+                // Prevent browsing to link if has child links
+                if($(this).next('ul').length > 0){
+                    e.preventDefault();
+                    e.stopPropagation();
+                    $($link).trigger('drilldown.parentclick');
+                } else {
+                    //$($link).trigger('click');
+                    $($link).addClass(defaults.active_class);
+                }
+
+                $($link).trigger('drilldown.linklclick');
+
+            });
+
+            // Breadcrumbs
+            $('#'+idHeader).on('click', 'a', function(e) {
+
+                if($(this).hasClass('link-back')){
+                    linkIndex = $('#'+idWrapper+' .'+defaults.parent_class_link+'.active').length;
+                    linkIndex = linkIndex-2;
+                    $('a.'+defaults.active_class+':last', $ddObj).removeClass(defaults.active_class);
+                } else {
+                    // Get link index
+                    var linkIndex = parseInt($(this).index('#'+idHeader+' a'));
+                    if(linkIndex == 0){
+                        $('a',$ddObj).removeClass(defaults.active_class);
+                    } else {
+                        // Select equivalent active link
+                        linkIndex = linkIndex-1;
+                        $('a.'+defaults.active_class+':gt('+linkIndex+')',$ddObj).removeClass(defaults.active_class);
+                    }
+                }
+
+                resetDrilldown($ddObj, $wrapper);
+
+                e.preventDefault();
+                $($ddObj).trigger('drilldown.linklclick');
+            });
+
+            // Set up accordion
+            function setUpDrilldown() {
+                $('ul',$ddObj).each(function(){
+                    $(this).addClass(defaults.submenu_class);
+                });
+
+                $($ddObj).before($header);
+
+                $arrow = '<span class="'+defaults.icon_class+'"></span>';
+
+                // Set sub menu width and offset
+                $('li',$ddObj).each(function() {
+                    if($('> ul',this).length){
+                        $(this).addClass(defaults.parent_class);
+                        $('> a',this).addClass(defaults.parent_class_link).append($arrow);
+                    }
+                });
+
+                // Add css class
+                $('ul',$wrapper).each(function() {
+                    //$(this).addClass(defaults.submenu_class);
+                    $('li:last',this).addClass('last');
+                });
+
+                $('> ul > li:last',$wrapper).addClass('last');
+
+            }
+        });
+
+        // Drill Down
+        function actionDrillDown(element, wrapper, obj) {
+            //actionDrillDown($activeLi, $wrapper, $ddObj);
+            // Declare header
+            var $header = $('.'+defaults.header_class, wrapper);
+
+            // Get new breadcrumb and header text
+            var getNewBreadcrumb = $(defaults.header_tag,$header).text(); // .html()
+            var getNewHeaderText = $('> a',element).text(); // .html()
+
+            // Add new breadcrumb
+            if(!$('ul',$header).length){
+                $($header).prepend('<ul class="'+defaults.header_class_list+'"></ul>');
+            }
+            if(getNewBreadcrumb == defaults.default_text){
+                $('ul li:last-child',$header).remove();
+                // edit
+                $('ul',$header).append('<li class="first"><a href="#">'+defaults.reset_text+'</a></li>');
+            } else {
+                $('ul li:last-child',$header).remove();
+                $('ul',$header).append('<li><a href="#">'+getNewBreadcrumb+'</a></li>');
+            }
+            $('ul',$header).append('<li class="active">'+getNewHeaderText+'</li>');
+
+            // Update header text
+            updateHeader($header, getNewHeaderText);
+
+            // declare child link
+            var activeLink = $('> a',element);
+
+            // add active class to link
+            $(activeLink).addClass(defaults.active_class);
+
+            // Find all sibling items & hide
+            var $siblingsLi = $(element).siblings();
+            $($siblingsLi).hide();
+
+            $(activeLink).hide();
+            $('> ul li',element).show();
+            $('> ul',element)
+                .css({'overflow-x':'hidden', 'white-space':'nowrap'})
+                .show()
+                .animate({"margin-right": 0}, defaults.speed);
+
+            // Write cookie if save state is on
+            if(defaults.save_state == true) {
+                let keyId = $(wrapper).attr('id');
+                writeStorage(keyId, obj);
+            }
+        }
+
+        function updateHeader(obj, html) {
+            if(!$(defaults.header_tag,obj).length) {
+                $(obj).append('<'+defaults.header_tag+' class="hidden '+defaults.header_tag_class+'"</'+defaults.header_tag+'>');
+            }
+
+            $(defaults.header_tag,obj).html(html);
+        }
+
+        // Reset accordion using active links
+        function resetDrilldown(obj, wrapper) {
+            var $header = $('.'+defaults.header_class, wrapper);
+            $('ul',$header).remove();
+            $('a',$header).remove();
+            $('li',obj).show();
+            $('a',obj).show();
+            var menuWidth = $(obj).outerWidth(true);
+            $('ul',obj).hide().css('margin-right', menuWidth + 'px');
+
+            updateHeader($header, defaults.default_text);
+            
+            // Add new breadcrumb
+            var getNewBreadcrumb = $(defaults.header_tag,$header).text(); // .html()
+            if(!$('ul',$header).length){
+                // edit
+                $($header).prepend('<ul class="'+defaults.header_class_list+'"></ul>');
+            }
+            if(getNewBreadcrumb == defaults.default_text){
+                // edit
+                $('ul',$header).append('<li class="active">'+defaults.default_text+'</li>');
+            }
+
+            // Write cookie if save state is on
+            if(defaults.save_state == true){
+                let keyId = $(wrapper).attr('id');
+                writeStorage(keyId, obj);
+            }
+
+            var activeObj = obj;
+            $('a.'+defaults.active_class,obj).each(function(i) {
+                var $activeLi = $(this).parent('li').stop();
+                actionDrillDown($activeLi, wrapper, obj);
+                activeObj = $(this).parent('li');
+            });
+
+            var showObj;
+            if ($('> ul', activeObj).length) {
+                showObj = $('> ul', activeObj);
+            }
+            else {
+                // root ul
+                var showObj = activeObj;
+            }
+
+        }
+
+        /**
+         * retrieve storage value and set active items
+         *
+         * @param   string  keyId   Storage Key ID 
+         * @param   object  obj     jQuery DOM object
+         * @return  void
+         */
+        function readStorage(keyId, obj) {
+            var activeIndex = defaults.storage.fetch(keyId);
+            if(activeIndex != null) {
+                $.each(activeIndex, function(index, value){
+                    value = parseInt(value) + 0;
+                    var $li = $('li:eq('+value+')',obj);
+                    $('> a', $li).addClass(defaults.active_class);
+                });
+            }
+        }
+
+        /**
+         * write active menu index into local storage
+         *
+         * @param   string  keyId   Storage Key ID 
+         * @param   object  obj     jQuery DOM object
+         * @return  void
+         */
+        function writeStorage(keyId, obj) {
+            var activeIndex = [];
+            // create array of active items index value
+            $('a.'+defaults.active_class,obj).each(function(i){
+                var $arrayItem = $(this).parent('li');
+                var itemIndex = $('li',obj).index($arrayItem);
+                activeIndex.push(itemIndex);
+            });
+
+            defaults.storage.update(keyId, activeIndex);
+        }
+
+        function resizeDrilldown($ddObj, $wrapper) {
+                // set wrapper to auto width to force resize
+                $($wrapper).css({width: 'auto'});
+                $($ddObj).css({width: 'auto'});
+                resetDrilldown($ddObj, $wrapper);
+        }
+
+    };
+})(jQuery);
+
 /**
  * Ajax base class
  *
