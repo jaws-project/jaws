99b2778f6e3cdcf8d7773d4818a9f00efafa21d9
afz <afzcode@gmail.com>
2020-05-28 03:43:43 +0430

    [Menu]: cosmetics

--- gadgets/Menu/Resources/index.js
+++ gadgets/Menu/Resources/index.js
@@ -18,19 +18,19 @@ function Jaws_Gadget_Menu() { return {
     init: function(mainGadget, mainAction)
     {
         $('.drilldown').drilldown({
-            wrapper_class	    : 'drilldown panel panel-success',
-            menu_class		    : 'drilldown-menu',
-            submenu_class	    : 'nav ',
-            parent_class		: 'dd-parent',
-            parent_class_link	: 'dd-parent-a',
-            active_class		: 'active',
+            wrapper_class       : 'drilldown panel panel-success',
+            menu_class          : 'drilldown-menu',
+            show_submenu_icon   : false,
+            parent_class        : '',
+            parent_class_link   : '',
+            active_class        : 'active',
             header_class_list   : 'breadcrumb',
-            header_class		: 'breadcrumbwrapper',
-            speed       	    : 200,
-            save_state		    : true,
-            default_text	    : this.gadget.defines.title,
+            header_class        : 'breadcrumbwrapper',
+            speed               : 0,
+            save_state          : true,
+            default_text        : this.gadget.defines.title,
             show_end_nodes      : true, // drill to final empty nodes
-            header_tag		    : 'div',// h3
+            header_tag          : 'div',// h3
             header_tag_class    : 'list-group-item active', // hidden list-group-item active
             storage             : this.gadget.storage
         });
