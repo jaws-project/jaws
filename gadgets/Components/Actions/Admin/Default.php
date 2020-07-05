2e714c0fefdfb69cb0718ae5cbf9af2e22668649
afz <afzcode@gmail.com>
2020-07-05 21:33:35 +0430

    [Components]: gadget/action renamed to reqGadget/reqAction

--- gadgets/Components/Actions/Admin/Default.php
+++ gadgets/Components/Actions/Admin/Default.php
@@ -28,11 +28,11 @@ class Components_Actions_Admin_Default extends Jaws_Gadget_Action
         $menubar = new Jaws_Widgets_Menubar();
         if ($this->gadget->GetPermission('ManageGadgets')) {
             $menubar->AddOption('Gadgets', _t('COMPONENTS_GADGETS'),
-                                BASE_SCRIPT . '?gadget=Components&amp;action=Gadgets', 'gadgets/Components/Resources/images/gadgets.png');
+                                BASE_SCRIPT . '?reqGadget=Components&amp;reqAction=Gadgets', 'gadgets/Components/Resources/images/gadgets.png');
         }
         if ($this->gadget->GetPermission('ManagePlugins')) {
             $menubar->AddOption('Plugins', _t('COMPONENTS_PLUGINS'),
-                                BASE_SCRIPT . '?gadget=Components&amp;action=Plugins', 'gadgets/Components/Resources/images/plugins.png');
+                                BASE_SCRIPT . '?reqGadget=Components&amp;reqAction=Plugins', 'gadgets/Components/Resources/images/plugins.png');
         }
         $menubar->Activate($action);
         return $menubar->Get();
