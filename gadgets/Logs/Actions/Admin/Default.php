24cbed969ffcd3bd16ee6eda84b4ad131fc1ceee
afz <afzcode@gmail.com>
2020-07-05 21:33:42 +0430

    [Logs]: gadget/action renamed to reqGadget/reqAction

--- gadgets/Logs/Actions/Admin/Default.php
+++ gadgets/Logs/Actions/Admin/Default.php
@@ -27,12 +27,12 @@ class Logs_Actions_Admin_Default extends Jaws_Gadget_Action
         $menubar = new Jaws_Widgets_Menubar();
         $menubar->AddOption('Logs',
                             _t('LOGS_TITLE'),
-                            BASE_SCRIPT . '?gadget=Logs&amp;action=Logs',
+                            BASE_SCRIPT . '?reqGadget=Logs&amp;reqAction=Logs',
                             STOCK_NEW);
         if ($this->gadget->GetPermission('ManageSettings')) {
             $menubar->AddOption('Settings',
                                 _t('GLOBAL_PROPERTIES'),
-                                BASE_SCRIPT . '?gadget=Logs&amp;action=Settings',
+                                BASE_SCRIPT . '?reqGadget=Logs&amp;reqAction=Settings',
                                 STOCK_PREFERENCES);
         }
         $menubar->Activate($action);
