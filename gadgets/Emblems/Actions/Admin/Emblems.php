af4dc86625770019f740c00390d113f597bac152
afz <afzcode@gmail.com>
2020-07-05 21:33:37 +0430

    [Emblems]: gadget/action renamed to reqGadget/reqAction

--- gadgets/Emblems/Actions/Admin/Emblems.php
+++ gadgets/Emblems/Actions/Admin/Emblems.php
@@ -216,6 +216,6 @@ class Emblems_Actions_Admin_Emblems extends Jaws_Gadget_Action
             }
         }
 
-        return Jaws_Header::Location(BASE_SCRIPT . '?gadget=Emblems');
+        return Jaws_Header::Location(BASE_SCRIPT . '?reqGadget=Emblems');
     }
 }
