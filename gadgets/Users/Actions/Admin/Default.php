ac18a3184645c29865136a0b7f8b3fbcc809b99c
afz <afzcode@gmail.com>
2020-07-05 21:33:53 +0430

    [Users]: gadget/action renamed to reqGadget/reqAction

--- gadgets/Users/Actions/Admin/Default.php
+++ gadgets/Users/Actions/Admin/Default.php
@@ -47,33 +47,43 @@ class Users_Actions_Admin_Default extends Jaws_Gadget_Action
 
         $menubar = new Jaws_Widgets_Menubar();
         if ($this->gadget->GetPermission('ManageUsers')) {
-            $menubar->AddOption('Users',
-                                _t('USERS_TITLE'),
-                                BASE_SCRIPT . '?gadget=Users&amp;action=Users',
-                                'gadgets/Users/Resources/images/users_mini.png');
+            $menubar->AddOption(
+                'Users',
+                _t('USERS_TITLE'),
+                $this->gadget->url('Users'),
+                'gadgets/Users/Resources/images/users_mini.png'
+            );
         }
         if ($this->gadget->GetPermission('ManageGroups')) {
-            $menubar->AddOption('Groups',
-                                _t('USERS_GROUPS_GROUPS'),
-                                BASE_SCRIPT . '?gadget=Users&amp;action=Groups',
-                                'gadgets/Users/Resources/images/groups_mini.png');
+            $menubar->AddOption(
+                'Groups',
+                _t('USERS_GROUPS_GROUPS'),
+                $this->gadget->url('Groups'),
+                'gadgets/Users/Resources/images/groups_mini.png'
+            );
         }
-        $menubar->AddOption('ACLs',
+        $menubar->AddOption(
+            'ACLs',
             _t('USERS_ACLS'),
-            BASE_SCRIPT . '?gadget=Users&amp;action=ACLs',
-            'gadgets/Users/Resources/images/acls.png');
+            $this->gadget->url('ACLs'),
+            'gadgets/Users/Resources/images/acls.png'
+        );
 
         if ($this->gadget->GetPermission('ManageOnlineUsers')) {
-            $menubar->AddOption('OnlineUsers',
+            $menubar->AddOption(
+                'OnlineUsers',
                 _t('USERS_ONLINE_USERS'),
-                BASE_SCRIPT . '?gadget=Users&amp;action=OnlineUsers',
-                STOCK_PREFERENCES);
+                $this->gadget->url('OnlineUsers'),
+                STOCK_PREFERENCES
+            );
         }
         if ($this->gadget->GetPermission('ManageSettings')) {
-            $menubar->AddOption('Settings',
-                                _t('GLOBAL_SETTINGS'),
-                                BASE_SCRIPT . '?gadget=Users&amp;action=Settings',
-                                STOCK_PREFERENCES);
+            $menubar->AddOption(
+                'Settings',
+                _t('GLOBAL_SETTINGS'),
+                $this->gadget->url('Settings'),
+                STOCK_PREFERENCES
+            );
         }
         $menubar->Activate($action);
         return $menubar->Get();
