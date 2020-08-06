8cced936b017b1adf23d0ad39af85d7de04a6a0e
afz <afzcode@gmail.com>
2020-08-06 11:28:14 +0430

    [Forums]: Added hooks for user attributes extension

---
+++ gadgets/Forums/Hooks/UsersAttributes.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Forums - Preferences hook
+ *
+ * @category    GadgetHook
+ * @package     Forums
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2020 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ */
+class Forums_Hooks_UsersAttributes extends Jaws_Gadget_Hook
+{
+    /**
+     * Gets user's attributes extension
+     *
+     * @access  public
+     * @return  array   Formatted array for using in Users attributes extension action
+     */
+    function Execute()
+    {
+        $attrs = array();
+        $attrs['level'] = array(
+            'type'  => 'number',
+            'title' => _t('FORUMS_USERS_ATTRIBUTES_LEVEL'),
+            'value' => 1,
+            'ltr' => true,
+        );
+
+        return $attrs;
+    }
+
+}
