e9d394bc31546ffe289d2771e8bc4089071c52de
afz <afzcode@gmail.com>
2020-08-21 15:13:16 +0430

    [Jaws_ExTemplate]: Added document comment link to all tags

--- include/Jaws/ExTemplate/Tags/Decrement.php
+++ include/Jaws/ExTemplate/Tags/Decrement.php
@@ -8,6 +8,7 @@
  * @author      Ali Fazelzadeh <afz@php.net>
  * @copyright   2020 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/lesser.html
+ * @doc         https://shopify.github.io/liquid/tags/variable/
  */
 class Jaws_ExTemplate_Tags_Decrement extends Jaws_ExTemplate_Tag
 {
--- include/Jaws/ExTemplate/Tags/Raw.php
+++ include/Jaws/ExTemplate/Tags/Raw.php
@@ -8,6 +8,7 @@
  * @author      Ali Fazelzadeh <afz@php.net>
  * @copyright   2020 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/lesser.html
+ * @doc         https://shopify.github.io/liquid/tags/raw/
  */
 class Jaws_ExTemplate_Tags_Raw extends Jaws_ExTemplate_TagSegmental
 {
