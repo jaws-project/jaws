90a2d9fc71a6c710c0654eb786eef359337aa9f6
afz <afzcode@gmail.com>
2020-09-05 05:25:40 +0430

    [Categories]: using new translation api interface method

--- gadgets/Categories/Actions/Admin/Categories.php
+++ gadgets/Categories/Actions/Admin/Categories.php
@@ -20,12 +20,12 @@ class Categories_Actions_Admin_Categories extends Categories_Actions_Admin_Defau
     {
         $this->gadget->CheckPermission('ManageCategories');
         $this->AjaxMe('script.js');
-        $this->gadget->define('confirmDelete', _t('GLOBAL_CONFIRM_DELETE'));
+        $this->gadget->define('confirmDelete', Jaws::t('CONFIRM_DELETE'));
         $this->gadget->define('lbl_gadget', _t('CATEGORIES_GADGET'));
         $this->gadget->define('lbl_action', _t('CATEGORIES_ACTION'));
-        $this->gadget->define('lbl_title', _t('GLOBAL_TITLE'));
-        $this->gadget->define('lbl_edit', _t('GLOBAL_EDIT'));
-        $this->gadget->define('lbl_delete', _t('GLOBAL_DELETE'));
+        $this->gadget->define('lbl_title', Jaws::t('TITLE'));
+        $this->gadget->define('lbl_edit', Jaws::t('EDIT'));
+        $this->gadget->define('lbl_delete', Jaws::t('DELETE'));
         $this->gadget->define('req_gadget', $req_gadget);
         $this->gadget->define('req_action', $req_action);
 
@@ -35,26 +35,26 @@ class Categories_Actions_Admin_Categories extends Categories_Actions_Admin_Defau
         //Menu bar
         $tpl->SetVariable('menubar', empty($menubar)? $this->MenuBar('Categories') : $menubar);
 
-        $tpl->SetVariable('lbl_of', _t('GLOBAL_OF'));
-        $tpl->SetVariable('lbl_to', _t('GLOBAL_TO'));
-        $tpl->SetVariable('lbl_items', _t('GLOBAL_ITEMS'));
-        $tpl->SetVariable('lbl_per_page', _t('GLOBAL_PERPAGE'));
-        $tpl->SetVariable('lbl_cancel', _t('GLOBAL_CANCEL'));
-        $tpl->SetVariable('lbl_save', _t('GLOBAL_SAVE'));
-        $tpl->SetVariable('lbl_add', _t('GLOBAL_ADD'));
+        $tpl->SetVariable('lbl_of', Jaws::t('OF'));
+        $tpl->SetVariable('lbl_to', Jaws::t('TO'));
+        $tpl->SetVariable('lbl_items', Jaws::t('ITEMS'));
+        $tpl->SetVariable('lbl_per_page', Jaws::t('PERPAGE'));
+        $tpl->SetVariable('lbl_cancel', Jaws::t('CANCEL'));
+        $tpl->SetVariable('lbl_save', Jaws::t('SAVE'));
+        $tpl->SetVariable('lbl_add', Jaws::t('ADD'));
 
-        $tpl->SetVariable('lbl_term',   _t('GLOBAL_TERM'));
+        $tpl->SetVariable('lbl_term',   Jaws::t('TERM'));
         $tpl->SetVariable('lbl_gadget', _t('CATEGORIES_GADGET'));
         $tpl->SetVariable('lbl_action', _t('CATEGORIES_ACTION'));
-        $tpl->SetVariable('lbl_title',  _t('GLOBAL_TITLE'));
-        $tpl->SetVariable('lbl_published', _t('GLOBAL_PUBLISHED'));
-        $tpl->SetVariable('lbl_no',  _t('GLOBAL_NO'));
-        $tpl->SetVariable('lbl_yes', _t('GLOBAL_YES'));
-        $tpl->SetVariable('lbl_description', _t('GLOBAL_DESCRIPTION'));
-        $tpl->SetVariable('lbl_meta_title',  _t('GLOBAL_META_TITLE'));
-        $tpl->SetVariable('lbl_meta_keywords',    _t('GLOBAL_META_KEYWORDS'));
-        $tpl->SetVariable('lbl_meta_description', _t('GLOBAL_META_DESCRIPTION'));
-        $tpl->SetVariable('lbl_meta_info', _t('GLOBAL_META_INFO'));
+        $tpl->SetVariable('lbl_title',  Jaws::t('TITLE'));
+        $tpl->SetVariable('lbl_published', Jaws::t('PUBLISHED'));
+        $tpl->SetVariable('lbl_no',  Jaws::t('NO'));
+        $tpl->SetVariable('lbl_yes', Jaws::t('YES'));
+        $tpl->SetVariable('lbl_description', Jaws::t('DESCRIPTION'));
+        $tpl->SetVariable('lbl_meta_title',  Jaws::t('META_TITLE'));
+        $tpl->SetVariable('lbl_meta_keywords',    Jaws::t('META_KEYWORDS'));
+        $tpl->SetVariable('lbl_meta_description', Jaws::t('META_DESCRIPTION'));
+        $tpl->SetVariable('lbl_meta_info', Jaws::t('META_INFO'));
 
         $tpl->SetVariable('lbl_insert_time', _t('CATEGORIES_INSERT_TIME'));
 
@@ -81,7 +81,7 @@ class Categories_Actions_Admin_Categories extends Categories_Actions_Admin_Defau
             $tpl->SetVariable('lbl_gadget', _t('CATEGORIES_GADGET'));
             $tpl->SetVariable('lbl_action', _t('CATEGORIES_ACTION'));
             if (!Jaws_Error::IsError($gadgetList) && count($gadgetList) > 0) {
-                array_unshift($gadgetList, array('name' => 0, 'title' => _t('GLOBAL_ALL')));
+                array_unshift($gadgetList, array('name' => 0, 'title' => Jaws::t('ALL')));
                 foreach ($gadgetList as $gadget) {
                     $tpl->SetBlock('Categories/visible_filters/filter_gadget');
                     $tpl->SetVariable('value', $gadget['name']);
