c0db7bd6aa771a7fb39c8685c355d5a06f6c3849
afz <afzcode@gmail.com>
2020-12-24 16:22:10 +0330

    [Users]: Fixed typo

--- gadgets/Users/Account/Default/Register.php
+++ gadgets/Users/Account/Default/Register.php
@@ -166,8 +166,8 @@ class Users_Account_Default_Register extends Users_Account_Default
             $urlParams['referrer'] = $referrer;
         }
 
-        if (Jaws_Error::IsError($result)) {
-            http_response_code($result->getCode());
+        if (Jaws_Error::IsError($error)) {
+            http_response_code($error->getCode());
         } else {
             // 201 http code for success login
             http_response_code(201);
