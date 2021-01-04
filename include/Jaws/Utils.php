a65497c2405f1dab3f9ecf2e4d61a70ac4db2263
afz <afzcode@gmail.com>
2021-01-05 02:10:11 +0330

    [Jaws_Utils]: tail of last commit

--- include/Jaws/Utils.php
+++ include/Jaws/Utils.php
@@ -427,7 +427,7 @@ class Jaws_Utils
              */
             function is_vaild_theme(&$item, $key, $path)
             {
-                if (!file_exists($path . $item . DIRECTORY_SEPARATOR. 'Layout.html')) {
+                if (!file_exists($path . $item . '/Layout.html')) {
                     $item = '';
                 }
                 return true;
@@ -501,135 +501,6 @@ class Jaws_Utils
         }
 
         return is_null($locality)? $themes : (is_null($theme)? $themes[$locality] : @$themes[$locality][$theme]);
-    }
-
-    /**
-     * Detect MIME Content-type by extension for a file
-     *
-     * @access  public
-     * @param   string  $filename   File name
-     * @return  string  Returns the content type in MIME format
-     * @see     https://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types
-     */
-    static function mime_extension_type($filename)
-    {
-        static $mime_types;
-        if (!isset($mime_types)) {
-            $mime_types = array(
-                // directory/folder
-                JAWS_FILE_TYPE['FOLDER'] => array(
-                    'folder' => 'inode/directory',
-                ),
-                // archive
-                JAWS_FILE_TYPE['ARCHIVE'] => array(
-                    'tar'   => 'application/x-tar',
-                    'zip'   => 'application/zip',
-                    'rar'   => 'application/x-rar-compressed',
-                    '7z'    => 'application/x-7z-compressed',
-                    'bz'    => 'application/x-bzip',
-                    'bz2'   => 'application/x-bzip2',
-                    'gz'    => 'application/x-gzip',
-                ),
-                // audio
-                JAWS_FILE_TYPE['AUDIO'] => array(
-                    'mid'   => 'audio/midi',
-                    'm4a'   => 'audio/mp4',
-                    'mp4a'  => 'audio/mp4',
-                    'mp3'   => 'audio/mpeg',
-                    'mpga'  => 'audio/mpeg',
-                    'ogg'   => 'audio/ogg',
-                    'weba'  => 'audio/webm',
-                    'aac'   => 'audio/x-aac',
-                    'mka'   => 'audio/x-matroska',
-                    'wma'   => 'audio/x-ms-wma',
-                    'wav'   => 'audio/x-wav',
-                ),
-                // font
-                JAWS_FILE_TYPE['FONT'] => array(
-                    'otf'   => 'font/otf',
-                    'ttf'   => 'font/ttf',
-                    'woff'  => 'font/woff',
-                    'woff2' => 'font/woff2',
-                ),
-                // image
-                JAWS_FILE_TYPE['IMAGE'] => array(
-                    'bmp'   => 'image/bmp',
-                    'gif'   => 'image/gif',
-                    'jpeg'  => 'image/jpeg',
-                    'jpg'   => 'image/jpeg',
-                    'png'   => 'image/png',
-                    'svg'   => 'image/svg+xml',
-                    'tiff'  => 'image/tiff',
-                    'webp'  => 'image/webp',
-                    'ico'   => 'image/x-icon',
-                    'pcx'   => 'image/x-pcx',
-                ),
-                // text
-                JAWS_FILE_TYPE['TEXT'] => array(
-                    'css'   => 'text/css',
-                    'csv'   => 'text/csv',
-                    'html'  => 'text/html',
-                    'htm'   => 'text/html',
-                    'txt'   => 'text/plain',
-                    'text'  => 'text/plain',
-                    'conf'  => 'text/plain',
-                    'log'   => 'text/plain',
-                    'ini'   => 'text/plain',
-                    'php'   => 'text/x-php',
-                    'java'  => 'text/x-java-source',
-                    'opml'  => 'text/x-opml',
-                    'vcf'   => 'text/x-vcard',
-                ),
-                // video
-                JAWS_FILE_TYPE['VIDEO'] => array(
-                    '3gp'   => 'video/3gpp',
-                    'mp4'   => 'video/mp4',
-                    'mpg4'  => 'video/mp4',
-                    'mpeg'  => 'video/mpeg',
-                    'mpg'   => 'video/mpeg',
-                    'ogv'   => 'video/ogg',
-                    'mov'   => 'video/quicktime',
-                    'webm'  => 'video/webm',
-                    'flv'   => 'video/x-flv',
-                    'mkv'   => 'video/x-matroska',
-                    'avi'   => 'video/x-msvideo',
-                ),
-                // unknown
-                JAWS_FILE_TYPE['UNKNOWN'] => array(
-                    'atom'  => 'application/atom+xml',
-                    'jar'   => 'application/java-archive',
-                    'js'    => 'application/javascript',
-                    'json'  => 'application/json',
-                    'pdf'   => 'application/pdf',
-                    'rss'   => 'application/rss+xml',
-                    'apk'   => 'application/vnd.android.package-archive',
-                    'wsdl'  => 'application/wsdl+xml',
-                    'lnk'   => 'application/x-ms-shortcut',
-                    'sql'   => 'application/x-sql',
-                    'swf'   => 'application/x-shockwave-flash',
-                    'xhtml' => 'application/xhtml+xml',
-                    'xml'   => 'application/xml',
-                    'xsl'   => 'application/xml',
-                    'dtd'   => 'application/xml-dtd',
-                    'xslt'  => 'application/xslt+xml',
-                ),
-            );
-        }
-
-        $ext = pathinfo($filename, PATHINFO_EXTENSION);
-        foreach (JAWS_FILE_TYPE as $type) {
-            if (array_key_exists($ext, $mime_types[$type])) {
-                return array(
-                    'mime' => $mime_types[$type][$ext],
-                    'type' => $type
-                );
-            }
-        }
-
-        return array(
-            'mime' => 'application/octet-stream',
-            'type' => JAWS_FILE_TYPE['UNKNOWN']
-        );
     }
 
 }
