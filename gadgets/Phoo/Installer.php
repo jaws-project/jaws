e5cecb5432613c4176b463485e11137b32fc6295
afz <afzcode@gmail.com>
2021-01-08 16:40:27 +0330

    [Phoo]: no longer using DIRECTORY_SEPARATOR

--- gadgets/Phoo/Installer.php
+++ gadgets/Phoo/Installer.php
@@ -65,7 +65,7 @@ class Phoo_Installer extends Jaws_Gadget_Installer
             return new Jaws_Error(Jaws::t('ERROR_FAILED_DIRECTORY_UNWRITABLE', ROOT_DATA_PATH));
         }
 
-        $new_dir = ROOT_DATA_PATH . 'phoo' . DIRECTORY_SEPARATOR;
+        $new_dir = ROOT_DATA_PATH . 'phoo/';
         if (!Jaws_FileManagement_File::mkdir($new_dir)) {
             return new Jaws_Error(Jaws::t('ERROR_FAILED_CREATING_DIR', $new_dir));
         }
