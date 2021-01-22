42979e5244c527a712bbcf6696e57e778c1854cd
afz <afzcode@gmail.com>
2021-01-22 17:51:30 +0330

    Added new class: Jaws_Exception

---
+++ include/Jaws/Exception.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Jaws Exception
+ *
+ * @category    Exception
+ * @package     Core
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2020 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/lesser.html
+ */
+class Jaws_Exception extends ErrorException
+{
+    /**
+     * Destructor
+     *
+     * @access  public
+     * @return  void
+     */
+    function __destruct() {
+        if (isset($GLOBALS['log'])) {
+            $GLOBALS['log']->Log($this->severity, '[' . $this->code . ']: ' . $this->message);
+        }
+    }
+
+    /**
+     * Sets the Exception message
+     *
+     * @access  public
+     * @param   string  $message    Error message
+     * @return  void
+     */
+    function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Sets the Exception code
+     *
+     * @access  public
+     * @param   int     $code   Exception code
+     * @return  void
+     */
+    function setCode($code)
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * Sets exception severity
+     *
+     * @access  public
+     * @param   int     $severity  Exception severity
+     * @return  void
+     */
+    function setSeverity($severity = E_ERROR)
+    {
+        $this->severity = $severity;
+    }
+
+}
