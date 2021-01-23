060ca83d28f19cf5be089eaee1623180f2fa5d05
afz <afzcode@gmail.com>
2021-01-23 11:13:47 +0330

    [Blocks]: @2021

--- gadgets/Blocks/Hooks/Menu.php
+++ gadgets/Blocks/Hooks/Menu.php
@@ -5,7 +5,7 @@
  * @category    GadgetHook
  * @package     Blocks
  * @author      Ali Fazelzadeh <afz@php.net>
- * @copyright   2008-2020 Jaws Development Group
+ * @copyright   2008-2021 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/gpl.html
  */
 class Blocks_Hooks_Menu extends Jaws_Gadget_Hook
