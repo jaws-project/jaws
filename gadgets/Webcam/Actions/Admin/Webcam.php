5f3ab5fd1132abac9441adbe4fcce4ec2a80b5ef
afz <afzcode@gmail.com>
2021-01-23 11:14:59 +0330

    [Webcam]: @2021

--- gadgets/Webcam/Actions/Admin/Webcam.php
+++ gadgets/Webcam/Actions/Admin/Webcam.php
@@ -6,7 +6,7 @@
  * @package    Webcam
  * @author     Jonathan Hernandez <ion@suavizado.com>
  * @author     Pablo Fischer <pablo@pablo.com.mx>
- * @copyright  2004-2020 Jaws Development Group
+ * @copyright  2004-2021 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class Webcam_Actions_Admin_Webcam extends Jaws_Gadget_Action
