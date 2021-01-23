aa8f54ce130e1951d74e66dfe4358e5c06d5ed74
afz <afzcode@gmail.com>
2021-01-23 11:13:56 +0330

    [Contact]: @2021

--- gadgets/Contact/Hooks/Menu.php
+++ gadgets/Contact/Hooks/Menu.php
@@ -5,7 +5,7 @@
  * @category   GadgetHook
  * @package    Contact
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2007-2020 Jaws Development Group
+ * @copyright  2007-2021 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class Contact_Hooks_Menu extends Jaws_Gadget_Hook
--- gadgets/Contact/Resources/script.js
+++ gadgets/Contact/Resources/script.js
@@ -6,7 +6,7 @@
  * @author     Pablo Fischer <pablo@pablo.com.mx>
  * @author     Ali Fazelzadeh <afz@php.net>
  * @author     Mohsen Khahani <mkhahani@gmail.com>
- * @copyright  2005-2015 Jaws Development Group
+ * @copyright  2005-2021 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 /**
