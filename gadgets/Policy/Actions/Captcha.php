ea447af4490dead79a5e816913a21f1fa2d788cb
afz <afzcode@gmail.com>
2021-01-23 11:14:31 +0330

    [Policy]: @2021

--- gadgets/Policy/Actions/Captcha.php
+++ gadgets/Policy/Actions/Captcha.php
@@ -5,7 +5,7 @@
  * @category   Gadget
  * @package    Policy
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2008-2020 Jaws Development Group
+ * @copyright  2008-2021 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/lesser.html
  */
 class Policy_Actions_Captcha extends Jaws_Gadget_Action
--- gadgets/Policy/Package.php
+++ gadgets/Policy/Package.php
@@ -5,7 +5,7 @@
  * @category   PackageConfig
  * @package    Policy
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2007-2020 Jaws Development Group
+ * @copyright  2007-2021 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/lesser.html
  */
 $info = array(           
