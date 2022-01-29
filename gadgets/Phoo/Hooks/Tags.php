c83757b59de33a7e001b91c6da5a64bef9cee1e7
afz <afzcode@gmail.com>
2022-01-29 23:02:34 +0330

    [Phoo]: Added hook of Tags

---
+++ gadgets/Phoo/Hooks/Tags.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Phoo - Tags gadget hook
+ *
+ * @category    GadgetHook
+ * @package     Phoo
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2022 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ */
+class Phoo_Hooks_Tags extends Jaws_Gadget_Hook
+{
+    /**
+     * Returns an array with the results of a tag content
+     *
+     * @access  public
+     * @param   string  $action     Action name
+     * @param   array   $references Array of References
+     * @return  array   An array of entries that matches a certain pattern
+     */
+    function Execute($action, $references)
+    {
+        if(empty($action) || !is_array($references) || empty($references)) {
+            return false;
+        }
+
+/*
+        $table->join('phoo_image', 'phoo_image.id', 'phoo_image_album.phoo_image_id');
+        $table->join('phoo_album', 'phoo_album.id', 'phoo_image_album.phoo_album_id');
+
+        $table->and()->where('phoo_image.published', true);
+        $table->and()->where('phoo_album.published', true);
+*/
+        $result = Jaws_ORM::getInstance()
+            ->table('phoo_image')
+            ->select('id:integer', 'filename', 'fast_url', 'title', 'description', 'updatetime')
+            ->where('id', $references, 'in')
+            ->and()
+            ->where('published', true)
+            ->fetchAll();
+        if (Jaws_Error::IsError($result)) {
+            return array();
+        }
+
+        $date = Jaws_Date::getInstance();
+        $photos = array();
+        foreach ($result as $r) {
+            $photo = array();
+            $photo['title']   = $r['title'];
+            $photo['url']     = $this->gadget->urlMap('Photo', array('photo' => $r['id']));
+            $photo['outer']   = false;
+            $photo['image']   = $this->gadget->model->load('Common')->GetThumbPath($r['filename']);
+            $photo['snippet'] = $r['description'];
+            $photo['date']    = $date->ToISO($r['updatetime']);
+            $photos[$r['id']] = $photo;
+        }
+
+        return $photos;
+    }
+
+}
