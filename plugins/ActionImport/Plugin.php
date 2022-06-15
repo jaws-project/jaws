0cf10b150fa4d0f6049a2b00416a4789629a0764
afz <afzcode@gmail.com>
2022-06-15 19:11:55 +0430

    [ActionImport]: @2022

--- plugins/ActionImport/Plugin.php
+++ plugins/ActionImport/Plugin.php
@@ -5,7 +5,7 @@
  * @category   Plugin
  * @package    ActionImport
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2008-2020 Jaws Development Group
+ * @copyright   2008-2022 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class ActionImport_Plugin extends Jaws_Plugin
