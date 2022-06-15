e9d1651c7dc9f8776d4ea0f715b355d79b951c54
afz <afzcode@gmail.com>
2022-06-15 19:10:13 +0430

    [SysInfo]: @2022

--- gadgets/SysInfo/Package.php
+++ gadgets/SysInfo/Package.php
@@ -5,7 +5,7 @@
  * @category   PackageConfig
  * @package    SysInfo
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2008-2021 Jaws Development Group
+ * @copyright   2008-2022 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/lesser.html
  */
 $info = array(           
