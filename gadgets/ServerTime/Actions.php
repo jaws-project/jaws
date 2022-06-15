4a2dd5357d94ff2f07805dcf7437e67d520cb9bb
afz <afzcode@gmail.com>
2022-06-15 19:10:03 +0430

    [ServerTime]: @2022

--- gadgets/ServerTime/Actions.php
+++ gadgets/ServerTime/Actions.php
@@ -5,7 +5,7 @@
  * @category    GadgetActions
  * @package     ServerTime
  * @author      Pablo Fischer <pablo@pablo.com.mx>
- * @copyright   2004-2021 Jaws Development Group
+ * @copyright   2004-2022 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/gpl.html
  */
 
