530f8a6ec26566fde44250c4e115213ab2368986
afz <afzcode@gmail.com>
2022-06-15 19:09:53 +0430

    [Policy]: @2022

--- gadgets/Policy/Map.php
+++ gadgets/Policy/Map.php
@@ -5,7 +5,7 @@
  * @category   GadgetMaps
  * @package    Policy
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2008-2021 Jaws Development Group
+ * @copyright   2008-2022 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/lesser.html
  */
 $maps[] = array(
