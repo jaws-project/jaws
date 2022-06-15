bb6e223568de58f4230b084be8c066d025265e1f
afz <afzcode@gmail.com>
2022-06-15 19:09:40 +0430

    [Launcher]: @2022

--- gadgets/Launcher/Actions.php
+++ gadgets/Launcher/Actions.php
@@ -5,7 +5,7 @@
  * @category    GadgetActions
  * @package     Launcher
  * @author      Jonathan Hernandez <ion@suavizado.com>
- * @copyright   2006-2021 Jaws Development Group
+ * @copyright   2006-2022 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/gpl.html
  */
 /* Actions*/
--- gadgets/Launcher/Hooks/Menu.php
+++ gadgets/Launcher/Hooks/Menu.php
@@ -5,7 +5,7 @@
  * @category   GadgetHook
  * @package    Launcher
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2008-2021 Jaws Development Group
+ * @copyright   2008-2022 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class Launcher_Hooks_Menu extends Jaws_Gadget_Hook
