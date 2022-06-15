879e521eeff82ac7623f01aab11dfdacc0ff88f7
afz <afzcode@gmail.com>
2022-06-15 19:09:33 +0430

    [Files]: @2022

--- gadgets/Files/Map.php
+++ gadgets/Files/Map.php
@@ -5,7 +5,7 @@
  * @category    GadgetMaps
  * @package     Files
  * @author      Ali Fazelzadeh <afz@php.net>
- * @copyright   2019-2021 Jaws Development Group
+ * @copyright   2019-2022 Jaws Development Group
  */
 $maps[] = array(
     'file',
