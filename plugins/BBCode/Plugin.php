d09e49e765c38a967ecb1809eaeca640d1438364
afz <afzcode@gmail.com>
2022-06-15 19:11:57 +0430

    [BBCode]: @2022

--- plugins/BBCode/Plugin.php
+++ plugins/BBCode/Plugin.php
@@ -5,7 +5,7 @@
  * @category   Plugin
  * @package    BBCode
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright  2012-2020 Jaws Development Group
+ * @copyright   2012-2022 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class BBCode_Plugin extends Jaws_Plugin
