68554162a023296cf7eade0f9137dcab9231e126
afz <afzcode@gmail.com>
2022-06-15 19:10:15 +0430

    [Tms]: @2022

--- gadgets/Tms/Actions.php
+++ gadgets/Tms/Actions.php
@@ -5,7 +5,7 @@
  * @category    GadgetActions
  * @package     TMS
  * @author      Ali Fazelzadeh <afz@php.net>
- * @copyright   2012-2021 Jaws Development Group
+ * @copyright   2012-2022 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/lesser.html
  */
 
