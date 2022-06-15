5085215bec12c5d38aeb377b0f2c92513aebeab7
afz <afzcode@gmail.com>
2022-06-15 19:09:29 +0430

    [Faq]: @2022

--- gadgets/Faq/Hooks/Sitemap.php
+++ gadgets/Faq/Hooks/Sitemap.php
@@ -4,8 +4,8 @@
  *
  * @category    GadgetHook
  * @package     Faq
- * @author      Mojtaba Ebrahimi <ebrahimi@zehneziba.ir>
- * @copyright   2008-2021 Jaws Development Group
+ * @author      ZehneZiba <zzb@zehneziba.ir>
+ * @copyright   2008-2022 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/gpl.html
  */
 class Faq_Hooks_Sitemap extends Jaws_Gadget_Hook
