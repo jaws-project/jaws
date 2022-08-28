0cb4e98ecdf9eb246c294bb5311725ee8a894090
afz <afzcode@gmail.com>
2022-08-28 16:59:17 +0430

    [Forums]: Added sample "Files" hook

---
+++ gadgets/Forums/Hooks/Files.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Forums - Files hook
+ *
+ * @category    GadgetHook
+ * @package     Forums
+ * @author      Ali Fazelzadeh <afz@php.net>
+ * @copyright   2021-2022 Jaws Development Group
+ * @license     http://www.gnu.org/copyleft/lgpl.html
+ */
+class Forums_Hooks_Files extends Jaws_Gadget_Hook
+{
+    /**
+     * Check access permission
+     *
+     * @access  public
+     * @param   string  $interface  Gadget interface(gadget, action, reference, ...)
+     * @return  bool    True if allowed otherwise False
+     */
+    function Execute($interface = array())
+    {
+        return true;
+    }
+
+}
