0d951bfc0aa83d65154e27294edc19540b7a8e7b
Ali Fazelzadeh <afzcode@gmail.com>
2023-08-19 06:55:01 +0330

    [Phoo]: Applied new changes in Jaws_Request

--- gadgets/Phoo/Actions/Admin/Ajax.php
+++ gadgets/Phoo/Actions/Admin/Ajax.php
@@ -48,7 +48,7 @@ class Phoo_Actions_Admin_Ajax extends Jaws_Gadget_Action
         @list($id, $title, $desc, $allow_comments, $published, $albums, $meta_keywords, $meta_description, $tags) =
             $this->gadget->request->fetchAll('post');
         $albums = $this->gadget->request->fetch('5:array', 'post');
-        $desc = $this->gadget->request->fetch(2, 'post', 'strip_crlf');
+        $desc = $this->gadget->request->fetch(2, 'post', false, array('filter' => 'strip_crlf'));
         if (!$this->gadget->GetPermission('ManageAlbums')) {
             $albums    = null;
             $published = null;
