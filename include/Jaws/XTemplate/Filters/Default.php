1555eb1ccd205b16606274736d6b66d06d6bd2ff
Ali Fazelzadeh <afzcode@gmail.com>
2023-09-23 07:48:55 +0330

    [Jaws_XTemplate_Filters_Default]: Added strtodate/datetostr & strto time/timetostr filters

--- include/Jaws/XTemplate/Filters/Default.php
+++ include/Jaws/XTemplate/Filters/Default.php
@@ -11,16 +11,69 @@
 class Jaws_XTemplate_Filters_Default extends Jaws_XTemplate_Filters
 {
     /**
-     * Formats a date using Jaws_Date::Format
+     * Formats UTC timestamp to string using Jaws_Date::Format
      *
-     * @param mixed $input
-     * @param string $format
+     * @param   int     $input
+     * @param   string  $format
+     * @param   string  $calendar
      *
      * @return string
      */
-    public static function date($input, $format = '', $calendar = '')
+    public static function datetostr($input, $format = '', $calendar = '')
     {
-        return Jaws_Date::getInstance($calendar)->Format($input, $format);
+        return Jaws_Date::getInstance($calendar)->Format($input, $format, false);
+    }
+
+    /**
+     * Convert datetime string to UTC timestamp
+     *
+     * @param   string  $input
+     * @param   string  $calendar
+     *
+     * @return int
+     */
+    public static function strtodate($input, $calendar = '')
+    {
+        $result = 0;
+        if (!empty($input)) {
+            $result = (int)Jaws_Date::getInstance($calendar)->ToBaseDate(preg_split('/[\/\- \:]/', $input), 'U');
+        }
+
+        return $result;
+    }
+
+    /**
+     * Formats time to string
+     *
+     * @param   int     $input  seconds since midnight
+     * @param   string  $format
+     *
+     * @return string
+     */
+    public static function timetostr($input, $format = '')
+    {
+        return Jaws_Date::getInstance()->Format($input, $format, false);
+    }
+
+    /**
+     * Convert time string to seconds since midnight
+     *
+     * @param   string  $input  time string (for example: 13:45)
+     *
+     * @return int
+     */
+    public static function strtotime($input)
+    {
+        $time = new Jaws_Regexp('/([0-9]+)\:([0-9]+)(?:\:([0-9]+))?\s*([ap]m)?/i');
+        if (false === $time->match($input)) {
+            return 0;
+        }
+        @list($all, $hours, $minutes, $seconds, $meridiem) = $time->matches;
+        if (isset($meridiem) && $meridiem == 'pm' && $hours != 12) {
+            $hours = $hours + 12;
+        }
+
+        return $hours*3600 + $minutes*60 + $seconds;
     }
 
     /**
