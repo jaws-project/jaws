b51fef08896b34a67f9d052e548e88e4bc8f54c2
Ali Fazelzadeh <afzcode@gmail.com>
2023-09-25 07:06:48 +0330

    [Jaws_Layout]: make buildnumber available in clients

--- include/Jaws/Layout.php
+++ include/Jaws/Layout.php
@@ -119,6 +119,7 @@ class Jaws_Layout
 
         // set default site language
         $this->_Languages[] = $this->app->GetLanguage();
+        $this->app->define('', 'buildnumber', $this->attributes['buildnumber']);
         $this->app->define('', 'direction', Jaws::t('LANG_DIRECTION'));
         $this->app->define('', 'loadingMessage', Jaws::t('LOADING'));
         $this->app->define('', 'reloadMessage', Jaws::t('RELOAD_MESSAGE'));
