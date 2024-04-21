023fc037479f1c56f904590642811603c7a4026a
Ali Fazelzadeh <afzcode@gmail.com>
2024-04-21 17:45:52 +0330

    [Emblems]: Fixed gadget javascript file for compatibility with new incoming changes...

--- gadgets/Emblems/Resources/script.js
+++ gadgets/Emblems/Resources/script.js
@@ -8,6 +8,11 @@
  * @copyright   2005-2022 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
+function Jaws_Gadget_Emblems() { return {
+    // ASync callback method
+    AjaxCallback : {
+    },
+}};
 /**
  * Use async mode, create Callback
  */
