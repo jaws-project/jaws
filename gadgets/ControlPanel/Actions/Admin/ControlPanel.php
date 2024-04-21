3a03e276e72d25436452a32b466c6ba42c08b42e
Ali Fazelzadeh <afzcode@gmail.com>
2024-04-21 17:56:51 +0330

    [ControlPanel]: @2024

--- gadgets/ControlPanel/Actions/Admin/ControlPanel.php
+++ gadgets/ControlPanel/Actions/Admin/ControlPanel.php
@@ -6,7 +6,7 @@
  * @package     ControlPanel
  * @author      Jonathan Hernandez <ion@suavizado.com>
  * @author      Ali Fazelzadeh <afz@php.net>
- * @copyright   2004-2022 Jaws Development Group
+ * @copyright   2004-2024 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/lesser.html
  */
 class ControlPanel_Actions_Admin_ControlPanel extends Jaws_Gadget_Action
