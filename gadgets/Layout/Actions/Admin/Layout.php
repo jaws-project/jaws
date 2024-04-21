4be1b423437a0425c4a7bae295e1e7a80d74bb90
Ali Fazelzadeh <afzcode@gmail.com>
2024-04-21 17:57:10 +0330

    [Layout]: @2024

--- gadgets/Layout/Actions/Admin/Layout.php
+++ gadgets/Layout/Actions/Admin/Layout.php
@@ -5,7 +5,7 @@
  * @category    GadgetAdmin
  * @package     Layout
  * @author      Ali Fazelzadeh <afz@php.net>
- * @copyright   2008-2022 Jaws Development Group
+ * @copyright   2008-2024 Jaws Development Group
  * @license     http://www.gnu.org/copyleft/lesser.html
  */
 class Layout_Actions_Admin_Layout extends Jaws_Gadget_Action
