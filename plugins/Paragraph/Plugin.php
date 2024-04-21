2bb20ffc7dadb777726e1d1e02be13cd41f48920
Ali Fazelzadeh <afzcode@gmail.com>
2024-04-21 18:41:20 +0330

    [Paragraph]: @2024

--- plugins/Paragraph/Plugin.php
+++ plugins/Paragraph/Plugin.php
@@ -5,7 +5,7 @@
  * @category   Plugin
  * @package    Paragraph
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright   2017-2022 Jaws Development Group
+ * @copyright   2017-2024 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/lesser.html
  */
 class Paragraph_Plugin extends Jaws_Plugin
