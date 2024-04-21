bf1be7f7ad4d4d12f5b6bc9bf2a121168f5577a9
Ali Fazelzadeh <afzcode@gmail.com>
2024-04-21 18:41:21 +0330

    [SpellNumber]: @2024

--- plugins/SpellNumber/Plugin.php
+++ plugins/SpellNumber/Plugin.php
@@ -5,7 +5,7 @@
  * @category   Plugin
  * @package    SpellNumber
  * @author     Ali Fazelzadeh <afz@php.net>
- * @copyright   2006-2022 Jaws Development Group
+ * @copyright   2006-2024 Jaws Development Group
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class SpellNumber_Plugin extends Jaws_Plugin
